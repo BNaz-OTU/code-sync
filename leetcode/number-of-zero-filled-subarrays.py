98e382d2af1665b61a80be06aa73efca3b6916ef
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-09 12:50:32 -0400

    Update solution for number-of-zero-filled-subarrays

--- leetcode/number-of-zero-filled-subarrays.py
+++ leetcode/number-of-zero-filled-subarrays.py
@@ -1,3 +1,15 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        #Testing git pusher
+        streak = 0
+        counter = 0
+        for index in range(len(nums)):
+            if (nums[index] == 0 and streak == 0):
+                counter += 1
+                streak = 1
+            elif (nums[index] == 0 and streak > 0):
+                streak += 1
+                counter += streak
+            else:
+                streak = 0
+        
+        return counter
