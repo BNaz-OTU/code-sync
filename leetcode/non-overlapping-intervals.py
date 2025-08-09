a7109cbb8510f819433e673ffce5687b56d06c1a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-09 13:59:43 -0400

    Update solution for non-overlapping-intervals

--- leetcode/non-overlapping-intervals.py
+++ leetcode/non-overlapping-intervals.py
@@ -1,32 +1,14 @@
 class Solution:
+    # USE SOLN: https://www.youtube.com/watch?v=nONCGxWoUfM
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        final = 0
+        prev_end = intervals[0][1]
 
-        # DIDNT WORK 
-        
-        boundary = None
-        count = 0
-
-        for inval in intervals:
-            if (boundary == None):
-                boundary = inval
-
-            # # Checks if there are overlaps at the start
-            # elif (boundary[0] == inval[0]):
-            #     print(f'Issue START: B: {boundary} | interval {inval}')
-            #     count += 1
-            
-            # # Checks if there are overlaps at the end
-            # elif (boundary[1] == inval[1]):
-            #     print(f'Issue END: B: {boundary} | interval {inval}')
-            #     count += 1
-            
-            elif (boundary[0] < inval[0] and boundary[1] > inval[1]):
-                count += 1
-            
-            if (boundary[0] > inval[0]):
-                boundary[0] = inval[0]
-            
-            if (boundary[1] < inval[1]):
-                boundary[1] = inval[1]
-        
-        return count
+        for start, end in intervals[1:]:
+            if (start >= prev_end):
+                prev_end = end
+            else:
+                final += 1
+                prev_end = min(end, prev_end)
+        return final
