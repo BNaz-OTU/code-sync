13c3316c4a34afbfb6db7e0a2f7ad4cdced94280
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-12 14:38:23 -0400

    Update solution for daily-leads-and-partners

--- leetcode/daily-leads-and-partners.py
+++ leetcode/daily-leads-and-partners.py
@@ -1,9 +1,7 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT 
-    date_id, 
-    make_name, 
-    COUNT(DISTINCT(lead_id)) AS "unique_leads", 
-    COUNT(DISTINCT(partner_id)) AS "unique_partners"
-FROM DailySales
-GROUP BY date_id, make_name
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    return daily_sales.groupby(['date_id', 'make_name']).agg(
+        unique_leads = ('lead_id', 'nunique'), 
+        unique_partners = ('partner_id', 'nunique')
+    ).reset_index()
