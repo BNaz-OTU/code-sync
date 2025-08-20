0f9fc3fa75756559832b9597d0cad1f346fd1b8e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-20 13:29:57 -0400

    Update solution for customer-who-visited-but-did-not-make-any-transactions

--- leetcode/customer-who-visited-but-did-not-make-any-transactions.py
+++ leetcode/customer-who-visited-but-did-not-make-any-transactions.py
@@ -1,10 +1,6 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT 
-    V.customer_id, 
-    COUNT(*) AS 'count_no_trans'
-FROM Visits AS V
-LEFT JOIN Transactions AS T
-ON V.visit_id = T.visit_id
-WHERE T.transaction_id IS NULL
-GROUP BY customer_id
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    df1 = visits.join(transactions.set_index('visit_id'), on='visit_id', how='left')
+    df2 = df1.loc[df1['transaction_id'].isnull(), ['customer_id', 'transaction_id']]
+    return df2.groupby('customer_id').size().reset_index(name='count_no_trans')
