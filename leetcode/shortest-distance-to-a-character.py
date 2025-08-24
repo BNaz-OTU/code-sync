485f4618d67dca5247df5a4ca49b5fe04bbed37a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-24 16:31:58 -0400

    Update solution for shortest-distance-to-a-character

--- leetcode/shortest-distance-to-a-character.py
+++ leetcode/shortest-distance-to-a-character.py
@@ -1,14 +1,33 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        # USED SOLN: https://leetcode.com/problems/shortest-distance-to-a-character/solutions/2854034/python-99-92-faster-two-pointers-o-n-solution
-        carr = [index for index, value in enumerate(s) if value == c]
-        ans = []
-        j = 0
-        # Since we inc 'i' every time, we can just use for loop
-        for i in range(len(s)):
-            # Only increment if next occurrence is closer than current one
-            if j < len(carr) - 1:
-                if abs(carr[j + 1] - i) < abs(carr[j] - i):
-                    j += 1
-            ans.append(abs(carr[j] - i))
-        return ans
+        fast = 0 
+        slow = None
+        new_list = []
+        stack = []
+
+        while fast < len(s):
+            if (s[fast] == c):
+                stack.append(fast)
+                temp = stack[::-1]
+
+                while len(temp) > 0:
+                    if (slow is None):
+                        new_list.append(fast - temp.pop())
+                    else:
+                        curr_idx = temp.pop()
+                        dist = min(abs(curr_idx - slow), abs(fast - curr_idx))
+                        new_list.append(dist)
+
+                stack = [] 
+                slow = fast
+
+            else:
+                stack.append(fast)
+            
+            fast += 1
+        
+        if (len(stack) > 0):
+            for val in stack:
+                new_list.append(val - slow)
+        
+        return new_list
