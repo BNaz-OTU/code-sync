dcb9a23c9ba8392865208b3ec37b87adf228a4f2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-24 16:56:45 -0400

    Update solution for flipping-an-image

--- leetcode/flipping-an-image.py
+++ leetcode/flipping-an-image.py
@@ -1,18 +1,24 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        left = 0
+        right = len(image) - 1
 
-        # USED SOLN: https://www.youtube.com/watch?v=qnd1WiutGio
+        for row in image:
 
-        for idx in range(len(image)):
+            # Case for when the middle row is odd
+            if (len(row) % 2 == 1):
+                row[len(row) // 2] = 1 - row[len(row) // 2]
+
+            while left < right:
+                print('hi')
+                if (row[left] == row[right]):
+                    row[left] = 1 - row[left]
+                    row[right] = 1 - row[right]
+
+                left += 1
+                right -= 1
+
             left = 0
             right = len(image) - 1
 
-            while (left <= right):
-                if (image[idx][left] == image[idx][right]):
-                    image[idx][left] = 1 - image[idx][left]
-                    image[idx][right] = image[idx][left]
-                
-                left += 1
-                right -= 1
-        
         return image
