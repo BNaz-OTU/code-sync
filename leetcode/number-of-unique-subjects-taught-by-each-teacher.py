1d4570b9af1d249fe61f85e00188fc5f0c0ee4a3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-25 09:31:43 -0400

    Update solution for number-of-unique-subjects-taught-by-each-teacher

--- leetcode/number-of-unique-subjects-taught-by-each-teacher.py
+++ leetcode/number-of-unique-subjects-taught-by-each-teacher.py
@@ -1,4 +1,5 @@
-import pandas as pd
+# Write your MySQL query statement below
 
-def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
-    return teacher.groupby('teacher_id')['subject_id'].nunique().reset_index(name="cnt")
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS "cnt"
+FROM Teacher
+GROUP BY teacher_id
