5a2434501322e9dc6455cb33248e9ea0f300a63b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-26 11:24:32 -0400

    Update solution for the-latest-login-in-2020

--- leetcode/the-latest-login-in-2020.py
+++ leetcode/the-latest-login-in-2020.py
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT user_id, MAX(time_stamp) AS "last_stamp"
-FROM Logins
-WHERE time_stamp BETWEEN '2020-01-01 00:00:00' AND '2020-12-31 23:59:59'
-GROUP BY user_id
+def latest_login(logins: pd.DataFrame) -> pd.DataFrame:
+    df = logins.loc[(logins['time_stamp'] >= '2020-01-01 00:00:00') & (logins['time_stamp'] <= '2020-12-31 23:59:59')]
+
+    return df.groupby('user_id')['time_stamp'].max().reset_index(name='last_stamp')
