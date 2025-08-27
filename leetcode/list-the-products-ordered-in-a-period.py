7c72ad0126930e4ebdce2d8e65ced40e4e0c53cc
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-27 15:42:29 -0400

    Add solution for list-the-products-ordered-in-a-period

---
+++ leetcode/list-the-products-ordered-in-a-period.py
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT P.product_name, SUM(O.unit) AS "unit"
+FROM Products AS P
+JOIN Orders AS O
+ON P.product_id = O.product_id
+WHERE O.order_date BETWEEN "2020-02-01" AND "2020-02-29"
+GROUP BY P.product_id 
+HAVING SUM(O.unit) >= 100
