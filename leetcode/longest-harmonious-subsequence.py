ee56fcb5facba80669196afc5ecb8a8eb3fa62c1
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-29 18:28:09 -0400

    Update solution for longest-harmonious-subsequence

--- leetcode/longest-harmonious-subsequence.py
+++ leetcode/longest-harmonious-subsequence.py
@@ -1,23 +1,21 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        fast = 1
+        sort_nums = sorted(nums)
         slow = 0
-        nums = sorted(nums)
-
+        fast = 1
+        count = 0
         maxCount = 0
 
-        while fast < len(nums):
-            # print(nums)
-            # print(slow)
-            # print(fast)
-            # print("-----")
-            if (abs(nums[fast] - nums[slow]) > 1):
-                slow += 1
-            
-            elif (abs(nums[fast] - nums[slow]) == 1):
-                maxCount = max(maxCount, (fast - slow) + 1)
+        while (fast < len(nums)):
+            print(fast, sort_nums[fast], sort_nums[slow])
+            if (abs(sort_nums[fast] - sort_nums[slow]) == 1):
+                count = (fast - slow) + 1
+                maxCount = max(maxCount, count)
                 fast += 1
 
+            elif (abs(sort_nums[fast] - sort_nums[slow]) > 1):
+                slow += 1
+            
             else:
                 fast += 1
         
