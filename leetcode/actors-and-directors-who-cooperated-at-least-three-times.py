b200d0c62afc56b7a33952d4528610cc7c21bb7f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-29 17:08:48 -0400

    Update solution for actors-and-directors-who-cooperated-at-least-three-times

--- leetcode/actors-and-directors-who-cooperated-at-least-three-times.py
+++ leetcode/actors-and-directors-who-cooperated-at-least-three-times.py
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT actor_id, director_id
-FROM ActorDirector
-GROUP BY actor_id, director_id
-HAVING COUNT(*) >= 3
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(['actor_id', 'director_id']).size().reset_index(name='count')
+    return df.loc[df['count'] >= 3, ['actor_id', 'director_id']]
