d92192b163a40636d7477795a2f2f0a3ee4ced55
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-29 17:54:30 -0400

    Update solution for remove-linked-list-elements

--- leetcode/remove-linked-list-elements.py
+++ leetcode/remove-linked-list-elements.py
@@ -5,17 +5,20 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        # curr = head
-        dummyNode = ListNode()
-        dummyNode.next = head
+        
+        # Setup dummynode so its connected to the head
+        dummyNode = ListNode(next=head)
 
-        curr = dummyNode
-        print(curr)
-        while curr:
-            # print
+        # Readjust the position/index of head so its at the beginning since dummynode was added
+        head = dummyNode
+
+        # Create a temp copy to iterate through the linked list
+        curr = head
+
+        while (curr):
             while (curr.next and curr.next.val == val):
                 curr.next = curr.next.next
-        
+            
             curr = curr.next
-
-        return dummyNode.next
+        
+        return head.next
