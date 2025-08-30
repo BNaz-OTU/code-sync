d642bd25162051c5267202cd4680f735d86ff4e0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-30 16:35:15 -0400

    Update solution for next-greater-element-i

--- leetcode/next-greater-element-i.py
+++ leetcode/next-greater-element-i.py
@@ -2,19 +2,19 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         final = [-1] * len(nums1)
         mono_stack = []
-
-        nums1_dict_idx = {}
+        dict_idx = {}
 
         for idx, val in enumerate(nums1):
-            nums1_dict_idx[val] = idx
-
-        for val in nums2:
-
-            while (len(mono_stack) > 0 and val > mono_stack[-1]):
-                index = nums1_dict_idx[mono_stack.pop()]
-                final[index] = val
-            
-            if (val in nums1):
-                mono_stack.append(val)
+            dict_idx[val] = idx
         
+        for val in nums2:
+            
+            while (len(mono_stack) > 0 and mono_stack[-1] < val):
+                catch = mono_stack.pop()
+                index = dict_idx[catch]
+                final[index] = val
+        
+            if (val in dict_idx):
+                mono_stack.append(val)
+                
         return final
