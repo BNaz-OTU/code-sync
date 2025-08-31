be2210de9ef94d5f21d4484b41dc2ccf238bea25
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-31 15:42:18 -0400

    Add solution for queries-quality-and-percentage

---
+++ leetcode/queries-quality-and-percentage.py
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT 
+    query_name,
+    ROUND((SUM(rating / position)) / COUNT(*), 2) AS "quality",
+    ROUND(AVG(rating < 3) * 100, 2) "poor_query_percentage"
+
+    -- ROUND(AVG(IF(rating < 3, COUNT(*))), 2) AS "poor_query_percentage"
+FROM Queries
+GROUP BY query_name
