88dc0d1804237217ac474329e64f9bffbe116342
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-31 14:24:36 -0400

    Update solution for remove-k-digits

--- leetcode/remove-k-digits.py
+++ leetcode/remove-k-digits.py
@@ -1,18 +1,26 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        stack = []
-        
+        mono_stack = []
+
         for val in num:
-            while (len(stack) > 0 and stack[-1] > val and k > 0):
-                stack.pop()
+            while (len(mono_stack) > 0 and mono_stack[-1] > val and k > 0):
                 k -= 1
+                mono_stack.pop()
             
-            stack.append(val)
-
-        while (k > 0):
-            k -= 1
-            stack.pop()
-
-        new_num = "".join(stack).lstrip('0')
+            mono_stack.append(val)
         
-        return str(new_num) if new_num != "" else '0'
+        while (k > 0):
+            mono_stack.pop()
+            k -= 1
+        
+        final = "".join(mono_stack)
+        final = final.lstrip('0')
+        
+        if (len(final) > 0):
+            return final
+        else:
+            return '0'
+
+        # return str(final)
+        print(final)
+        print(mono_stack)
