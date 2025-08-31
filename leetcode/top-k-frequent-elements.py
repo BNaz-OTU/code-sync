a2517768c99f5ab44439494dc4752b32756b3c55
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-08-31 12:41:33 -0400

    Update solution for top-k-frequent-elements

--- leetcode/top-k-frequent-elements.py
+++ leetcode/top-k-frequent-elements.py
@@ -1,32 +1,30 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_dict = {}
-        num_occurence_dict = {}
+        num_count_dict = {}
         final = []
 
         for idx in range(1, len(nums) + 1):
             freq_dict[idx] = []
         
         for val in nums:
-            if (val in num_occurence_dict):
-                num_occurence_dict[val] += 1
+            if (val in num_count_dict):
+                num_count_dict[val] += 1
             else:
-                num_occurence_dict[val] = 1
+                num_count_dict[val] = 1
         
-        for key in num_occurence_dict:
-            freq_dict[num_occurence_dict[key]].append(key)
+        for key in num_count_dict:
+            freq_dict[num_count_dict[key]].append(key)
         
         for key in reversed(freq_dict):
-            # print(count < len(freq_dict[key]))
             count = 0
-            while count < len(freq_dict[key]) and k > 0:
+            while (k > 0 and count < len(freq_dict[key])):
                 final.append(freq_dict[key][count])
-                # print(freq_dict[key][count])
                 count += 1
                 k -= 1
             
             if (k == 0):
                 return final
 
-        print(num_occurence_dict)
+        print(num_count_dict)
         print(freq_dict)
