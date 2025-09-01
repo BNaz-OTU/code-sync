4cea8aa57e967f94e7db9068dc2cd5804b932cb7
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-01 17:47:49 -0400

    Add solution for maximum-average-subarray-i

---
+++ leetcode/maximum-average-subarray-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        maxCount = 0
+        count = 0
+        left = 0
+
+        for idx in range(len(nums)):
+            
+            if (idx + 1 == k):
+                print('here')
+                count += nums[idx]
+                avg = count / k
+                maxCount = avg
+            
+            elif (idx < k):
+                count += nums[idx]
+
+            else:
+                count -= nums[left]
+                count += nums[idx]
+                left += 1
+
+                avg = count / k
+                maxCount = max(maxCount, avg)
+        
+        return maxCount
