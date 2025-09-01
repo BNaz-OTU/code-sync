411404c86973ddac5593961606c13b0ba184c099
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-01 17:54:55 -0400

    Update solution for maximum-average-subarray-i

--- leetcode/maximum-average-subarray-i.py
+++ leetcode/maximum-average-subarray-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        # ** DIDNT USE SOLN, JUST CHECKED FOR REF: https://www.youtube.com/watch?v=UdUUnoiLkPg **
         maxCount = 0
         count = 0
         left = 0
