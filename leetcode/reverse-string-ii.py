a196876953473b522930dbf4f81331218ad984b8
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-01 15:05:31 -0400

    Add solution for reverse-string-ii

---
+++ leetcode/reverse-string-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        count = 0
+        s_list = list(s)
+
+        if (len(s) < k):
+            return s[::-1]
+
+        while count < len(s):
+            left = count
+            right = count + k - 1
+
+            if (right > len(s)):
+                right = len(s) - 1
+
+            while(left < right):
+                s_list[right], s_list[left] = s_list[left], s_list[right]
+                left += 1
+                right -= 1
+            
+            count += 2 * k
+        
+        return "".join(s_list)
