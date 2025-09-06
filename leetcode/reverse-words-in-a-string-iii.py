f5904a47d008a6a4ec6b185c5401e615a12338c7
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-06 16:12:13 -0400

    Update solution for reverse-words-in-a-string-iii

--- leetcode/reverse-words-in-a-string-iii.py
+++ leetcode/reverse-words-in-a-string-iii.py
@@ -1,18 +1,17 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s_list = s.split(" ")
+        word_list = s.split(" ")
 
-        for idx in range(len(s_list)):
-            word = s_list[idx]
+        for idx in range(len(word_list)):
+            word = list(word_list[idx])
             left = 0
             right = len(word) - 1
-            iter_word = list(word)
 
-            while left < right:
-                iter_word[left], iter_word[right] = iter_word[right], iter_word[left]
+            while (left < right):
+                word[left], word[right] = word[right], word[left]
                 left += 1
                 right -= 1
-            print(iter_word)
-            s_list[idx] = "".join(iter_word)
+            
+            word_list[idx] = "".join(word)
         
-        return " ".join(s_list)
+        return " ".join(word_list)
