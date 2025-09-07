0afef2209a258c9f74bf5d78759dfc1043ee0476
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-07 16:23:55 -0400

    Update solution for 3sum

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -1,27 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # USED SOLN: https://www.youtube.com/watch?v=jzZsG8n2R9A
-
-        final = []
         nums.sort()
+        final = []
+        print(nums)
 
-        for i, a in enumerate(nums):
-            if (i > 0 and a == nums[i - 1]):
+        for idx in range(len(nums) - 1):
+
+            if (idx > 0 and nums[idx - 1] == nums[idx]):
                 continue
-            
-            left, right = i + 1, len(nums) - 1
-            while (left < right):
-                threeSum = a + nums[left] + nums[right]
 
-                if (threeSum > 0):
+            left = idx + 1
+            right = len(nums) - 1
+            cur_sum = 0
+            
+
+            while (left < right):
+                cur_sum = nums[idx] + nums[left] + nums[right]
+                
+                if (cur_sum > 0):
                     right -= 1
                 
-                elif (threeSum < 0):
+                elif (cur_sum < 0):
+                    left += 1
+                
+                else:
+                    final.append([nums[idx], nums[left], nums[right]])
                     left += 1
 
-                else:
-                    final.append([a, nums[left], nums[right]])
-                    left += 1
                     while (nums[left] == nums[left - 1] and left < right):
                         left += 1
         
