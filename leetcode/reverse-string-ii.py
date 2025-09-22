b752d1c28ee7bf6bf4fc03512c503d3e49368c4c
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-22 18:39:55 -0400

    Update solution for reverse-string-ii

--- leetcode/reverse-string-ii.py
+++ leetcode/reverse-string-ii.py
@@ -2,14 +2,15 @@ class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         s_list = list(s)
 
-        for idx in range(0, len(s), 2*k):
-            left = idx 
+        for idx in range(0, len(s) - 1, 2 * k):
+            left = idx
             right = idx + k - 1
 
-            if(right > len(s) - 1):
+            if (right >= len(s)):
                 right = len(s) - 1
 
-            while(left < right):
+            while left < right:
+                print(f"Left: {left} | Right: {right}")
                 s_list[left], s_list[right] = s_list[right], s_list[left]
                 left += 1
                 right -= 1
