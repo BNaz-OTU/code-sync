eeed88e1b6b8b88dd6a530a5201fe5bf38701a2d
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-25 15:27:21 -0400

    Update solution for shortest-distance-to-a-character

--- leetcode/shortest-distance-to-a-character.py
+++ leetcode/shortest-distance-to-a-character.py
@@ -1,33 +1,33 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        fast = 0 
-        slow = None
-        new_list = []
-        stack = []
+        final = []
+        slow = 0 
+        fast = 0
+        let_found = None
 
         while fast < len(s):
             if (s[fast] == c):
-                stack.append(fast)
-                temp = stack[::-1]
-
-                while len(temp) > 0:
-                    if (slow is None):
-                        new_list.append(fast - temp.pop())
+                # print(f"FOUND {c}")
+                while slow <= fast:
+                    if (let_found is None):
+                        final.append(fast - slow)
+                
                     else:
-                        curr_idx = temp.pop()
-                        dist = min(abs(curr_idx - slow), abs(fast - curr_idx))
-                        new_list.append(dist)
+                        small = min(abs(fast - slow), abs(let_found - slow))
+                        final.append(small)
 
-                stack = [] 
-                slow = fast
+                    slow += 1
+
+                let_found = fast
+                fast += 1
 
             else:
-                stack.append(fast)
-            
-            fast += 1
+                fast += 1
+
+        if (len(final) != len(s)):
+            count = 1
+            while len(final) < len(s):
+                final.append(count)
+                count += 1
         
-        if (len(stack) > 0):
-            for val in stack:
-                new_list.append(val - slow)
-        
-        return new_list
+        return final
