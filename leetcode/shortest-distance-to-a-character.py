33a4f2348945f2a00cee658eba44bfa87bff9b38
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-09-25 15:44:59 -0400

    Update solution for shortest-distance-to-a-character

--- leetcode/shortest-distance-to-a-character.py
+++ leetcode/shortest-distance-to-a-character.py
@@ -5,6 +5,18 @@ class Solution:
         fast = 0
         let_found = None
 
+        # Two Pointer Approach:
+        # One pointer will go fast, while another pointer will go slow. The fast
+        # pointer will be responsible for searching for the 'specific character'
+        # while the slow character will keep track how far apart it is from the 'specific 
+        # character' when it is found. When it locates the first instance of the 'specific 
+        # character', it will mark its, location in 'let_found', for now the second loop
+        # would only calculate the distance between, the first instance of the 'specific
+        # character'. Once the 'fast pointer' locates another 'specific character', then
+        # it will begin comparing the distance between the current 'specific character'
+        # and the previous 'specific character' to determine which one is close given the current
+        # position of the 'slow pointer/index'.
+
         while fast < len(s):
             if (s[fast] == c):
                 # print(f"FOUND {c}")
@@ -24,6 +36,9 @@ class Solution:
             else:
                 fast += 1
 
+        # In the event that the 'specific character' doesn't show up at the end, the distance 
+        # will be calculated/incremented by 1 each time it goes further away from 
+        # the last seen 'specific character'
         if (len(final) != len(s)):
             count = 1
             while len(final) < len(s):
