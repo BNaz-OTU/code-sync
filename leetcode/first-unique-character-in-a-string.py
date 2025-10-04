592a5e89948bdc77942fb4adbad42be9866fbfb2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-10-04 12:48:58 -0400

    Update solution for first-unique-character-in-a-string

--- leetcode/first-unique-character-in-a-string.py
+++ leetcode/first-unique-character-in-a-string.py
@@ -1,28 +1,29 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        # dictHolder = {}
-        # found = set()
 
         found = {}
         minV = None
 
-
         for idx, val in enumerate(s):
             if (val in found):
-                found[val] = len(s)
+                found[val] = -1
             else:
                 found[val] = idx
         
         print(found)
 
         for key in found:
+
+            if (found[key] == -1):
+                continue
+
             if minV is None:
                 minV = found[key]
-            
+                
             else:
                 minV = min(minV, found[key])
         
-        if (minV == len(s)):
+        if (minV == None):
             return -1
         else:
             return minV
