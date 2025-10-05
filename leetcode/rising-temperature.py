3fabe2f77de158f66a7e041e954adafbbbcc6ef2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-10-05 12:55:21 -0400

    Add solution for rising-temperature

---
+++ leetcode/rising-temperature.py
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT W2.id AS "Id"
+FROM Weather AS W1
+JOIN Weather AS W2
+ON W1.recordDate = DATE_SUB(W2.recordDate, INTERVAL 1 DAY)
+WHERE W1.temperature < W2.temperature
