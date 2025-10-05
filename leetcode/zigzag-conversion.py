1ece78c9502002f0d85cf8621794cd3af1d0720f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-10-04 20:57:26 -0400

    Add solution for zigzag-conversion

---
+++ leetcode/zigzag-conversion.py
@@ -0,0 +1,27 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+
+        final = ""
+
+        if (numRows % 2 == 0):
+            for idx in range(numRows):
+                print(s[idx::numRows + 2])
+                final += s[idx::numRows + 2]
+        else:
+            for idx in range(numRows):
+                if (idx % 2 == 0):
+                    print(s[idx::numRows + 1])
+                    final += s[idx::numRows + 1]
+                else:
+                    print(s[idx::numRows - 1])
+                    final += s[idx::numRows - 1]
+        
+        return final
+
+
+        pass
+        # P Y A I H R N
+        # A P L S I I G
+
+        # Even Up by 2
+        # Odd up by 1
