05b9f22e0f67d7b150ed8d365ae1c825130c86d6
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-10-06 14:24:35 -0400

    Update solution for customer-placing-the-largest-number-of-orders

--- leetcode/customer-placing-the-largest-number-of-orders.py
+++ leetcode/customer-placing-the-largest-number-of-orders.py
@@ -1,6 +1,10 @@
-import pandas as pd
+# Write your MySQL query statement below
 
-def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
-    df = orders.groupby('customer_number').size().reset_index(name='count')
-    df1 = df.sort_values('count', ascending=False)
-    return df1.loc[:, ['customer_number']].head(1)
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
+
+-- SELECT *
+-- FROM T1
