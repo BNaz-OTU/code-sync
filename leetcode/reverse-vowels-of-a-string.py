d7c714bedb71c9d2ac59a85418f7c0968711fb17
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-01 12:54:55 -0400

    Update solution for reverse-vowels-of-a-string

--- leetcode/reverse-vowels-of-a-string.py
+++ leetcode/reverse-vowels-of-a-string.py
@@ -1,30 +1,21 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        """
-        Two Pointer Approach:
-        First establish a variable containing all the vowels, which will later be used to check if 
-        the character is a vowel. Setup a left and right pointer to traverse both sides of the string.
-        In the event that a character on both pointers is a vowel swap the characters, then continue 
-        incrementing the pointers to keep advancing thru the 'string'. Also, increment whenever a pointer 
-        isn't a vowel.
-        """
-
-        vowel = "aeiouAEIOU"
-        s_list = list(s)
+        vowels = "aeiouAEIOU"
+        list_s = list(s)
 
         left = 0
         right = len(s) - 1
 
         while left < right:
-            if (s_list[left] in vowel and s_list[right] in vowel):
-                s_list[left], s_list[right] = s_list[right], s_list[left]
+            if (list_s[left] in vowels and list_s[right] in vowels):
+                list_s[left], list_s[right] = list_s[right], list_s[left]
                 left += 1
                 right -= 1
             
-            if (s_list[right] not in vowel):
-                right -= 1
-            
-            if (s_list[left] not in vowel):
+            if (list_s[left] not in vowels):
                 left += 1
+            
+            if (list_s[right] not in vowels):
+                right -= 1
         
-        return "".join(s_list)
+        return "".join(list_s)
