921650f77faba317c843791f768a887643ef1704
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-07 20:12:47 -0500

    Update solution for project-employees-i

--- leetcode/project-employees-i.py
+++ leetcode/project-employees-i.py
@@ -1,9 +1,6 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT 
-    P.project_id, 
-    ROUND(AVG(E.experience_years), 2) AS "average_years"
-FROM Project AS P
-JOIN Employee AS E
-ON P.employee_id = E.employee_id
-GROUP BY P.project_id
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    df = project.join(employee.set_index('employee_id'), on='employee_id', how='inner')
+    df2 = df.groupby('project_id')['experience_years'].mean().round(2).reset_index()
+    return df2.rename(columns={'experience_years' : 'average_years'})
