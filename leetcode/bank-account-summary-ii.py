8e66ddcb6d5770be1144217ab87acf4dd683497b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-11 06:15:19 -0500

    Update solution for bank-account-summary-ii

--- leetcode/bank-account-summary-ii.py
+++ leetcode/bank-account-summary-ii.py
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT U.name AS "NAME", SUM(amount) AS "BALANCE"
-FROM Users AS U
-JOIN Transactions AS T
-ON U.account = T.account
-GROUP BY U.account
-HAVING SUM(amount) > 10000
+def account_summary(users: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    df = users.join(transactions.set_index('account'), on='account', how='inner')
+    df2 = df.groupby(['account', 'name'])['amount'].sum().reset_index()
+    return df2.loc[
+        df2['amount'] > 10000, ['name', 'amount']
+    ].rename(columns={'name' : 'NAME', 'amount' : 'BALANCE'})
