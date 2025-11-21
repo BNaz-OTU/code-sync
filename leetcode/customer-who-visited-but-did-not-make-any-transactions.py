3bb72d4e72730b31760dc5753e9fcbb5b1d5431b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-21 10:47:34 -0500

    Update solution for customer-who-visited-but-did-not-make-any-transactions

--- leetcode/customer-who-visited-but-did-not-make-any-transactions.py
+++ leetcode/customer-who-visited-but-did-not-make-any-transactions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
-    df1 = visits.join(transactions.set_index('visit_id'), on='visit_id', how='left')
-    df2 = df1.loc[df1['transaction_id'].isnull(), ['customer_id', 'transaction_id']]
-    return df2.groupby('customer_id').size().reset_index(name='count_no_trans')
+    df = visits.join(transactions.set_index('visit_id'), on='visit_id', how='left')
+    no_visit_df = df.loc[df['transaction_id'].isnull()]
+    return no_visit_df.groupby('customer_id')['transaction_id'].size().reset_index(name="count_no_trans")
