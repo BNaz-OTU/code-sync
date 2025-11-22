a3f101be74e271ca018794228b04830654a949a7
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-22 10:16:13 -0500

    Update solution for queries-quality-and-percentage

--- leetcode/queries-quality-and-percentage.py
+++ leetcode/queries-quality-and-percentage.py
@@ -1,12 +1,8 @@
-import pandas as pd
+# Write your MySQL query statement below
 
-round2 = lambda x: round(x + 1e-9, 2)
-
-def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
-    
-    queries['quality'] = queries.rating / queries.position
-    queries['poor_query_percentage'] = (queries.rating < 3) * 100
-
-    group = queries.groupby('query_name')[['quality', 'poor_query_percentage']].mean().apply(round2).reset_index()
-
-    return group
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) AS "quality",
+    ROUND(AVG(rating < 3) * 100, 2) AS "poor_query_percentage"
+FROM Queries
+GROUP BY query_name
