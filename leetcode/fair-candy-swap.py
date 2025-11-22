677bf6e0bb07ee876853c9766fdebc21a6d2680a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-21 20:41:45 -0500

    Update solution for fair-candy-swap

--- leetcode/fair-candy-swap.py
+++ leetcode/fair-candy-swap.py
@@ -1,30 +1,32 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        # sort_Alice = sorted(aliceSizes)
+        sort_Bob = sorted(bobSizes)
         sumAlice = sum(aliceSizes)
         sumBob = sum(bobSizes)
 
         target = (sumAlice + sumBob) // 2
 
-        print(f"Alice Sum: {sumAlice} | Bob Sum: {sumBob}")
+        # print(f"Alice Sum: {sumAlice} | Bob Sum: {sumBob}")
 
         # Calculate for Alice swapping with Bob
         for val in aliceSizes:
             bob_target = target - (sumAlice - val)
 
-            if (bob_target in bobSizes):
-                return [val, bob_target]
+            # if (bob_target in bobSizes):
+            #     return [val, bob_target]
 
-            # left = 0
-            # right = len(bobSizes) - 1
+            left = 0
+            right = len(bobSizes) - 1
 
-            # while (left <= right):
-            #     middle = (left + right) // 2
+            while (left <= right):
+                middle = (left + right) // 2
 
-            #     if (bobSizes[middle] == bob_target):
-            #         return [val, bobSizes[middle]]
+                if (sort_Bob[middle] == bob_target):
+                    return [val, sort_Bob[middle]]
                 
-            #     elif (bobSizes[middle] < bob_target):
-            #         left = middle + 1
+                elif (sort_Bob[middle] < bob_target):
+                    left = middle + 1
                 
-            #     else:
-            #         right = middle - 1
+                else:
+                    right = middle - 1
