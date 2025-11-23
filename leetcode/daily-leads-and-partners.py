78b9486a76df668123bef959b3ce2a8d3c07ec4d
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-23 13:48:47 -0500

    Update solution for daily-leads-and-partners

--- leetcode/daily-leads-and-partners.py
+++ leetcode/daily-leads-and-partners.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
     return daily_sales.groupby(['date_id', 'make_name']).agg(
-        unique_leads = pd.NamedAgg(column="lead_id", aggfunc="nunique"),
-        unique_partners = pd.NamedAgg(column="partner_id", aggfunc="nunique")
+        unique_leads = ('lead_id', 'nunique'),
+        unique_partners = ('partner_id', 'nunique')
     ).reset_index()
