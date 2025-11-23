a95d946d3f478251716f79080fa1e7c07f7d9806
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-23 17:02:06 -0500

    Update solution for intersection-of-two-linked-lists

--- leetcode/intersection-of-two-linked-lists.py
+++ leetcode/intersection-of-two-linked-lists.py
@@ -6,18 +6,40 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        set_link = set()
 
+        # USED REFERENCE: https://www.youtube.com/watch?v=D0X0BONOQhI
+
+        # Declare Variables
+        tempA = headA
+        tempB = headB
+        lengthA = 0
+        lengthB = 0
+
+        # 1. Determine the Length of each Linked List
         while headA:
-            if (headA not in set_link):
-                set_link.add(headA)
-            
+            lengthA += 1
             headA = headA.next
         
         while headB:
-            if (headB in set_link):
-                return headB
-
+            lengthB += 1
             headB = headB.next
+
+        # 2. If they have different lengths update the linked list, so that they 
+        # both have the same starting point
+        while (lengthA > lengthB):
+            tempA = tempA.next
+            lengthA -= 1
+        
+        while (lengthB > lengthA):
+            tempB = tempB.next
+            lengthB -= 1
+        
+        # 3. Find the Intersecting Node
+        while tempA:
+            if (tempA == tempB):
+                return tempA
+            
+            tempA = tempA.next
+            tempB = tempB.next
 
         return None
