2cca7485fa06873a925b04b5b9b88ae8c9cb2883
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-25 20:07:18 -0500

    Update solution for intersection-of-two-linked-lists

--- leetcode/intersection-of-two-linked-lists.py
+++ leetcode/intersection-of-two-linked-lists.py
@@ -6,40 +6,32 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-
-        # USED REFERENCE: https://www.youtube.com/watch?v=D0X0BONOQhI
-
-        # Declare Variables
         tempA = headA
         tempB = headB
-        lengthA = 0
-        lengthB = 0
+        countA = 0
+        countB = 0
 
-        # 1. Determine the Length of each Linked List
-        while headA:
-            lengthA += 1
-            headA = headA.next
-        
-        while headB:
-            lengthB += 1
-            headB = headB.next
-
-        # 2. If they have different lengths update the linked list, so that they 
-        # both have the same starting point
-        while (lengthA > lengthB):
-            tempA = tempA.next
-            lengthA -= 1
-        
-        while (lengthB > lengthA):
-            tempB = tempB.next
-            lengthB -= 1
-        
-        # 3. Find the Intersecting Node
         while tempA:
-            if (tempA == tempB):
-                return tempA
-            
             tempA = tempA.next
+            countA += 1
+        
+        while tempB:
             tempB = tempB.next
+            countB += 1
+        
+        while countA > countB:
+            headA = headA.next
+            countA -= 1
+        
+        while countB > countA:
+            headB = headB.next
+            countB -= 1
+        
+        while headA:
+            if (headA == headB):
+                return headA
+            
+            headA = headA.next
+            headB = headB.next
 
         return None
