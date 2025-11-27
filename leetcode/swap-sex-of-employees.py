c59c0990c9329817456de744fd2cb74a06628e9e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-26 20:22:37 -0500

    Update solution for swap-sex-of-employees

--- leetcode/swap-sex-of-employees.py
+++ leetcode/swap-sex-of-employees.py
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-UPDATE Salary
-SET sex = IF(sex="m", "f", "m")
+def swap_salary(salary: pd.DataFrame) -> pd.DataFrame:
+    swap = lambda x: "f" if x == "m" else "m"
+    salary["sex"] = salary.sex.apply(swap)
+    return salary
