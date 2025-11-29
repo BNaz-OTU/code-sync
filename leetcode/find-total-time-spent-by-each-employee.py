dab518c05679eb7cc38815cd6733074d8a6a2c88
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-29 15:13:19 -0500

    Update solution for find-total-time-spent-by-each-employee

--- leetcode/find-total-time-spent-by-each-employee.py
+++ leetcode/find-total-time-spent-by-each-employee.py
@@ -1,11 +1,8 @@
-import pandas as pd
+# Write your MySQL query statement below
 
-def total_time(employees: pd.DataFrame) -> pd.DataFrame:
-    total_df = employees.groupby(['emp_id', 'event_day']).agg(
-        total_in = ('in_time', 'sum'),
-        total_out = ('out_time', 'sum')
-    ).reset_index()
-
-    total_df['total_time'] = total_df['total_out'] - total_df['total_in']
-
-    return total_df.loc[:, ['event_day', 'emp_id', 'total_time']].rename(columns={'event_day' : 'day'})
+SELECT 
+    event_day AS "day",
+    emp_id,
+    SUM(out_time - in_time) AS "total_time"
+FROM Employees
+GROUP BY emp_id, event_day
