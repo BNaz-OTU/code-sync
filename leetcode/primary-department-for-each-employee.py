8f09815bea0e65724f99177d97710b01d15b5a33
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-11-29 15:51:35 -0500

    Update solution for primary-department-for-each-employee

--- leetcode/primary-department-for-each-employee.py
+++ leetcode/primary-department-for-each-employee.py
@@ -1,12 +1,12 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT employee_id, department_id
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(*) = 1
+def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
+    # Has multiple departments
+    df1 = employee.loc[employee["primary_flag"] == "Y", ["employee_id", "department_id"]]
+    
+    # Only work 1 department
+    df2 = employee.groupby("employee_id").size().reset_index(name="count")
+    df3 = employee.join(df2.set_index("employee_id"), on="employee_id", how="inner")
+    df4 = df3.loc[df3["count"] == 1, ["employee_id", "department_id"]]
 
-UNION
-
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
+    return pd.concat([df1, df4])
