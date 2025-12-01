9e0d5836a9a7e1e887cfdd15670dc2085936cf13
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-01 18:55:41 -0500

    Update solution for shuffle-the-array

--- leetcode/shuffle-the-array.py
+++ leetcode/shuffle-the-array.py
@@ -1,9 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        ans = []
+        ans = [0] * (2 * n)
 
         for idx in range(n):
-            ans.append(nums[idx])
-            ans.append(nums[idx + n])
+            ans[idx * 2] = nums[idx]
+            ans[2 * idx + 1] = nums[idx + n]
+            # ans.append(nums[idx])
+            # ans.append(nums[idx + n])
         
         return ans
