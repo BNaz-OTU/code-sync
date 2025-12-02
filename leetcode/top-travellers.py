0e6dfb5caa521eb588d23893668abaf0aac838b3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-01 19:11:36 -0500

    Update solution for top-travellers

--- leetcode/top-travellers.py
+++ leetcode/top-travellers.py
@@ -1,7 +1,10 @@
-import pandas as pd
+# Write your MySQL query statement below
 
-def top_travellers(users: pd.DataFrame, rides: pd.DataFrame) -> pd.DataFrame:
-    df = users.join(rides.set_index('user_id'), on='id', how='left', lsuffix='_c')
-    df2 = df.groupby('id_c')['distance'].sum().reset_index(name='travelled_distance')
-    df3 = df2.join(users.set_index('id'), on='id_c')
-    return df3[['name', 'travelled_distance']].sort_values(by=['travelled_distance', 'name'], ascending=[False, True])
+SELECT 
+    U.name,
+    SUM(IF(R.distance IS NULL, 0, R.distance)) AS "travelled_distance"
+FROM Users AS U
+LEFT JOIN Rides AS R
+ON U.id = R.user_id
+GROUP BY U.id
+ORDER BY travelled_distance DESC, name ASC
