be60afe6f253dfeed699ff3b09bb1221eb590669
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-04 20:35:30 -0500

    Add solution for the-number-of-employees-which-report-to-each-employee

---
+++ leetcode/the-number-of-employees-which-report-to-each-employee.py
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT 
+    E2.employee_id,
+    E2.name,
+    COUNT(*) AS "reports_count",
+    ROUND(AVG(E1.age), 0) AS "average_age"
+FROM Employees AS E1
+LEFT JOIN Employees AS E2
+ON E1.reports_to = E2.employee_id 
+WHERE E1.reports_to IS NOT NULL
+GROUP BY E2.employee_id
+ORDER BY E2.employee_id
