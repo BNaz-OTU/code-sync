af9254e9a7f497a1c0557faa272f9e3115ba26f0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-06 15:34:08 -0500

    Update solution for insert-into-a-binary-search-tree

--- leetcode/insert-into-a-binary-search-tree.py
+++ leetcode/insert-into-a-binary-search-tree.py
@@ -6,14 +6,32 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        # curr = root
-
-        if (root is None):
+        # Edge Case when the given tree is empty
+        if root is None:
             return TreeNode(val)
+
+        curr = root
+
+        # Remembers the last connection
+        prev = curr
+
+        # Keep looping unitl we find the correct Node to do the insertion
+        while (curr is not None):
+            # Update the last connection
+            prev = curr
+
+            if (val < curr.val):
+                # print(curr.val) # DEBUG
+                curr = curr.left
+
+            else:
+                # print(curr.val) # DEBUG
+                curr = curr.right
         
-        if (val < root.val):
-            root.left = self.insertIntoBST(root.left, val)
+        # After leaving the loop use the last connection to determine where to place the new value
+        if (val < prev.val):
+            prev.left = TreeNode(val)
         else:
-            root.right = self.insertIntoBST(root.right, val)
+            prev.right = TreeNode(val)
         
         return root
