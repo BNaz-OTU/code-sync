149ebf5b0edf1f1eeeb81dc48bb4341421fd9760
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-05 20:37:27 -0500

    Add solution for sales-analysis-iii

---
+++ leetcode/sales-analysis-iii.py
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT P.product_id, P.product_name
+FROM Product AS P
+JOIN Sales AS S
+ON P.product_id = S.product_id
+GROUP BY P.product_id
+HAVING (MAX(S.sale_date) BETWEEN "2019-01-01" AND "2019-03-31") AND 
+    (MIN(S.sale_date) BETWEEN "2019-01-01" AND "2019-03-31")
