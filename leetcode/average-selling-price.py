5b657c8869ed5038017ea62f67974b3ed63d7a6f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-07 14:44:00 -0500

    Update solution for average-selling-price

--- leetcode/average-selling-price.py
+++ leetcode/average-selling-price.py
@@ -1,9 +1,15 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT 
-    P.product_id,
-    IFNULL(ROUND(SUM(P.price * U.units) / SUM(U.units), 2), 0) AS "average_price"
-FROM Prices AS P
-LEFT JOIN UnitsSold AS U
-ON (U.purchase_date BETWEEN P.start_date AND P.end_date) AND (P.product_id = U.product_id)
-GROUP BY P.product_id
+def average_selling_price(prices: pd.DataFrame, units_sold: pd.DataFrame) -> pd.DataFrame:
+    df = prices.join(units_sold.set_index("product_id"), on="product_id", how="inner")
+    df2 = df.loc[(df["start_date"] <= df["purchase_date"]) & (df["purchase_date"] <= df["end_date"])]
+    df2["revenue"] = df2["price"] * df2["units"]
+    df3 = df2.groupby("product_id").agg(
+        total_units = ("units", "sum"),
+        total_revenue = ("revenue", "sum")
+    ).reset_index()
+
+    df3["average_price"] = round(df3["total_revenue"] / df3["total_units"], 2)
+
+    df4 = prices.join(df3.set_index("product_id"), on="product_id", how="left").fillna(0)
+    return df4[["product_id", "average_price"]].drop_duplicates()
