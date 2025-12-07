eae2eff566d1da1eab3a4f1e4315abffffca2fcf
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-07 14:09:39 -0500

    Update solution for the-number-of-employees-which-report-to-each-employee

--- leetcode/the-number-of-employees-which-report-to-each-employee.py
+++ leetcode/the-number-of-employees-which-report-to-each-employee.py
@@ -1,12 +1,18 @@
-# Write your MySQL query statement below
+import pandas as pd
 
-SELECT 
-    MGR.employee_id,
-    MGR.name,
-    COUNT(*) AS "reports_count",
-    ROUND(AVG(E1.age), 0) AS "average_age"
-FROM Employees AS E1
-JOIN Employees AS MGR
-ON E1.reports_to = MGR.employee_id
-GROUP BY MGR.employee_id
-ORDER BY MGR.employee_id
+def round2(num):
+    if (num % 1 >= 0.5):
+        return math.ceil(num)
+    else:
+        return math.floor(num)
+
+def count_employees(employees: pd.DataFrame) -> pd.DataFrame:
+    df = employees.join(employees.set_index("employee_id"), on="reports_to", how="inner", rsuffix="_o")
+    df2 = df.groupby(["reports_to", "name_o"]).agg(
+        reports_count = ("employee_id", "size"),
+        average_age = ("age", "mean")
+    ).reset_index()
+
+    df2["average_age"] = df2["average_age"].apply(round2)
+
+    return df2.rename(columns={"reports_to" : "employee_id", "name_o" : "name"}).sort_values("employee_id")
