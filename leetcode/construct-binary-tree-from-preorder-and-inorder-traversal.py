d38bdc22995d997b4eccda46b7ff7f87cd8ebbb8
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-08 09:51:46 -0500

    Update solution for construct-binary-tree-from-preorder-and-inorder-traversal

--- leetcode/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ leetcode/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,40 +6,14 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        set_check_pre = set()
-        set_check_ord = set()
 
-        # Start with checking the preorder, since that always shows the root
+        # USED SOLN: https://www.youtube.com/watch?v=ihj4IQGZ2zc
 
+        if (not preorder or not inorder):
+            return None
         root = TreeNode(preorder[0])
-        curr = root
-        final = root
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
 
-        # After creating the root node, add the node to a set so that I know when
-        # to stop iterating through the inorder
-        set_check_pre.add(preorder[0])
-
-        idx = 0
-        for idx in range(len(inorder)):
-            set_check_ord.add(inorder[idx])
-            if (inorder[idx] in set_check_pre):
-                break
-        
-        new_list = inorder[:idx]
-        new_list = new_list[::-1]
-        updated_inorder = inorder[idx + 1:]
-        print(updated_inorder)
-
-        return
-        print(new_list)
-        # Create left branch of the tree
-        for val in newlist:
-            curr.left = TreeNode(val)
-            curr = curr.left
-        
-        # Iterate through the preorder list to find the right tree ignore values found in left 
-        # using a set of already found values from inorder list
-        root = root.right
-        curr = root
-
-        # for val in
+        return root
