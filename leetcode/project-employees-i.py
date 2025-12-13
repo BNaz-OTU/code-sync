c637377b4136df04fe46eb35d1ea8f163a794f22
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-13 12:46:33 -0500

    Update solution for project-employees-i

--- leetcode/project-employees-i.py
+++ leetcode/project-employees-i.py
@@ -2,7 +2,7 @@
 
 SELECT 
     P.project_id,
-    ROUND(SUM(E.experience_years) / COUNT(*), 2) AS "average_years"
+    ROUND(AVG(E.experience_years), 2) AS "average_years"
 FROM Project AS P
 JOIN Employee AS E
 ON P.employee_id = E.employee_id
