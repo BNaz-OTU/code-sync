32209de38d303d458fc5f255bd82973d5d1fa0d4
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-13 16:38:07 -0500

    Update solution for construct-binary-tree-from-preorder-and-inorder-traversal

--- leetcode/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ leetcode/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -9,11 +9,11 @@ class Solution:
 
         # USED SOLN: https://www.youtube.com/watch?v=ihj4IQGZ2zc
 
-        if (not preorder or not inorder):
+        if not preorder or not inorder:
             return None
+        
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
         root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
-
         return root
