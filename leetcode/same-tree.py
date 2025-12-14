2df107a727f1667f16f1440b3e03410bfd422c5e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-14 12:38:36 -0500

    Update solution for same-tree

--- leetcode/same-tree.py
+++ leetcode/same-tree.py
@@ -7,10 +7,9 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 
-        # USED SOLN: https://www.youtube.com/watch?v=vRbbcKXCxOw
-
         if (p is None and q is None):
             return True
+            
         
         if (p is None or q is None or p.val != q.val):
             return False
