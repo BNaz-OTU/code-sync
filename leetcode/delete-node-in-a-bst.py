8e2c2e3cec18ebdfb4cdc28962c69370c79bdbbc
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-15 09:48:18 -0500

    Update solution for delete-node-in-a-bst

--- leetcode/delete-node-in-a-bst.py
+++ leetcode/delete-node-in-a-bst.py
@@ -5,30 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minimumNode(self, root):
-        curr = root
-        while (curr and curr.left):
-            curr = curr.left
-        
-        return curr
-
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if (root is None):
-            return None
-        
+        if root is None:
+            return root
+
+        # print(root.val, key)
         if (key < root.val):
             root.left = self.deleteNode(root.left, key)
+        
         elif (key > root.val):
             root.right = self.deleteNode(root.right, key)
+        
         else:
-            print(root)
             if (root.right is None):
                 return root.left
+
             elif (root.left is None):
                 return root.right
-            else:
-                newKey = self.minimumNode(root.right)
-                root.val = newKey.val
-                root.right = self.deleteNode(root.right, newKey.val)
 
+            else:
+                minimumNode = self.minimum(root.right)
+                root.val = minimumNode.val
+                # print(root)
+                # print(root.right)
+                # print("Second Loop")
+                root.right = self.deleteNode(root.right, minimumNode.val)
+                return root
+        
         return root
+        
+    def minimum(self, root):
+        root = root
+        while root and root.left:
+            root = root.left
+        return root
+    
+    # self.minimum(root)
