786c3b4391a39f4ab11e67dbed2ba15b768b4d0d
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2025-12-17 08:38:59 -0500

    Update solution for kth-smallest-element-in-a-bst

--- leetcode/kth-smallest-element-in-a-bst.py
+++ leetcode/kth-smallest-element-in-a-bst.py
@@ -6,25 +6,16 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        a_list = []
 
-        # USED SOLN (#2 InOrder Traversal): https://neetcode.io/solutions/kth-smallest-element-in-a-bst
-
-        # USED FOR REFERENCE: https://www.youtube.com/watch?v=5LUXSvjmGCw 
-
-        final = []
-        # found = []
-
-        def inOrder(root):
-            if (root is None):
+        def dfs(root):
+            if root is None:
                 return
-            
-            inOrder(root.left)
-            final.append(root.val)
-            # print(f"Val: {root.val} | Length of List: {len(final)} | List: {final}")
-            # if (len(final) == k):
-            #     found.append(root.val)
-            
-            inOrder(root.right)
+
+            dfs(root.left)
+            a_list.append(root.val)
+            dfs(root.right)
         
-        inOrder(root)
-        return final[k - 1]
+        dfs(root)
+        
+        return a_list[k - 1]
