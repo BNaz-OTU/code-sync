55ff2737cbc237a53b584e81be58c50bb9ebb0d0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-01-16 14:50:05 -0500

    Add solution for count-good-nodes-in-binary-tree

---
+++ leetcode/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        countGoodNodes = 0
+        arr = []
+
+        def dfs(root):
+            if (root is None):
+                return
+            
+            nonlocal arr, countGoodNodes
+
+            arr.append(root.val)
+            # print(arr, max(arr))
+
+            if (max(arr) <= root.val):
+                countGoodNodes += 1
+
+            dfs(root.left)
+            dfs(root.right)
+            arr.pop()
+        
+        dfs(root)
+        return countGoodNodes
