6dd27818bcca82640f26ff25867bcf5d544f4090
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-01-27 20:42:11 -0500

    Update solution for employees-earning-more-than-their-managers

--- leetcode/employees-earning-more-than-their-managers.py
+++ leetcode/employees-earning-more-than-their-managers.py
@@ -1,5 +1,8 @@
-import pandas as pd
+# Write your MySQL query statement below
 
-def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    df = employee.join(employee.set_index('id'), on='managerId', how='inner', lsuffix="_emp", rsuffix="_mgr")
-    return df.loc[(df['salary_emp'] > df['salary_mgr']), ['name_emp']].rename(columns={'name_emp' : 'Employee'})
+SELECT 
+    EMP.name AS "Employee"
+FROM Employee AS EMP
+LEFT JOIN Employee AS MGR
+ON EMP.managerId = MGR.id
+WHERE EMP.salary > MGR.salary
