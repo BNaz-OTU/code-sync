89a5de74d59ddf81e55b0e8242583f67e89be7a2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-01-30 11:44:07 -0500

    Add solution for confirmation-rate

---
+++ leetcode/confirmation-rate.py
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT
+    S.user_id,
+    ROUND(SUM(IF(C.action = "confirmed", 1, 0)) / COUNT(*), 2) AS "confirmation_rate"
+FROM Signups AS S
+LEFT JOIN Confirmations AS C
+ON S.user_id = C.user_id
+GROUP BY S.user_id
