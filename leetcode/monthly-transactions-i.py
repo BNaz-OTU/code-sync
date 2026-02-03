f4b851ac645be3fb8cf9e2a823a741094fe7a3f0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-02 21:17:15 -0500

    Add solution for monthly-transactions-i

---
+++ leetcode/monthly-transactions-i.py
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT 
+    CONCAT(YEAR(trans_date), "-", DATE_FORMAT(trans_date, '%m')) AS "month",
+    country,
+    COUNT(*) AS "trans_count",
+    SUM(IF(state = "approved", 1, 0)) AS "approved_count",
+    SUM(amount) AS "trans_total_amount",
+    SUM(IF(state = "approved", amount, 0)) AS "approved_total_amount"
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y %M'), country
