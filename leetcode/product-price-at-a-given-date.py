e3370febae5611bd513b0494a7c2fc6343118989
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-07 14:50:46 -0500

    Update solution for product-price-at-a-given-date

--- leetcode/product-price-at-a-given-date.py
+++ leetcode/product-price-at-a-given-date.py
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
 
+-- Get the products that changed price before 2019-08-16 and find the closest date to 2019-08-16
+
+-- USED FOR REF: https://www.youtube.com/watch?v=a7xb-Q6WoW8
+
 WITH T1 AS (
     SELECT 
         product_id,
@@ -7,9 +11,10 @@ WITH T1 AS (
     FROM Products
     WHERE change_date <= "2019-08-16"
     GROUP BY product_id
-    HAVING MAX(change_date)
 )
 
+-- From T1, use its data to join with the exisiting table to locate the most recent price change before or
+-- on the day of the price change date (2019-08-16)
 SELECT 
     P.product_id,
     P.new_price AS "price"
@@ -19,6 +24,7 @@ ON P.product_id = T1.product_id AND T1.date = P.change_date
 
 UNION 
 
+-- When a product didn't change price until after 2019-08-16
 SELECT 
     product_id,
     10 AS "price"
