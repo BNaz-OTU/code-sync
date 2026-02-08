f67786c93c4ee92faeab60508fbc0788cebfa48f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-08 14:27:25 -0500

    Add solution for sort-characters-by-frequency

---
+++ leetcode/sort-characters-by-frequency.py
@@ -0,0 +1,24 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        dictChar = {}
+        helperList = []
+        final = ""
+
+        for char in s:
+            if (char in dictChar):
+                dictChar[char] += 1
+            else:
+                dictChar[char] = 1
+        
+        print(dictChar)
+
+        for key, val in dictChar.items():
+            helperList.append([val, key])
+
+        heapify(helperList)
+        while len(helperList) > 0:
+            val = heappop(helperList)
+            newStr = val[1] * val[0]
+            final += newStr
+        
+        return final[::-1]
