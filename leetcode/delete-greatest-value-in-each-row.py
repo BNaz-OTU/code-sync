f962863ae110d3e6e503fe68760454bb0fb68eca
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-08 12:04:02 -0500

    Update solution for delete-greatest-value-in-each-row

--- leetcode/delete-greatest-value-in-each-row.py
+++ leetcode/delete-greatest-value-in-each-row.py
@@ -1,21 +1,15 @@
 class Solution:
     def deleteGreatestValue(self, grid: List[List[int]]) -> int:
-        final = 0
-
+        finalsum = 0
         for idx in range(len(grid)):
-            for jdx in range(len(grid[idx])):
-                grid[idx][jdx] = grid[idx][jdx] * -1
-        
-        print(grid)
-
-        while len(grid[0]) > 0:
-            deleteList = []
-            for idx in range(len(grid)):
-                # arr = grid[idx]
-                heapify(grid[idx])
-                val = heappop(grid[idx])
-                deleteList.append(val)
+            for kdx in range(len(grid[idx])):
+                grid[idx][kdx] = grid[idx][kdx] * -1
                 
-            final += min(deleteList)
+        while (len(grid[0]) > 0):
+            deleted = []
+            for idx in range(len(grid)):
+                heapify(grid[idx])
+                deleted.append(heappop(grid[idx]))
+            finalsum += min(deleted) * -1 
         
-        return final * -1
+        return finalsum
