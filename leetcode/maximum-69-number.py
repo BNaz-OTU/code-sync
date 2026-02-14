b4b12cc7a04ad3e8d8adf07dce4e560a862a5f4d
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-14 13:39:15 -0500

    Add solution for maximum-69-number

---
+++ leetcode/maximum-69-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        
+        str_69 = []
+        found = False
+        final = ""
+        for num in str(num):
+            str_69.append(num)
+
+        print(str_69)
+
+        for idx, val in enumerate(str_69):
+            if (val == "6" and found == False):
+                final += "9"
+                found = True
+            else:
+                final += val
+        
+        return int(final)
