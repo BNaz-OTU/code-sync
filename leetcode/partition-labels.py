1511f00fb623e94a962fab0523b8261c1d2498fe
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-16 16:32:42 -0500

    Add solution for partition-labels

---
+++ leetcode/partition-labels.py
@@ -0,0 +1,31 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        dictPart = {}
+        final = []
+
+        for idx in range(len(s)):
+            if (s[idx] in dictPart):
+                dictPart[s[idx]] = idx
+            
+            else:
+                dictPart[s[idx]] = idx
+
+        print(dictPart)
+
+        end = 0
+        size = 0
+        for idx in range(len(s)):
+            size += 1
+
+            if (end == 0):
+                end = dictPart[s[idx]] 
+
+            if (dictPart[s[idx]] > end):
+                end = dictPart[s[idx]]
+
+            if (idx == end):
+                final.append(size)
+                size = 0
+                end = 0
+        
+        return final
