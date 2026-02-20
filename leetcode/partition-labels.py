68014b860faf1ce58ea708f2c6b2ffb2f5dc8da2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-20 11:05:02 -0500

    Update solution for partition-labels

--- leetcode/partition-labels.py
+++ leetcode/partition-labels.py
@@ -1,33 +1,21 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # USED SOLN: https://www.youtube.com/watch?v=WQZdwxZJAhY
-
-        dictPart = {}
+        dictLetter = {}
         final = []
 
+        for idx, val in enumerate(s):
+            dictLetter[val] = idx
+        
+        current = 0
+        counter = 1
+        
         for idx in range(len(s)):
-            if (s[idx] in dictPart):
-                dictPart[s[idx]] = idx
+            current = max(current, dictLetter[s[idx]])
+
+            if (current == idx):
+                final.append(counter)
+                counter = 0
             
-            else:
-                dictPart[s[idx]] = idx
-
-        print(dictPart)
-
-        end = 0
-        size = 0
-        for idx in range(len(s)):
-            size += 1
-
-            if (end == 0):
-                end = dictPart[s[idx]] 
-
-            if (dictPart[s[idx]] > end):
-                end = dictPart[s[idx]]
-
-            if (idx == end):
-                final.append(size)
-                size = 0
-                end = 0
+            counter += 1
         
         return final
