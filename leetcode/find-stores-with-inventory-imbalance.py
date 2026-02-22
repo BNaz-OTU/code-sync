908e73372ad6ec3aa6f52b66a95ce5908cbfe8db
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-22 10:25:26 -0500

    Update solution for find-stores-with-inventory-imbalance

--- leetcode/find-stores-with-inventory-imbalance.py
+++ leetcode/find-stores-with-inventory-imbalance.py
@@ -1,31 +1,47 @@
-# Write your MySQL query statement below
-WITH T1 AS(
+-- Write your PostgreSQL query statement below
+-- USED SOLN: https://leetcode.com/problems/find-stores-with-inventory-imbalance/solutions/7004525/simple-solution-ez-to-understand-join-ro-uwih
+
+WITH T1 AS (
     SELECT 
-        *,
-        ROW_NUMBER() OVER (PARTITION BY store_id ORDER BY price DESC) AS "expensive",
-        ROW_NUMBER() OVER (PARTITION BY store_id ORDER BY price) AS "cheap"
-    FROM inventory
+        I.store_id,
+        I.product_name,
+        I.quantity,
+        MAX(I.price) AS "Max_Price",
+        MIN(I.price) AS "Min_Price"
+    FROM inventory AS I
+    GROUP BY I.store_id
+    HAVING COUNT(DISTINCT product_name) >= 3
 ),
 
 T2 AS (
     SELECT 
-        store_id,
-        MAX(CASE WHEN expensive = 1 THEN product_name ELSE NULL END) AS "most_exp_product",
-        MAX(CASE WHEN cheap = 1 THEN product_name ELSE NULL END) AS "cheapest_product",
-        SUM(CASE WHEN expensive = 1 THEN quantity ELSE NULL END) AS "exp_qty",
-        SUM(CASE WHEN cheap = 1 THEN quantity ELSE NULL END) AS "chp_qty"
+        I.store_id,
+        I.product_name,
+        I.quantity,
+        I.price
     FROM T1
-    GROUP BY store_id
-    HAVING COUNT(*) >= 3
+    JOIN inventory AS I 
+    ON T1.store_id = I.store_id AND (I.price = T1.Max_Price OR I.price = T1.Min_Price)
+),
+
+T3 AS (
+    SELECT 
+        Max_T.store_id,
+        Max_T.product_name AS "most_exp_product",
+        Max_T.quantity AS "exp_qty",
+        Min_T.product_name AS "cheapest_product",
+        Min_T.quantity AS "chp_qty"
+    FROM T2 AS Max_T
+    JOIN T2 AS Min_T
+    ON Max_T.store_id = Min_T.store_id AND Min_T.price < Max_T.price AND Max_T.quantity < Min_T.quantity
 )
 
 SELECT 
     S.*,
-    T2.most_exp_product,
-    cheapest_product,
-    ROUND(chp_qty / exp_qty, 2) AS "imbalance_ratio"
-FROM T2
+    T3.most_exp_product,
+    T3.cheapest_product,
+    ROUND(T3.chp_qty / T3.exp_qty, 2) AS "imbalance_ratio"
+FROM T3
 JOIN stores AS S
-ON T2.store_id = S.store_id
-WHERE exp_qty < chp_qty
-ORDER BY imbalance_ratio DESC, store_name
+ON T3.store_id = S.store_id
+ORDER BY imbalance_ratio DESC, S.store_name
