249b1d00ea1434b237f02d34baaed19b9d69920f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-21 21:41:00 -0500

    Update solution for merge-triplets-to-form-target-triplet

--- leetcode/merge-triplets-to-form-target-triplet.py
+++ leetcode/merge-triplets-to-form-target-triplet.py
@@ -1,31 +1,18 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        # ANOTHER SOLN: https://www.youtube.com/watch?v=kShkQLQZ9K4
+        x_col = []
+        y_col = []
+        z_col = []
 
-        x_col_arr = []
-        y_col_arr = []
-        z_col_arr = []
-
-        if (len(triplets) == 1 and triplets[0] == target):
-            return True
-
-        for idx in range(len(triplets)):
-            flag = False
-            for jdx in range(len(triplets[idx])):
-
-                if (triplets[idx][jdx] > target[jdx]):
-                    flag = True
-            
-            if (flag == False):
-                x_col_arr.append(triplets[idx][0])
-                y_col_arr.append(triplets[idx][1])
-                z_col_arr.append(triplets[idx][2])
+        for triplet in triplets:
+            if (triplet[0] > target[0] or triplet[1] > target[1] or triplet[2] > target[2]):
+                continue
+            else:
+                x_col.append(triplet[0])
+                y_col.append(triplet[1])
+                z_col.append(triplet[2])
         
-        if (len(x_col_arr) >= 2 and target[0] in x_col_arr and target[1] in y_col_arr and target[2] in z_col_arr):
+        if (target[0] in x_col and target[1] in y_col and target[2] in z_col):
             return True
         else:
             return False
-
-        print(f"X's: {x_col_arr}")
-        print(f"Y's: {y_col_arr}")
-        print(f"Z's: {z_col_arr}")
