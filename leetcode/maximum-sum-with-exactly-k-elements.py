bac56eec987624cd9dcb4bc9a1457d89b717e56e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-21 21:44:45 -0500

    Update solution for maximum-sum-with-exactly-k-elements

--- leetcode/maximum-sum-with-exactly-k-elements.py
+++ leetcode/maximum-sum-with-exactly-k-elements.py
@@ -1,11 +1,10 @@
 class Solution:
     def maximizeSum(self, nums: List[int], k: int) -> int:
-        
-        max_num = max(nums)
+        maxNum = max(nums)
         final = 0
 
         for _ in range(k):
-            final += max_num 
-            max_num += 1
+            final += maxNum
+            maxNum += 1
         
         return final
