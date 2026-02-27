5d7194d274c1e053ece485d96bac594930c72d41
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-27 07:57:17 -0500

    Update solution for consecutive-numbers

--- leetcode/consecutive-numbers.py
+++ leetcode/consecutive-numbers.py
@@ -1,15 +1,9 @@
 # Write your MySQL query statement below
--- ANOTHER SOLN: https://www.youtube.com/watch?v=smd03AqX5LA
 
-WITH T1 AS (
-SELECT 
-    *,
-    LAG(num, 1) OVER(ORDER BY id) AS "prev_1_step",
-    LAG(num, 2) OVER(ORDER BY id) AS "prev_2_step"
-FROM Logs
-)
-
-SELECT 
-    DISTINCT(num) AS "ConsecutiveNums"
-FROM T1
-WHERE (num = prev_1_step) AND (num = prev_2_step) AND (prev_1_step = prev_2_step)
+SELECT
+    DISTINCT(L1.num) AS "ConsecutiveNums"
+FROM Logs AS L1
+JOIN Logs AS L2
+ON L1.id = L2.id + 1 AND L1.num = L2.num
+JOIN Logs AS L3
+ON L1.id = L3.id + 2 AND L1.num = L3.num
