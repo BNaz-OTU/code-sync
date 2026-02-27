ff95877998c1130b4c2fc2fc4046bb2c3eeb944f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-27 08:54:59 -0500

    Update solution for subsets-ii

--- leetcode/subsets-ii.py
+++ leetcode/subsets-ii.py
@@ -1,25 +1,23 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # ANOTHER METHOD: https://www.youtube.com/watch?v=Vn2v6ajA7U0
-
         final = []
         subset = []
         nums.sort()
 
         def dfs(idx, subset):
-            if (idx >= len(nums) and subset not in final):
-                final.append(subset.copy())
-                return 
-
             if (idx >= len(nums)):
-                return 
+                final.append(subset.copy())
+                return
             
-            # Add Number
+            # Include the number
             subset.append(nums[idx])
             dfs(idx + 1, subset)
-
-            # Don't Add Number
             subset.pop()
+
+            while idx < len(nums) - 1 and nums[idx] == nums[idx + 1]:
+                idx += 1
+
+            # Exclude the number
             dfs(idx + 1, subset)
         
         dfs(0, subset)
