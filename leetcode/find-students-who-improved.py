7c2171a4efb1f46fc6d8c48fa525811a9fc856ee
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-02-27 07:53:01 -0500

    Update solution for find-students-who-improved

--- leetcode/find-students-who-improved.py
+++ leetcode/find-students-who-improved.py
@@ -1,23 +1,22 @@
 # Write your MySQL query statement below
 WITH T1 AS (
-    SELECT
+    SELECT 
         *,
-        -- ROW_NUMBER() OVER(PARTITION BY student_id, subject ORDER BY score) AS "test_rnk",
-        ROW_NUMBER() OVER(PARTITION BY student_id, subject ORDER BY exam_date) AS "date_rnk_first",
-        ROW_NUMBER() OVER(PARTITION BY student_id, subject ORDER BY exam_date DESC) AS "date_rnk_latest"
+        ROW_NUMBER() OVER(PARTITION BY student_id, subject ORDER BY exam_date DESC) AS "latest_date",
+        ROW_NUMBER() OVER(PARTITION BY student_id, subject ORDER BY exam_date) AS "first_date"
     FROM Scores
 ),
 
 T2 AS (
     SELECT 
-        student_id, 
+        student_id,
         subject,
-        MAX(CASE WHEN date_rnk_first = 1 THEN score ELSE NULL END) AS "first_score",
-        MAX(CASE WHEN date_rnk_latest = 1 THEN score ELSE NULL END) AS "latest_score"
+        MAX(CASE WHEN first_date = 1 THEN score ELSE NULL END) AS "first_score",
+        MAX(CASE WHEN latest_date = 1 THEN score ELSE NULL END) AS "latest_score"
     FROM T1
     GROUP BY student_id, subject
     HAVING COUNT(*) > 1
-    )
+)
 
 SELECT *
 FROM T2
