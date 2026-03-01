794f0850ca83c09dd008adfc235b6714dc72063b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-01 16:07:10 -0500

    Update solution for number-of-islands

--- leetcode/number-of-islands.py
+++ leetcode/number-of-islands.py
@@ -1,34 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        counter = 0
-        visited = set()
         ROWS = len(grid)
         COLS = len(grid[0])
+        visited = set()
+        counter = 0
 
         def dfs(row, col, visited):
-            if (
-                (row < 0 or col < 0) or
-                (row >= ROWS or col >= COLS) or
+            if ((row < 0 or row >= ROWS) or
+                (col < 0 or col >= COLS) or
                 ((row, col) in visited) or
-                (grid[row][col] == "0")
-                ):
-                return
-
-            if (grid[row][col] == "1"):
-                visited.add((row, col))
+                (grid[row][col] == "0")):
+                return 
             
-            return (
-                dfs(row - 1, col, visited) or
-                dfs(row + 1, col, visited) or
-                dfs(row, col + 1, visited) or
-                dfs(row, col - 1, visited)
-            )
+            visited.add((row, col))
 
-        for idx in range(len(grid)):
-            for jdx in range(len(grid[idx])):
+            dfs(row + 1, col, visited)
+            dfs(row - 1, col, visited)
+            dfs(row, col + 1, visited)
+            dfs(row, col - 1, visited)
+
+        for idx in range(ROWS):
+            for jdx in range(COLS):
                 if (grid[idx][jdx] == "1" and (idx, jdx) not in visited):
-                    counter += 1
                     dfs(idx, jdx, visited)
-                    # print(visited)
-
+                    counter += 1
+        
         return counter
