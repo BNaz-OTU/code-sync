f9e232dc7bfdbdb8d1a1466ae90fb1e535fe16ac
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-01 15:44:54 -0500

    Update solution for letter-combinations-of-a-phone-number

--- leetcode/letter-combinations-of-a-phone-number.py
+++ leetcode/letter-combinations-of-a-phone-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         # USED SOLN: https://www.youtube.com/watch?v=0snEunUacZY
-        final = []
 
-        dictPhone = {
+        letToNum = {
             "2" : ["a", "b", "c"],
             "3" : ["d", "e", "f"],
             "4" : ["g", "h", "i"],
@@ -11,17 +10,18 @@ class Solution:
             "6" : ["m", "n", "o"],
             "7" : ["p", "q", "r", "s"],
             "8" : ["t", "u", "v"],
-            "9" : ["w", "x", "y", "z"],
+            "9" : ["w", "x", "y", "z"]
         }
 
-        def backtrack(i, curStr):
+        final = []
+
+        def backtrack(idx, curStr):
             if (len(curStr) == len(digits)):
                 final.append(curStr)
                 return
             
-
-            for char in dictPhone[digits[i]]:
-                backtrack(i + 1, curStr + char)
+            for char in letToNum[digits[idx]]:
+                backtrack(idx + 1, curStr + char)
         
         if (len(digits) > 0):
             backtrack(0, "")
