4010276e5e4fe5129f2d3a8e756b7b3e74afebfd
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-07 10:59:01 -0500

    Update solution for pacific-atlantic-water-flow

--- leetcode/pacific-atlantic-water-flow.py
+++ leetcode/pacific-atlantic-water-flow.py
@@ -1,19 +1,23 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         # USED SOLN: https://www.youtube.com/watch?v=s-VkcjHqkGI
-
         ROWS, COLS = len(heights), len(heights[0])
         pac, atl = set(), set()
 
         def dfs(row, col, visit, prevHeight):
-            if ((row, col) in visit or row < 0 or col < 0 or row == ROWS or col == COLS or heights[row][col] < prevHeight):
-                return 
+            if ((row, col) in visit or
+                (row < 0 or col < 0) or 
+                (row == ROWS or col == COLS) or
+                (heights[row][col] < prevHeight)):
+                return
             
             visit.add((row, col))
+
             dfs(row + 1, col, visit, heights[row][col])
             dfs(row - 1, col, visit, heights[row][col])
             dfs(row, col + 1, visit, heights[row][col])
             dfs(row, col - 1, visit, heights[row][col])
+
         
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
@@ -23,10 +27,11 @@ class Solution:
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS - 1, atl, heights[r][COLS - 1])
         
-        res = []
-        for row in range(ROWS):
-            for col in range(COLS):
-                if ((row, col) in pac and (row, col) in atl):
-                    res.append([row, col])
+        final = []
+
+        for idx in range(ROWS):
+            for jdx in range(COLS):
+                if ((idx, jdx) in pac and (idx, jdx) in atl):
+                    final.append((idx, jdx))
         
-        return res
+        return final
