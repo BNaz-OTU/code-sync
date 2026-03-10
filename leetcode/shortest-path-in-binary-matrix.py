50d101d02070fbd200a9e418795a43ab01583631
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-09 22:12:19 -0400

    Update solution for shortest-path-in-binary-matrix

--- leetcode/shortest-path-in-binary-matrix.py
+++ leetcode/shortest-path-in-binary-matrix.py
@@ -1,23 +1,32 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # USED SOLN: https://www.youtube.com/watch?v=YnxUdAO7TAo
+        SIZE = len(grid)
+        neighbours = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
 
-        N = len(grid)
-        queue = deque([(0, 0, 1)]) # row, column, length
-        visit = set((0,0))
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+        if (grid[0][0] == 1 or grid[-1][-1] == 1):
+            return -1
+        
+        queue = deque()
+        visit = set()
+        queue.append((0, 0, 1))
 
         while len(queue) > 0:
-            row, column, length = queue.popleft()
-            if (min(row, column) < 0 or max(row, column) >= N or grid[row][column] == 1):
-                continue
-            
-            if (row == N - 1 and column == N - 1):
+            row, col, length = queue.popleft()
+            if (row == SIZE -1 and col == SIZE -1):
                 return length
-            
-            for dr, dc in directions:
-                if ((row + dr, column + dc) not in visit):
-                    visit.add((row + dr, column + dc))
-                    queue.append((row + dr, column + dc, length + 1))
-        
+
+            for dr, dc in neighbours:
+                n_row = row + dr
+                n_col = col + dc
+
+                if ((n_row < 0 or n_row >= SIZE) or
+                    (n_col < 0 or n_col >= SIZE) or
+                    ((n_row, n_col) in visit) or 
+                    (grid[n_row][n_col] == 1)):
+                    continue
+                
+                
+                queue.append((n_row, n_col, length + 1))
+                visit.add((n_row, n_col))
+
         return -1
