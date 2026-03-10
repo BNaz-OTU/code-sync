af3b6dd672d257abda5f40d59e15af04b46dd5cd
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-09 22:32:43 -0400

    Update solution for rotting-oranges

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -1,52 +1,50 @@
-from collections import deque
-# USED SOLN (didn't acc use solution but needed help explaining): https://www.youtube.com/watch?v=y704fEOx0s0
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        neighbours = [[1,0], [-1, 0], [0, -1], [0, 1]]
-        minTime = 0
-
-        fresh = 0
+        neighbours = [[1, 0], [-1, 0], [0, -1], [0, 1]]
         rotten = deque()
-        visited = set()
+        visit = set()
+        orange = 0
 
         for idx in range(ROWS):
             for jdx in range(COLS):
                 if (grid[idx][jdx] == 2):
                     rotten.append((idx, jdx, 0))
-                    visited.add((idx, jdx))
-                
+
                 if (grid[idx][jdx] == 1):
-                    fresh += 1
+                    orange += 1
+        
+        if (orange == 0):
+            return 0
         
         while len(rotten) > 0:
-            row, col, time = rotten.popleft()
+            currQueue = len(rotten)
 
-            if ((min(row, col) < 0) or
-                (row >= ROWS or col >= COLS) or
-                (grid[row][col] == 0)):
-                continue
-            
-            if (grid[row][col] == 1):
-                fresh -= 1
+            while currQueue > 0:
+                row, col, time = rotten.popleft()
 
-            minTime = time
+                for dr, dc in neighbours:
+                    n_row = row + dr
+                    n_col = col + dc
 
-            for diff_row, diff_col in neighbours:
-                next_row = row + diff_row
-                next_col = col + diff_col
+                    if ((n_row < 0 or n_row >= ROWS) or
+                        (n_col < 0 or n_col >= COLS) or
+                        (grid[n_row][n_col] == 2 or grid[n_row][n_col] == 0) or
+                        ((n_row, n_col) in visit)):
+                        continue
+                    
+                    grid[n_row][n_col] = 2
+                    orange -= 1
+                    visit.add((n_row, n_col))
+                    rotten.append((n_row, n_col, time + 1))
 
-                if ((next_row, next_col) in visited):
-                    continue
+                currQueue -= 1
                 
-                # if (grid[next_row])
-                rotten.append((next_row, next_col, time + 1))
-                visited.add((next_row, next_col))
+        if (orange == 0):
+            print("here")
+            return time
+        return -1
 
-
-        if (fresh != 0):
-            return -1
-
-        return minTime
-        print(rotten)
+        print(orange)
+        print(len(rotten))
