0c6a95cee5fb6e0bfe917cf5a1251595ec00d135
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-14 17:23:15 -0400

    Update solution for rotting-oranges

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -1,28 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        neighbours = [[1, 0], [-1, 0], [0, -1], [0, 1]]
-        rotten = deque()
+        # USED SOLN: https://leetcode.com/problems/rotting-oranges/submissions/1943436408
+        ROWS, COLS = len(grid), len(grid[0])
+        queue = deque()
         visit = set()
-        orange = 0
+        fresh = 0
+        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         for idx in range(ROWS):
             for jdx in range(COLS):
                 if (grid[idx][jdx] == 2):
-                    rotten.append((idx, jdx, 0))
-
+                    queue.append([idx, jdx, 0])
+                
                 if (grid[idx][jdx] == 1):
-                    orange += 1
+                    fresh += 1
         
-        if (orange == 0):
+        if (fresh == 0):
             return 0
         
-        while len(rotten) > 0:
-            currQueue = len(rotten)
+        while len(queue) > 0:
+            qLen = len(queue)
 
-            while currQueue > 0:
-                row, col, time = rotten.popleft()
+            while qLen > 0:
+                row, col, length = queue.popleft()
 
                 for dr, dc in neighbours:
                     n_row = row + dr
@@ -30,21 +30,18 @@ class Solution:
 
                     if ((n_row < 0 or n_row >= ROWS) or
                         (n_col < 0 or n_col >= COLS) or
-                        (grid[n_row][n_col] == 2 or grid[n_row][n_col] == 0) or
-                        ((n_row, n_col) in visit)):
+                        ((n_row, n_col) in visit) or
+                        (grid[n_row][n_col] == 0) or
+                        (grid[n_row][n_col] == 2)):
                         continue
                     
                     grid[n_row][n_col] = 2
-                    orange -= 1
+                    fresh -= 1
                     visit.add((n_row, n_col))
-                    rotten.append((n_row, n_col, time + 1))
+                    queue.append([n_row, n_col, length + 1])
 
-                currQueue -= 1
-                
-        if (orange == 0):
-            print("here")
-            return time
+                qLen -= 1
+    
+        if (fresh == 0):
+            return length
         return -1
-
-        print(orange)
-        print(len(rotten))
