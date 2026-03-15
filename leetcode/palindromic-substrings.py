8c81e6d9ab1f513dc6050d9a7168dae1cfa46fd3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-15 17:13:20 -0400

    Update solution for palindromic-substrings

--- leetcode/palindromic-substrings.py
+++ leetcode/palindromic-substrings.py
@@ -1,21 +1,22 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # USED SOLN: https://www.youtube.com/watch?v=4RACzI5-du8
         count = 0
-        for idx in range(len(s)):
-            # EVEN
-            count += self.countPali(s, idx, idx + 1)
-            
-            # ODD
-            count += self.countPali(s, idx, idx)
-        
-        return count
 
-    def countPali(self, s, left, right):
-        res = 0
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-                # print(f"Left IDX: {left} -- Left Val: {s[left]} | Right IDX: {right} -- Right Val: {s[right]} | Word: {s[left : right + 1]}")
-                res += 1
+        for idx in range(len(s)):
+            # Odd Case
+            left = idx
+            right = idx
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
                 left -= 1
                 right += 1
-        return res
+            
+            # Even Case
+            left = idx 
+            right = idx + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            
+        return count
