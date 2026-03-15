d6a01624fbe02fcd6ce75fb9bef641448a1b03ea
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-15 17:07:21 -0400

    Update solution for longest-palindromic-substring

--- leetcode/longest-palindromic-substring.py
+++ leetcode/longest-palindromic-substring.py
@@ -1,35 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # USED SOLN: https://www.youtube.com/watch?v=XYQecbcd6_c
+        if (len(s) == 1):
+            return s
 
-        # Credit (NeetCode): https://www.youtube.com/watch?v=XYQecbcd6_c
+        maxCount = 0
+        pali = ""
 
-        res = ""
-        resLen = 0
-
-        for idx in range(len(s)):
-            # Odd Check
+        for idx in range(len(s) - 1):
             left = idx
             right = idx
-
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                if ((right - left + 1) > resLen):
-                    res = s[left:right + 1]
-                    resLen = right - left + 1
-                
+                if ((right - left + 1) > maxCount):
+                    pali = s[left:right + 1]
+                    maxCount = right - left + 1
+                left -= 1
+                right += 1
+            
+            left = idx
+            right = idx + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if ((right - left + 1) > maxCount):
+                    pali = s[left:right + 1]
+                    maxCount = right - left + 1
                 left -= 1
                 right += 1
 
-            # Even Check
-            left = idx
-            right = idx + 1
-
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if ((right - left + 1) > resLen):
-                    res = s[left: right + 1]
-                    resLen = right - left + 1
-                
-                left -= 1
-                right += 1      
-
-
-        return res
+        return pali
