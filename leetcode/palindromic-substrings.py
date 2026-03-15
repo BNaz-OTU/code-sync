421379f058fe9053832ed497f5cb5c5d4feb5cb9
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-15 17:14:51 -0400

    Update solution for palindromic-substrings

--- leetcode/palindromic-substrings.py
+++ leetcode/palindromic-substrings.py
@@ -3,7 +3,7 @@ class Solution:
         count = 0
 
         for idx in range(len(s)):
-            # Odd Case
+            # Odd Case (Counts all the odd cases: "a", "aaa", "aaaaa")
             left = idx
             right = idx
             while left >= 0 and right < len(s) and s[left] == s[right]:
@@ -11,7 +11,7 @@ class Solution:
                 left -= 1
                 right += 1
             
-            # Even Case
+            # Even Case (Counts all the even cases: "aa", "aaaa")
             left = idx 
             right = idx + 1
             while left >= 0 and right < len(s) and s[left] == s[right]:
