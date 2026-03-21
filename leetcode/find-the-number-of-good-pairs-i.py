c63b841ac0bbddecfe5201839d52dfbf079ec8af
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-21 18:11:25 -0400

    Update solution for find-the-number-of-good-pairs-i

--- leetcode/find-the-number-of-good-pairs-i.py
+++ leetcode/find-the-number-of-good-pairs-i.py
@@ -1,10 +1,17 @@
+# OPTIMAL SOLN: https://leetcode.com/problems/find-the-number-of-good-pairs-i/solutions/5668094/simple-onm-python-solution-beginner-frie-9oe0
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        counter = 0
 
-        for n1 in nums1:
-            for n2 in nums2:
-                if (n1 % (n2 * k) == 0):
-                    counter += 1
+        key_map = defaultdict(int) # using a default dict for creating a dict for the function type int
         
-        return counter
+        # initialising the key values for default dict
+        for i in nums2:
+            key_map[i*k] += 1
+        
+        count = 0 
+
+        for num in nums1:
+            for key in key_map:
+                if num % key == 0:
+                    count += key_map[key] # for the case when the numbers in nums2 are repeated 
+        return count
