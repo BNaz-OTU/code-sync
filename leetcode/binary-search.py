30fe7742c13f5f85313f5628cc418a3031b42a4d
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-22 14:25:12 -0400

    Update solution for binary-search

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -4,14 +4,14 @@ class Solution:
         right = len(nums) - 1
 
         while left <= right:
-            middle = (left + right) // 2            
+            middle = right - left
 
             if (nums[middle] == target):
                 return middle
-
+                
             elif (nums[middle] > target):
                 right = middle - 1
-
+            
             else:
                 left = middle + 1
         
