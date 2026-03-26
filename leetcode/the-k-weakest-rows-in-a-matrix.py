0f4494f7e79c295ac1b0fafd073c9983ad971a59
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-26 19:22:09 -0400

    Update solution for the-k-weakest-rows-in-a-matrix

--- leetcode/the-k-weakest-rows-in-a-matrix.py
+++ leetcode/the-k-weakest-rows-in-a-matrix.py
@@ -1,19 +1,22 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        final = []
         heap = []
-        
+        final = []
         for row in range(len(mat)):
-            count = 0
+            row_count = 0
             for col in range(len(mat[0])):
-                if (mat[row][col] == 1):
-                    count += 1
-            heap.append([count, row])
+                if (mat[row][col] == 0):
+                    break
+                row_count += 1
+            
+            heap.append([row_count, row])
         
+        print(heap)
+
         heapify(heap)
 
         while len(final) != k:
-            print(heap)
-            _, n = heappop(heap)
-            final.append(n)
+            _, num = heappop(heap)
+            final.append(num)
+        
         return final
