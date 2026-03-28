ee831eb10e6f46253cf3ef665a5f10a5cc939b21
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-28 18:19:22 -0400

    Update solution for longest-consecutive-sequence

--- leetcode/longest-consecutive-sequence.py
+++ leetcode/longest-consecutive-sequence.py
@@ -1,17 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if (len(nums) == 0):
-            return 0
+        numSet = set(nums)
+        maxCount = 0
 
-        setNum = set(nums)
-        maxCount = 1
-
-        for num in setNum:
-            if ((num - 1) not in setNum):   # This will locate the lowest number to start 
-                count = 0                   # searching from, if there was [1, 2, 3, 4], we want to start at 1 because checking the other numbers is a waste of time since you can find a longer sequence at starting at 1
-                while (num + count) in setNum:
+        for num in numSet:
+            if (num - 1 in numSet):
+                continue
+            else: 
+                count = 0
+                while num in numSet:
+                    num += 1
                     count += 1
-
                 maxCount = max(count, maxCount)
         
         return maxCount
