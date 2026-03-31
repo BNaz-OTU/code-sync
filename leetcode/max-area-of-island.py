c26267d06e163010a3665858c039446dd26128ba
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-03-31 19:11:46 -0400

    Update solution for max-area-of-island

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,26 +1,39 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        visited = set()
-        allVisited = set()
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        allVisit = set()
+        queue = deque()
+        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         maxCount = 0
 
-        def dfs(row, col, visited):
-            if ((row < 0 or col < 0) or (row >= ROWS or col >= COLS) or (grid[row][col] == 0) or ((row, col) in visited) or ((row, col) in allVisited)):
-                return 
+        def bfs(row, col, visit, allVisit, queue):
 
-            if grid[row][col] == 1:
-                visited.add((row, col))
-                allVisited.add((row, col))
-            
-            return dfs(row + 1, col, visited) or dfs(row - 1, col, visited) or dfs(row, col + 1, visited) or dfs(row, col - 1, visited)
+            while len(queue) > 0:
+                cur_row, cur_col = queue.popleft()
 
-        for idx in range(len(grid)):
-            for jdx in range(len(grid[idx])):
-                if (grid[idx][jdx] == 1 and grid[idx][jdx] not in allVisited):
-                    dfs(idx, jdx, visited)
-                    maxCount = max(len(visited), maxCount)
-                    visited = set()
+                for dr, dc in neighbours:
+                    n_row = cur_row + dr
+                    n_col = cur_col + dc
 
+                    if ((n_row < 0 or n_row >= ROWS) or
+                        (n_col < 0 or n_col >= COLS) or
+                        ((n_row, n_col) in allVisit) or
+                        (grid[n_row][n_col] == 0)):
+                        continue
+                    
+                    queue.append((n_row, n_col))
+                    visit.add((n_row, n_col))
+                    allVisit.add((n_row, n_col))
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (grid[row][col] == 1 and (row, col) not in allVisit):
+                    queue.append((row, col))
+                    visit.add((row, col))
+                    allVisit.add((row, col))
+                    bfs(row, col, visit, allVisit, queue)
+                    maxCount = max(maxCount, len(visit))
+                    visit = set()
+        
         return maxCount
