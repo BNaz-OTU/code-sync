fa1c94880547e8b0dab60032d4dc55d8c3162efb
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-04 14:22:12 -0400

    Update solution for top-k-frequent-elements

--- leetcode/top-k-frequent-elements.py
+++ leetcode/top-k-frequent-elements.py
@@ -5,18 +5,15 @@ class Solution:
         final = []
 
         for num in nums:
-            if (num not in hashNum):
-                hashNum[num] = 1
-            else:
+            if (num in hashNum):
                 hashNum[num] += 1
-
-        print(hashNum)
-
-        for key, value in hashNum.items():
-            heap.append([value * -1, key])
+            else:
+                hashNum[num] = 1
+        
+        for key, val in hashNum.items():
+            heap.append([val * -1, key])
         
         heapify(heap)
-
         while len(final) != k:
             val = heappop(heap)
             final.append(val[1])
