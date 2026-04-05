b34aa1a846a584bf472d2ac8bbd25a14bb6d6bed
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-05 14:16:43 -0400

    Update solution for course-schedule

--- leetcode/course-schedule.py
+++ leetcode/course-schedule.py
@@ -1,32 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        hashCrs = {}
+        adjList = {}
         visit = set()
 
-        for crs in range(numCourses):
-            hashCrs[crs] = []
+        for num in range(numCourses):
+            adjList[num] = []
         
         for crs, pre in prerequisites:
-            hashCrs[crs].append(pre)
+            adjList[crs].append(pre)
         
+
         def dfs(crs):
-            if crs in visit:
-                return False
-            
-            if (hashCrs[crs] == []):
+            if (adjList[crs] == []):
                 return True
+            
+            if (crs in visit):
+                return False
             
             visit.add(crs)
 
-            for crs1 in hashCrs[crs]:
+            for crs1 in adjList[crs]:
                 if (not dfs(crs1)):
                     return False
             
             visit.remove(crs)
-            hashCrs[crs] = []
+            adjList[crs] = []
             return True
-            
-        
+
         for crs in range(numCourses):
             if (not dfs(crs)):
                 return False
