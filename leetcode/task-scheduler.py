bb70688cf0309b09e2a32798c483f361717fe1fc
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-05 13:45:38 -0400

    Update solution for task-scheduler

--- leetcode/task-scheduler.py
+++ leetcode/task-scheduler.py
@@ -1,19 +1,30 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapify(maxHeap)
+        heap = []
+        count = 0
+        dictTask = {}
+        queue = deque()
 
-        time = 0
-        q = deque()
-        while maxHeap or q:
-            time += 1
-            if maxHeap:
-                cnt = 1 + heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time + n])
+        for task in tasks:
+            if (task in dictTask):
+                dictTask[task] -= 1
             
-            if q and q[0][1] == time:
-                heappush(maxHeap, q.popleft()[0])
+            else:
+                dictTask[task] = -1
         
-        return time
+        for val in dictTask.values():
+            heap.append(val)
+        
+        heapify(heap)
+        while len(queue) > 0 or len(heap) > 0:
+            count += 1
+
+            if (heap):
+                v1 = 1 + heappop(heap)
+                if (v1 < 0):
+                    queue.append([v1, count + n])
+            
+            if (queue and queue[0][1] == count):
+                heappush(heap, queue.popleft()[0])
+
+        return count
