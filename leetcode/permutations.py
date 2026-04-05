fcddfd0949b01c3862c509fe218eeff2d2b00aa4
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-05 14:58:00 -0400

    Update solution for permutations

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -8,10 +8,16 @@ class Solution:
         n_perm = []
 
         for p in perms:
-            for idx in range(len(p) + 1):
-                p_copy = p.copy()
+            for idx in range(len(p) + 1): # Included '+ 1' so I can insert a number at the end
+                # Create a copy, if you don't the previous appended permutations will also be modified
+                p_copy = p.copy() 
+
+                # Insert the current number to all possible positions
                 p_copy.insert(idx, nums[0])
+
+                # After completing the insert for current permutation, add it to new perm list.
+                # This new perm list will get called recursively so more numbers get added
                 n_perm.append(p_copy)
         
-
+        # OTHER BASE CASE
         return n_perm
