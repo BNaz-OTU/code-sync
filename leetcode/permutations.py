e12d2da97f948f7dd85578cf26c70e8d68b95bc7
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-10 16:04:54 -0400

    Update solution for permutations

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -1,23 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # USED SOLN: https://www.youtube.com/watch?v=FZe0UqISmUw
+        
         if (len(nums) == 0):
             return [[]]
         
         perms = self.permute(nums[1:])
-        n_perm = []
+        res = []
 
         for p in perms:
-            for idx in range(len(p) + 1): # Included '+ 1' so I can insert a number at the end
-                # Create a copy, if you don't the previous appended permutations will also be modified
-                p_copy = p.copy() 
-
-                # Insert the current number to all possible positions
+            for idx in range(len(p) + 1):
+                p_copy = p.copy()
                 p_copy.insert(idx, nums[0])
-
-                # After completing the insert for current permutation, add it to new perm list.
-                # This new perm list will get called recursively so more numbers get added
-                n_perm.append(p_copy)
+                res.append(p_copy)
         
-        # OTHER BASE CASE
-        return n_perm
+        return res
