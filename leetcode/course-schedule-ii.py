977bce17cf027926bd3ba1c81ef918002dcc05f5
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-10 16:34:22 -0400

    Update solution for course-schedule-ii

--- leetcode/course-schedule-ii.py
+++ leetcode/course-schedule-ii.py
@@ -1,37 +1,36 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        hashMap = {}
-        cycle = set() # Used to detect if there is a cycle/forever loop of taking courses
-        visit = set() # Unlike cycle this is used to determine if I've already checked a given course and prevents duplicate courses from 
-                      # appearing in the final output
-        final = []
+        hashCrs = {}
 
-        for num in range(numCourses):
-            hashMap[num] = []
-
-        for crs, pre in prerequisites:
-            hashMap[crs].append(pre)
+        for idx in range(numCourses):
+            hashCrs[idx] = []
         
+        for crs, pre in prerequisites:
+            hashCrs[crs].append(pre)
+        
+        visit = set()
+        cycle = set()
+        final = []
         def dfs(crs):
-            if (crs in cycle):
+            if crs in cycle:
                 return False
 
-            if (crs in visit):
+            if crs in visit:
                 return True
             
             cycle.add(crs)
-            for pre in hashMap[crs]:
-                if (not dfs(pre)): 
+            
+            for crs1 in hashCrs[crs]:
+                if (not dfs(crs1)):
                     return False
-
+            
             cycle.remove(crs)
             visit.add(crs)
             final.append(crs)
             return True
-
-            
+        
         for crs in range(numCourses):
-            if (dfs(crs) == False):
+            if (not dfs(crs)):
                 return []
-
+        
         return final
