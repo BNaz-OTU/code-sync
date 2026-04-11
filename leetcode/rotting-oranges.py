88f3cccfab1b8f77949f63422f37d80383b9a800
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-11 15:23:39 -0400

    Update solution for rotting-oranges

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -4,50 +4,43 @@ class Solution:
         rotten = deque()
         visit = set()
         fresh = 0
-        neighbours = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (grid[row][col] == 1):
                     fresh += 1
-                
                 elif (grid[row][col] == 2):
                     rotten.append((row, col, 0))
                     visit.add((row, col))
         
+        # print(rotten, fresh)
+
         if fresh == 0:
             return 0
 
         while len(rotten) > 0:
-            qLen = len(rotten)
-            while qLen > 0:
-                row, col, time = rotten.popleft()
+            print(rotten, fresh)
+            cr, cc, time = rotten.popleft()
 
-                # if fresh == 0:
-                #     return time
+            for dr, dc in neighbours:
+                # print(cr, cc, time)
+                n_row = cr + dr
+                n_col = cc + dc
 
-                for dr, dc in neighbours:
-                    n_row = row + dr
-                    n_col = col + dc
-
-                    if ((n_row < 0 or n_row >= ROWS) or
-                        (n_col < 0 or n_col >= COLS) or
-                        (grid[n_row][n_col] == 0) or
-                        ((n_row, n_col) in visit)):
-                        continue
-                    
-                    grid[n_row][n_col] = 2
-                    fresh -= 1
+                if ((n_row < 0 or n_row >= ROWS) or
+                    (n_col < 0 or n_col >= COLS) or
+                    (grid[n_row][n_col] == 0) or
+                    ((n_row, n_col) in visit)):
+                    continue
+                
+                elif (grid[n_row][n_col] == 1):
                     rotten.append((n_row, n_col, time + 1))
                     visit.add((n_row, n_col))
-                
-                qLen -= 1
+                    fresh -= 1
+            # print("--------\n")
         
-        if (fresh != 0):
-            return -1
-        return time
-                
-
+        if fresh == 0:
+            return time
         
-        print(fresh)
-        print(rotten)
+        return -1
