f9f3e99601f96bc9eb004f01833f47fd85cfac27
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-12 14:50:57 -0400

    Update solution for rotting-oranges

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -2,8 +2,8 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         rotten = deque()
-        visit = set()
         fresh = 0
+        visit = set()
         neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         for row in range(ROWS):
@@ -14,33 +14,27 @@ class Solution:
                     rotten.append((row, col, 0))
                     visit.add((row, col))
         
-        # print(rotten, fresh)
-
-        if fresh == 0:
+        if (fresh == 0):
             return 0
-
+        
         while len(rotten) > 0:
-            print(rotten, fresh)
-            cr, cc, time = rotten.popleft()
+            c_row, c_col, time = rotten.popleft()
 
             for dr, dc in neighbours:
-                # print(cr, cc, time)
-                n_row = cr + dr
-                n_col = cc + dc
+                n_row = c_row + dr
+                n_col = c_col + dc
 
                 if ((n_row < 0 or n_row >= ROWS) or
                     (n_col < 0 or n_col >= COLS) or
-                    (grid[n_row][n_col] == 0) or
-                    ((n_row, n_col) in visit)):
+                    ((n_row, n_col) in visit) or 
+                    (grid[n_row][n_col] == 0)):
                     continue
                 
-                elif (grid[n_row][n_col] == 1):
-                    rotten.append((n_row, n_col, time + 1))
-                    visit.add((n_row, n_col))
-                    fresh -= 1
-            # print("--------\n")
-        
-        if fresh == 0:
+                rotten.append((n_row, n_col, time + 1))
+                visit.add((n_row, n_col))
+                fresh -= 1
+
+        if (fresh == 0):
             return time
-        
+
         return -1
