19ce7150968cbd36c960b6bc0655c4edc09e3769
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-12 15:55:37 -0400

    Update solution for count-and-say

--- leetcode/count-and-say.py
+++ leetcode/count-and-say.py
@@ -1,21 +1,22 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        
+
         def function(val):
             if (val == "1"):
                 return "11"
             
-            new_str = ""
+            new_s = ""
             count = 1
             for idx in range(len(val) - 1):
                 if (val[idx] == val[idx + 1]):
                     count += 1
+                
                 else:
-                    new_str += str(count) + val[idx]
+                    new_s += str(count) + val[idx]
                     count = 1
             
-            new_str += str(count) + val[idx + 1]
-            return new_str
+            new_s += str(count) + val[idx + 1]
+            return new_s
         
         val = "1"
         for _ in range(n - 1):
