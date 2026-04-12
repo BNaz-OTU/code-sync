e92881082aa9d787f3e7991a4cfc435ed4788718
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-12 15:50:58 -0400

    Update solution for clone-graph

--- leetcode/clone-graph.py
+++ leetcode/clone-graph.py
@@ -10,19 +10,20 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         adjList = {}
-        
-        def dfs(node):
-            if node in adjList:
-                return adjList[node]
 
-            adjList[node] = Node(node.val)
+        def dfs(node):
+            if (node in adjList):
+                return adjList[node]
             
+            copyVal = Node(node.val)
+            adjList[node] = copyVal
+
             for nei in node.neighbors:
-                adjList[node].neighbors.append(dfs(nei))
+                copyVal.neighbors.append(dfs(nei))
             
-            return adjList[node]
+            return copyVal
         
         if node is None:
             return None
-
+            
         return dfs(node)
