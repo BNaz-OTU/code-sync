585e0004485173b8b258cba4cc4aea741a4031d1
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-18 15:17:12 -0400

    Update solution for pacific-atlantic-water-flow

--- leetcode/pacific-atlantic-water-flow.py
+++ leetcode/pacific-atlantic-water-flow.py
@@ -1,23 +1,26 @@
 class Solution:
+    # https://www.youtube.com/watch?v=s-VkcjHqkGI
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # USED SOLN: https://www.youtube.com/watch?v=s-VkcjHqkGI
         ROWS, COLS = len(heights), len(heights[0])
         pac, atl = set(), set()
 
-        def dfs(row, col, visit, prevHeight):
-            if ((row, col) in visit or
-                (row < 0 or col < 0) or 
-                (row == ROWS or col == COLS) or
-                (heights[row][col] < prevHeight)):
+        def dfs(r, c, visit, prevHeight):
+            if ((r < 0 or r >= ROWS) or
+                (c < 0 or c >= COLS) or
+                (heights[r][c] < prevHeight) or
+                ((r, c) in visit)):
                 return
+
+            # if ((r, c) in visit or r < 0 or c < 0 or 
+            #     r == ROWS or c == COLS or 
+            #     heights[r][c] < prevHeight):
+            #     return
             
-            visit.add((row, col))
-
-            dfs(row + 1, col, visit, heights[row][col])
-            dfs(row - 1, col, visit, heights[row][col])
-            dfs(row, col + 1, visit, heights[row][col])
-            dfs(row, col - 1, visit, heights[row][col])
-
+            visit.add((r, c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
         
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
@@ -27,11 +30,10 @@ class Solution:
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS - 1, atl, heights[r][COLS - 1])
         
-        final = []
-
-        for idx in range(ROWS):
-            for jdx in range(COLS):
-                if ((idx, jdx) in pac and (idx, jdx) in atl):
-                    final.append((idx, jdx))
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
         
-        return final
+        return res
