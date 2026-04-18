e52ecdcea25cfebf0590d98e9ecc43d8ff7c5c74
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-18 13:01:13 -0400

    Update solution for max-area-of-island

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
         allVisit = set()
         queue = deque()
+        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         maxCount = 0
-        neighbours = [[1,0], [0, 1], [-1, 0], [0, -1]]
 
-        def bfs(queue, visit, allVisit):
-            # visit.add()
+        def bfs(row, col, visit, queue):
             while len(queue) > 0:
-                row, col = queue.popleft()
+                c_row, c_col = queue.popleft()
 
                 for dr, dc in neighbours:
-                    n_row = row + dr
-                    n_col = col + dc
+                    n_row = c_row + dr
+                    n_col = c_col + dc
 
                     if ((n_row < 0 or n_row >= ROWS) or
                         (n_col < 0 or n_col >= COLS) or
@@ -23,16 +23,17 @@ class Solution:
                     
                     allVisit.add((n_row, n_col))
                     visit.add((n_row, n_col))
-                    queue.append([n_row, n_col])
+                    queue.append((n_row, n_col))
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (grid[row][col] == 1 and (row, col) not in allVisit):
-                    visit = set()
-                    queue.append([row, col])
-                    visit.add((row, col))
                     allVisit.add((row, col))
-                    bfs(queue, visit, allVisit)
+                    visit.add((row, col))
+                    queue.append((row, col))
+                    bfs(row, col, visit, queue)
                     maxCount = max(maxCount, len(visit))
+                    visit = set()
+                    print(allVisit)
         
         return maxCount
