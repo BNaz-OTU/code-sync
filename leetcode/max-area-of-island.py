759f6702e0b662acbe5bbbe3099b17414722f931
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-19 14:39:57 -0400

    Update solution for max-area-of-island

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        visit = set()
         allVisit = set()
+        tempVisit = set()
         queue = deque()
         neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         maxCount = 0
 
-        def bfs(row, col, visit, queue):
+        def bfs(queue):
             while len(queue) > 0:
-                c_row, c_col = queue.popleft()
+                cur_row, cur_col = queue.popleft()
 
-                for dr, dc in neighbours:
-                    n_row = c_row + dr
-                    n_col = c_col + dc
+                for d_row, d_col in neighbours:
+                    n_row = cur_row + d_row
+                    n_col = cur_col + d_col
 
                     if ((n_row < 0 or n_row >= ROWS) or
                         (n_col < 0 or n_col >= COLS) or
@@ -22,18 +22,17 @@ class Solution:
                         continue
                     
                     allVisit.add((n_row, n_col))
-                    visit.add((n_row, n_col))
+                    tempVisit.add((n_row, n_col))
                     queue.append((n_row, n_col))
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (grid[row][col] == 1 and (row, col) not in allVisit):
+                    tempVisit.add((row, col))
                     allVisit.add((row, col))
-                    visit.add((row, col))
                     queue.append((row, col))
-                    bfs(row, col, visit, queue)
-                    maxCount = max(maxCount, len(visit))
-                    visit = set()
-                    print(allVisit)
+                    bfs(queue)
+                    maxCount = max(maxCount, len(tempVisit))
+                    tempVisit = set()
         
         return maxCount
