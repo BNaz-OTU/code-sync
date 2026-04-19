d7f432f6c3c4b31db33bb966267ded907e7a19f9
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-19 14:23:42 -0400

    Update solution for detect-capital

--- leetcode/detect-capital.py
+++ leetcode/detect-capital.py
@@ -2,10 +2,10 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         capitalCount = 0
 
-        for idx in range(len(word)):
-            if (ord(word[idx]) <= 90):
+        for letter in word:
+            if (ord(letter) <= 90):
                 capitalCount += 1
-
+        
         if (capitalCount == len(word) or capitalCount == 0 or (capitalCount == 1 and ord(word[0]) <= 90)):
             return True
         
