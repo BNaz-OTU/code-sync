aadd9cf61d781bda73279192240cf9e896c8e505
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-19 16:38:35 -0400

    Update solution for redundant-connection

--- leetcode/redundant-connection.py
+++ leetcode/redundant-connection.py
@@ -1,28 +1,28 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        # USED SOLN: https://neetcode.io/problems/redundant-connection/solution
-        n = len(edges)
-        adj = [[] for _ in range(n + 1)]
+        adjList = {}
 
-        def dfs(node, par):
-            if visit[node]:
-                return True
+        for num in range(len(edges)):
+            adjList[num + 1] = []
+        
+        def dfs(curNum, prevNum):
+            if curNum in visit:
+                return False
             
-            visit[node] = True
-            for nei in adj[node]:
-                if (nei == par):
+            visit.add(curNum)
+
+            for val in adjList[curNum]:
+                if (val == prevNum):
                     continue
                 
-                if dfs(nei, node):
-                    return True
-            return False
-        
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            visit = [False] * (n + 1)
-
-            if dfs(u, -1):
-                return [u, v]
-        
-        return []
+                if (not dfs(val, curNum)):
+                    return False
+            
+            return True
+            
+        for v1, v2 in edges:
+            visit = set()
+            adjList[v1].append(v2)
+            adjList[v2].append(v1)
+            if (not dfs(v1, 0)):
+                return [v1, v2]
