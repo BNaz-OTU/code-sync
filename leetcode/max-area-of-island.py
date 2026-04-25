7c8c3d3914d3b3ef532efceac49316d4d16c97dd
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-25 13:42:20 -0400

    Update solution for max-area-of-island

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,38 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        maxSize = 0
         ROWS, COLS = len(grid), len(grid[0])
         allVisit = set()
-        tempVisit = set()
-        queue = deque()
-        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        maxCount = 0
 
-        def bfs(queue):
-            while len(queue) > 0:
-                cur_row, cur_col = queue.popleft()
+        def dfs(row, col):
+            if ((row < 0 or row >= ROWS) or
+                (col < 0 or col >= COLS) or
+                (grid[row][col] == 0) or
+                ((row, col) in allVisit)):
+                return
+            
+            allVisit.add((row, col))
+            tempVisit.add((row, col))
 
-                for d_row, d_col in neighbours:
-                    n_row = cur_row + d_row
-                    n_col = cur_col + d_col
-
-                    if ((n_row < 0 or n_row >= ROWS) or
-                        (n_col < 0 or n_col >= COLS) or
-                        (grid[n_row][n_col] == 0) or
-                        ((n_row, n_col) in allVisit)):
-                        continue
-                    
-                    allVisit.add((n_row, n_col))
-                    tempVisit.add((n_row, n_col))
-                    queue.append((n_row, n_col))
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+            dfs(row + 1, col)
+            dfs(row - 1, col)
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (grid[row][col] == 1 and (row, col) not in allVisit):
-                    tempVisit.add((row, col))
-                    allVisit.add((row, col))
-                    queue.append((row, col))
-                    bfs(queue)
-                    maxCount = max(maxCount, len(tempVisit))
                     tempVisit = set()
+                    dfs(row, col)
+                    maxSize = max(maxSize, len(tempVisit))
         
-        return maxCount
+        return maxSize
