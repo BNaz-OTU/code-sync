7254a3bc623e3b74829c7fccc47dbb4e933bf2a3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-26 11:36:44 -0400

    Update solution for max-area-of-island

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,29 +1,37 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        maxSize = 0
         ROWS, COLS = len(grid), len(grid[0])
-        allVisit = set()
+        maxCount = 0
+        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        visit = set()
+        queue = deque()
 
-        def dfs(row, col):
-            if ((row < 0 or row >= ROWS) or
-                (col < 0 or col >= COLS) or
-                (grid[row][col] == 0) or
-                ((row, col) in allVisit)):
-                return
-            
-            allVisit.add((row, col))
-            tempVisit.add((row, col))
+        def bfs(queue):
+            counter = 0
+            while len(queue) > 0:
+                row, col = queue.popleft()
+                counter += 1
 
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-            dfs(row + 1, col)
-            dfs(row - 1, col)
+                for dr, dc in neighbours:
+                    n_row = row + dr
+                    n_col = col + dc
+
+                    if ((n_row < 0 or n_row >= ROWS) or
+                        (n_col < 0 or n_col >= COLS) or
+                        (grid[n_row][n_col] == 0) or
+                        ((n_row, n_col) in visit)):
+                        continue
+                    
+                    visit.add((n_row, n_col))
+                    queue.append((n_row, n_col))
+            return counter
+
 
         for row in range(ROWS):
             for col in range(COLS):
-                if (grid[row][col] == 1 and (row, col) not in allVisit):
-                    tempVisit = set()
-                    dfs(row, col)
-                    maxSize = max(maxSize, len(tempVisit))
+                if (grid[row][col] == 1 and (row, col) not in visit):
+                    queue.append((row, col))
+                    visit.add((row, col))
+                    maxCount = max(maxCount, bfs(queue))
         
-        return maxSize
+        return maxCount
