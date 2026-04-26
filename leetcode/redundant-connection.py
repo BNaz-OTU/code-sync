0dd6788c0378f98b41217674bb36bf7791097a4f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-26 13:00:05 -0400

    Update solution for redundant-connection

--- leetcode/redundant-connection.py
+++ leetcode/redundant-connection.py
@@ -5,24 +5,25 @@ class Solution:
         for num in range(len(edges)):
             adjList[num + 1] = []
         
-        def dfs(curNum, prevNum):
-            if curNum in visit:
+        def dfs(node, prev):
+            if node in visit:
                 return False
             
-            visit.add(curNum)
+            visit.add(node)
 
-            for val in adjList[curNum]:
-                if (val == prevNum):
+            for val in adjList[node]:
+                if (val == prev):
                     continue
                 
-                if (not dfs(val, curNum)):
+                if (not dfs(val, node)):
                     return False
-            
+                
             return True
-            
+
         for v1, v2 in edges:
             visit = set()
             adjList[v1].append(v2)
             adjList[v2].append(v1)
-            if (not dfs(v1, 0)):
+
+            if not dfs(v1, -1):
                 return [v1, v2]
