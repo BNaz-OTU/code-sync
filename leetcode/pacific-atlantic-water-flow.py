fcfe483cbc08091db5dcf1ba1a76a61d2313dd1a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-04-26 12:17:34 -0400

    Update solution for pacific-atlantic-water-flow

--- leetcode/pacific-atlantic-water-flow.py
+++ leetcode/pacific-atlantic-water-flow.py
@@ -1,16 +1,15 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific = set()
-        atlantic = set()
         ROWS, COLS = len(heights), len(heights[0])
+        atl, pac = set(), set()
 
         def dfs(row, col, ocean, prevHeight):
             if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
                 ((row, col) in ocean) or
-                (prevHeight > heights[row][col])):
+                (heights[row][col] < prevHeight)):
                 return
-
+            
             ocean.add((row, col))
 
             dfs(row + 1, col, ocean, heights[row][col])
@@ -19,19 +18,17 @@ class Solution:
             dfs(row, col - 1, ocean, heights[row][col])
 
         for row in range(ROWS):
-            dfs(row, 0, pacific, -1)
-            dfs(row, COLS - 1, atlantic, -1)
-        
+            dfs(row, 0, pac, -1)
+            dfs(row, COLS - 1, atl, -1)
+
         for col in range(COLS):
-            dfs(0, col, pacific, -1)
-            dfs(ROWS - 1, col, atlantic, -1)
-        
-        print(pacific)
-        print(atlantic)
+            dfs(0, col, pac, -1)
+            dfs(ROWS - 1, col, atl, -1)
+
         final = []
         for row in range(ROWS):
             for col in range(COLS):
-                if ((row, col) in pacific and (row, col) in atlantic):
+                if ((row, col) in atl and (row, col) in pac):
                     final.append([row, col])
         
         return final
