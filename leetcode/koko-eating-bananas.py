549702b921e74e71216c49d408e094babccea273
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-03 10:22:09 -0400

    Update solution for koko-eating-bananas

--- leetcode/koko-eating-bananas.py
+++ leetcode/koko-eating-bananas.py
@@ -1,23 +1,25 @@
 import math
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left = 1
         right = max(piles)
-        final = right
+        minTime = 0
 
         while left <= right:
             middle = (left + right) // 2
-            count = 0
 
+            count = 0
             for pile in piles:
                 count += math.ceil(pile / middle)
             
-            print(middle, count)
-            if (count <= h):
-                final = min(final, middle)
-                right = middle - 1
-
-            else:            
+            print(f"Left: {left} | Right: {right} | Middle: {middle} | Time: {count}")
+            
+            if (count > h):
                 left = middle + 1
+            
+            elif (count <= h):
+                minTime = middle
+                right = middle - 1
         
-        return final
+        return minTime
