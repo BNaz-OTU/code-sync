f049c4894292897842a15db6fc9ac56d9c70d270
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-02 21:53:23 -0400

    Update solution for search-a-2d-matrix

--- leetcode/search-a-2d-matrix.py
+++ leetcode/search-a-2d-matrix.py
@@ -1,19 +1,39 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
 
-        for idx in range(len(matrix)):
-            left = 0
-            right = len(matrix[0]) - 1
+        bot, top = 0, ROWS - 1
+        # bot = ROWS - 1
 
-            while left <= right:
-                middle = right - left
+        while bot <= top:
+            middle = (top + bot) // 2
 
-                if (matrix[idx][middle] == target):
-                    return True
-                
-                elif (matrix[idx][middle] > target):
-                    right = middle - 1
-                
-                else:
-                    left = middle + 1
+            if (target < matrix[middle][0]):
+                top = middle - 1
+            
+            elif (target > matrix[middle][-1]):
+                bot = middle + 1
+            
+            else:
+                break
+        
+        if (bot > top):
+            return False
+        
+        row = (top + bot) // 2
+
+        left, right = 0, COLS - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            if (matrix[row][middle] == target):
+                return True
+            
+            elif (matrix[row][middle] > target):
+                right = middle -1
+            
+            else:
+                left = middle + 1
+        
         return False
