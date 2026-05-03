c638f2bdc54e3a2b7bca95faba4c88a808da76b4
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-02 21:03:43 -0400

    Update solution for product-of-array-except-self

--- leetcode/product-of-array-except-self.py
+++ leetcode/product-of-array-except-self.py
@@ -1,28 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        [1, 1, 2, 6, 24]
-        [24, 24, 12, 4, 1]
-
-        arr1 = [1] * (len(nums) + 1)
-        arr2 = [1] * (len(nums) + 1)
+        nums_left_start = [1] * (len(nums) + 1)
+        nums_right_start = [1] * (len(nums) + 1)
         final = []
 
         count = 0
         for idx in range(len(nums)):
             count -= 1
-            arr1[idx + 1] = nums[idx] * arr1[idx]
-            arr2[idx + 1] = nums[count] * arr2[idx]
+            nums_left_start[idx + 1] = nums_left_start[idx] * nums[idx]
+            nums_right_start[idx + 1] = nums_right_start[idx] * nums[count]
+        
+        r_nums_right_start = nums_right_start[::-1]
 
-        r_arr2 = arr2[::-1]
-        for idx in range(len(arr1) - 1):
-            final.append(arr1[idx] * r_arr2[idx + 1])
+        for idx in range(len(nums)):
+            final.append(nums_left_start[idx] * r_nums_right_start[idx + 1])
         
         return final
-        
-        
-        # for idx in range(len(nums), -1, -1):
-        #     arr2[idx + 1] = 
-        #     print(idx)
-        
-        print(arr1)
-        print(arr2)
