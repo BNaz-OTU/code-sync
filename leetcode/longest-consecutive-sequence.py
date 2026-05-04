cb6112cd64db23c86191e6e5648c1a8ce31f937f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-03 20:06:35 -0400

    Update solution for longest-consecutive-sequence

--- leetcode/longest-consecutive-sequence.py
+++ leetcode/longest-consecutive-sequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         visit = set(nums)
-        maxCount = 0
+        maxLen = 0
 
         for num in visit:
-            if ((num - 1) not in visit):
+            if (num - 1 not in visit):
                 count = 0
-
-                while (count + num) in visit:
+                while (num + count) in visit:
                     count += 1
-                maxCount = max(maxCount, count)
-
-        return maxCount
+                
+                maxLen = max(maxLen, count)
+        
+        return maxLen
