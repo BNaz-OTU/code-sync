0d75d3dcdd53dc1f47c1f0ffface058fae9c5846
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-04 20:16:22 -0400

    Update solution for longest-palindromic-substring

--- leetcode/longest-palindromic-substring.py
+++ leetcode/longest-palindromic-substring.py
@@ -1,26 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # USED SOLN: https://www.youtube.com/watch?v=XYQecbcd6_c
-        maxCount = 0
-        pali = ""
+        maxLen = 1
+        curStr = s[0]
 
         for idx in range(len(s)):
-            left = idx
-            right = idx
+            left, right = idx, idx
+
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                if ((right - left + 1) > maxCount):
-                    pali = s[left:right + 1]
-                    maxCount = right - left + 1
+                if (right - left + 1 >= maxLen):
+                    curStr = s[left: right + 1]
+                    maxLen = right - left + 1
                 left -= 1
                 right += 1
             
-            left = idx
-            right = idx + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if ((right - left + 1) > maxCount):
-                    pali = s[left:right + 1]
-                    maxCount = right - left + 1
+            left, right = idx, idx + 1
+
+            while (left >= 0) and (right < len(s)) and (s[left] == s[right]):
+                if (right - left + 1 >= maxLen):
+                    curStr = s[left: right + 1]
+                    maxLen = right - left + 1
                 left -= 1
                 right += 1
 
-        return pali
+        return curStr
