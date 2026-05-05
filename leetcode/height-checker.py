c00ad23bc546bf5cb74c43c2beb72d5b92fde955
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-05 19:30:41 -0400

    Update solution for height-checker

--- leetcode/height-checker.py
+++ leetcode/height-checker.py
@@ -1,10 +1,10 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        sort_h = sorted(heights)
+        sorted_h = sorted(heights)
         counter = 0
 
         for idx in range(len(heights)):
-            if (sort_h[idx] != heights[idx]):
+            if (sorted_h[idx] != heights[idx]):
                 counter += 1
         
         return counter
