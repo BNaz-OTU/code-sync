b642dead70001596b2249e0b717b427de69fecb4
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-08 09:05:16 -0400

    Update solution for time-based-key-value-store

--- leetcode/time-based-key-value-store.py
+++ leetcode/time-based-key-value-store.py
@@ -3,31 +3,32 @@ class TimeMap:
     def __init__(self):
         self.hashMap = {}
         
-
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.hashMap:
             self.hashMap[key] = []
         
-        self.hashMap[key].append([value, timestamp])
-        
+        self.hashMap[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        values = self.hashMap.get(key, [])
+        result = ""
+        if (key not in self.hashMap):
+            return result
 
-        # binary search
-        left, right = 0, len(values) - 1
+        left = 0 
+        right = len(self.hashMap[key]) - 1
 
         while left <= right:
             middle = (left + right) // 2
 
-            if values[middle][1] <= timestamp:
-                res = values[middle][0]
+            if (self.hashMap[key][middle][0] <= timestamp):
+                result = self.hashMap[key][middle][1]
                 left = middle + 1
+            
             else:
                 right = middle - 1
-        
-        return res
+            
+        return result
+
         
 
 
