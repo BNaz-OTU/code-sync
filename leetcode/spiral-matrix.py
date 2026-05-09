3e6f63d91038ea595a2bfb40e690d7c688e9c5e3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-09 13:49:05 -0400

    Update solution for spiral-matrix

--- leetcode/spiral-matrix.py
+++ leetcode/spiral-matrix.py
@@ -1,8 +1,8 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        final = []
-        left, right = 0, len(matrix[0])
         top, bot = 0, len(matrix)
+        left, right = 0, len(matrix[0])
+        final = []
 
         while top < bot and left < right:
             for idx in range(left, right):
@@ -13,16 +13,17 @@ class Solution:
                 final.append(matrix[jdx][right - 1])
             
             right -= 1
-            if not(left < right and top < bot):
-                return final
-                
+
+            if not (top < bot and left < right):
+                break
+
             for wdx in range(right - 1, left - 1, -1):
                 final.append(matrix[bot - 1][wdx])
             
-
             bot -= 1
             for zdx in range(bot - 1, top - 1, -1):
                 final.append(matrix[zdx][left])
-
-            left += 1        
+            
+            left += 1
+        
         return final
