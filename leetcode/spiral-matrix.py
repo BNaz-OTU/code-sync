4079c86ed5a128cda579c411a70c34740803807b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-10 14:56:32 -0400

    Update solution for spiral-matrix

--- leetcode/spiral-matrix.py
+++ leetcode/spiral-matrix.py
@@ -9,21 +9,23 @@ class Solution:
                 final.append(matrix[top][idx])
             
             top += 1
+
             for jdx in range(top, bot):
                 final.append(matrix[jdx][right - 1])
             
             right -= 1
 
             if not (top < bot and left < right):
-                break
+                return final
 
             for wdx in range(right - 1, left - 1, -1):
                 final.append(matrix[bot - 1][wdx])
             
             bot -= 1
+
             for zdx in range(bot - 1, top - 1, -1):
                 final.append(matrix[zdx][left])
             
             left += 1
-        
+            
         return final
