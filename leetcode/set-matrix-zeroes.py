16307d606361fa54ec8b4cf1a9bdaa385620419b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-16 14:56:03 -0400

    Update solution for set-matrix-zeroes

--- leetcode/set-matrix-zeroes.py
+++ leetcode/set-matrix-zeroes.py
@@ -3,21 +3,38 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowTracker = [False] * len(matrix)
-        colTracker = [False] * len(matrix[0])
-
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                if (matrix[row][col] == 0):
-                    rowTracker[row] = True
-                    colTracker[col] = True
         
-        # print(rowTracker)
-        # print(colTracker)
+        ROWS, COLS = len(matrix), len(matrix[0])
+        firstRowZero = False
 
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                if (rowTracker[row] == True):
-                    matrix[row][col] = 0
-                elif (colTracker[col] == True):
-                    matrix[row][col] = 0
+        # Determine which rows and columns should be converted to 0:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (matrix[r][c] == 0):
+                    
+                    # Flag which columns need to be a zero
+                    matrix[0][c] = 0
+
+                    # Flag which rows need to be a zero
+                    if r > 0:
+                        matrix[r][0] = 0
+                    
+                    # Flag if the first row needs to be a zero
+                    else:
+                        firstRowZero = True
+        
+        # Convert most of the squares to zero (skip first row and first column)
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+        
+        # Convert the first column to 0, if necessary
+        if (matrix[0][0] == 0):
+            for r in range(ROWS):
+                matrix[r][0] = 0
+
+        # Convert the first row to 0, if necessary
+        if (firstRowZero):
+            for c in range(COLS):
+                matrix[0][c] = 0
