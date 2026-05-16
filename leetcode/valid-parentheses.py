a5d69c52b483c1a70129c8237ab1c73abf6c96b2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-16 15:30:37 -0400

    Add solution for valid-parentheses

---
+++ leetcode/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        OPEN_BRACKET = ["[", "{", "("]
+        stack = []
+
+        for bracket in s:
+            if (bracket in OPEN_BRACKET):
+                stack.append(bracket)
+            
+            elif (len(stack) > 0 and stack[-1] == "[" and bracket == "]"):
+                stack.pop()
+            
+            elif (len(stack) > 0 and stack[-1] == "(" and bracket == ")"):
+                stack.pop()
+            
+            elif (len(stack) > 0 and stack[-1] == "{" and bracket == "}"):
+                stack.pop()
+            
+            else:
+                return False
+        
+        return len(stack) == 0
