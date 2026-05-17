571a910a79201c8f51f78edeeafca371321d201a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-17 17:46:30 -0400

    Update solution for valid-sudoku

--- leetcode/valid-sudoku.py
+++ leetcode/valid-sudoku.py
@@ -1,29 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rowMap = {}
-        colMap = {}
-        boxMap = {}
+        # Initialize the HashMap's that will record the data of duplicates
+        rowHash = {}
+        colHash = {}
+        boxHash = {}
 
         for idx in range(9):
-            rowMap[idx] = []
-            colMap[idx] = []
+            rowHash[idx] = []
+            colHash[idx] = []
         
         for idx in range(3):
             for jdx in range(3):
-                boxMap[(idx, jdx)] = []
+                boxHash[(idx, jdx)] = []
         
+        # Parse through the Suduko board looking for duplicates
         for row in range(9):
             for col in range(9):
                 if (board[row][col] == "."):
                     continue
-
-                if ((board[row][col] in rowMap[row]) or
-                    (board[row][col] in colMap[col]) or
-                    (board[row][col] in boxMap[(row // 3, col // 3)])):
-                    return False
                 
-                rowMap[row].append(board[row][col])
-                colMap[col].append(board[row][col])
-                boxMap[(row // 3, col // 3)].append(board[row][col])
+                elif ((board[row][col] in rowHash[row] ) or
+                        (board[row][col] in colHash[col]) or
+                        (board[row][col] in boxHash[(row // 3, col // 3)])):
+                        return False
+                
+                else:
+                    rowHash[row].append(board[row][col])
+                    colHash[col].append(board[row][col])
+                    boxHash[(row // 3, col // 3)].append(board[row][col])
         
         return True
