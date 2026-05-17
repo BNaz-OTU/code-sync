aa7244c760a5c431de0a2490acbbf3cfd2e4d558
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-17 16:06:45 -0400

    Update solution for walking-robot-simulation

--- leetcode/walking-robot-simulation.py
+++ leetcode/walking-robot-simulation.py
@@ -1,28 +1,26 @@
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+                    # N - 0.  E - 1.  S - 2.   W - 3
+        direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        dirIdx = 0
         posX = 0
         posY = 0
         maxDist = 0
-                    # N = 0. E = 1.   S = 2.   W = 3
-        direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        count_dir = 0
-        obs_set = set()
 
-        # Convert obstacles into a set, to get a more efficient search time
-        for ob in obstacles:
-            obs_set.add((ob[0], ob[1]))
+        # Convert Obstacle array into 'set()' to get O(1) search time, better than O(n) search time in an array
+        obs_set = set()
+        for obsX, obsY in obstacles:
+            obs_set.add((obsX, obsY))
 
         for command in commands:
-            print(posX, posY)
             if (command == -1):
-                count_dir += 1
+                dirIdx += 1
             
             elif (command == -2):
-                count_dir -= 1
+                dirIdx -= 1
             
             else:
-                idx = count_dir % 4
-
+                idx = dirIdx % 4
                 for _ in range(command):
                     posX += direction[idx][0]
                     posY += direction[idx][1]
@@ -31,8 +29,7 @@ class Solution:
                         posX -= direction[idx][0]
                         posY -= direction[idx][1]
                         break
-                
+
                 maxDist = max(maxDist, (posX ** 2) + (posY ** 2))
         
-        print(posX, posY)
         return maxDist
