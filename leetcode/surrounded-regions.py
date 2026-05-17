23121c2ad00cf01664218ad34584c447d49ac117
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-17 17:57:48 -0400

    Update solution for surrounded-regions

--- leetcode/surrounded-regions.py
+++ leetcode/surrounded-regions.py
@@ -3,22 +3,22 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-
         ROWS, COLS = len(board), len(board[0])
 
         def dfs(row, col):
-            if ((row < 0 or row >= ROWS) or 
+            if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
                 (board[row][col] == "X") or
                 (board[row][col] == "T")):
                 return
-            
+
             board[row][col] = "T"
 
             dfs(row + 1, col)
             dfs(row - 1, col)
             dfs(row, col + 1)
             dfs(row, col - 1)
+
 
         for row in range(ROWS):
             dfs(row, 0)
@@ -28,11 +28,10 @@ class Solution:
             dfs(0, col)
             dfs(ROWS - 1, col)
         
-
         for row in range(ROWS):
             for col in range(COLS):
                 if (board[row][col] == "O"):
                     board[row][col] = "X"
                 
-                elif(board[row][col] == "T"):
+                elif (board[row][col] == "T"):
                     board[row][col] = "O"
