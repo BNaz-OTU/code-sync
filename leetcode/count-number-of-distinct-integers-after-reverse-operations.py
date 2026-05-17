093ec9ae256c9e0cd72e2f2bf4e9fc6cc5ac91b0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-17 16:12:37 -0400

    Update solution for count-number-of-distinct-integers-after-reverse-operations

--- leetcode/count-number-of-distinct-integers-after-reverse-operations.py
+++ leetcode/count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,8 +1,10 @@
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
         for idx in range(len(nums)):
-            val = nums[idx]
-            update_val = int(str(val)[::-1])
-            nums.append(update_val)
+            # Convert number to a string and reverse it
+            val = str(nums[idx])[::-1]
+
+            # Add number to the array, (since its a string need to re-convert back to 'int')
+            nums.append(int(val))
         
         return len(set(nums))
