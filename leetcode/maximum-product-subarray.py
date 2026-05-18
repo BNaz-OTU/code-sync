6baacb7e61da6c73f74bdea4203e390d2ca1b42c
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-18 14:19:39 -0400

    Update solution for maximum-product-subarray

--- leetcode/maximum-product-subarray.py
+++ leetcode/maximum-product-subarray.py
@@ -1,7 +1,7 @@
-# USED SOLN: https://www.youtube.com/watch?v=lXVy6YWFcRM
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         res = max(nums)
+
         curMin, curMax = 1, 1
 
         for n in nums:
@@ -9,9 +9,9 @@ class Solution:
                 curMin, curMax = 1, 1
                 continue
             
-            temp = curMax * n
+            tmp = curMax * n
             curMax = max(n * curMax, n * curMin, n)
-            curMin = min(temp, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
             res = max(res, curMax)
         
         return res
