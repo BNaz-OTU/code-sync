08676749659948c0cd5c31ce625d29af39e562f6
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-18 12:09:15 -0400

    Update solution for palindromic-substrings

--- leetcode/palindromic-substrings.py
+++ leetcode/palindromic-substrings.py
@@ -1,22 +1,23 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0
-
+        paliCounter = 0
+        
         for idx in range(len(s)):
-            # Odd Case (Counts all the odd cases: "a", "aaa", "aaaaa")
+            # Odd Case
             left = idx
             right = idx
+
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                count += 1
+                paliCounter += 1
                 left -= 1
                 right += 1
-            
-            # Even Case (Counts all the even cases: "aa", "aaaa")
-            left = idx 
+
+            # Even Case
+            left = idx
             right = idx + 1
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                count += 1
+                paliCounter += 1
                 left -= 1
                 right += 1
-            
-        return count
+        
+        return paliCounter
