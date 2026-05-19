dae45a3170ed92fba3cb506126c693363e513909
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-19 18:56:30 -0400

    Update solution for merge-intervals

--- leetcode/merge-intervals.py
+++ leetcode/merge-intervals.py
@@ -1,16 +1,33 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        sort_i = sorted(intervals)
-
-        final = [sort_i[0]]
-
-        for start, end in sort_i[1:]:
-            prev = final[-1][1]
-
-            if (start <= prev):
-                if (end > prev):
-                    final[-1][1] = end
-            else:
-                final.append([start, end])
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
+            return a[0] < b[0];
+        });
         
-        return final
+        vector<vector<int>> final;
+        vector<int> prev = intervals[0];
+
+        for (int idx = 1; idx < intervals.size(); idx++) {
+            vector<int> interval = intervals[idx];
+
+            if (interval[0] <= prev[1]) {
+                if (interval[1] > prev[1]) {
+                    prev[1] = interval[1];
+                } 
+            }
+
+            else {
+                final.push_back(prev);
+                prev = interval;
+            }
+        }
+
+        final.push_back(prev);
+
+        cout << final.size() << "\n";
+
+        return final;
+        
+    }
+};
