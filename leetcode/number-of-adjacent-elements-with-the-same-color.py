e5a0bb04b3267db5827a36c29093ac9695d7633f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-22 18:11:41 -0400

    Update solution for number-of-adjacent-elements-with-the-same-color

--- leetcode/number-of-adjacent-elements-with-the-same-color.py
+++ leetcode/number-of-adjacent-elements-with-the-same-color.py
@@ -2,26 +2,25 @@ class Solution:
     def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
         colorList = [0] * n
         final = []
-
         count = 0
-        for idx, color in queries:
-            prev = colorList[idx - 1] if idx > 0 else 0
-            nxt = colorList[idx + 1] if idx < n - 1 else 0
 
-            if ((colorList[idx] != 0) and (colorList[idx] == prev)):
+        for position, color in queries:
+            if ((position - 1) >= 0 and (colorList[position] == colorList[position - 1]) and colorList[position] != 0):
                 count -= 1
             
-            if ((colorList[idx] != 0) and (colorList[idx] == nxt)):
+            if ((position + 1) < len(colorList) and (colorList[position] == colorList[position + 1]) and colorList[position] != 0):
                 count -= 1
-            
-            colorList[idx] = color
+                
+            colorList[position] = color
 
-            if ((colorList[idx] != 0) and (colorList[idx] == prev)):
+            if ((position - 1) >= 0 and (colorList[position] == colorList[position - 1]) and colorList[position] != 0):
                 count += 1
             
-            if ((colorList[idx] != 0) and (colorList[idx] == nxt)):
+            if ((position + 1) < len(colorList) and (colorList[position] == colorList[position + 1]) and colorList[position] != 0):
                 count += 1
 
             final.append(count)
-        
+            # print(colorList)
+            # print(final)
+
         return final
