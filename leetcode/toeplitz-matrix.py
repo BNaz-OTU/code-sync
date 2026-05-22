0ed97d3a1fa7c71e6c7c3e0f2906b1e0a079a656
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-22 18:29:02 -0400

    Update solution for toeplitz-matrix

--- leetcode/toeplitz-matrix.py
+++ leetcode/toeplitz-matrix.py
@@ -1,16 +1,10 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         ROWS, COLS = len(matrix), len(matrix[0])
-        hashMapSubtract = {}
 
-        for row in range(ROWS):
-            for col in range(COLS):
-                if ((row - col) not in hashMapSubtract):
-                    hashMapSubtract[row - col] = set()
-                    hashMapSubtract[row - col].add(matrix[row][col])
-                
-                elif ((row - col) in hashMapSubtract and (matrix[row][col] not in hashMapSubtract[row - col])):
-                    # print(hashMapSubtract[row - col], " | ", matrix[row][col])
+        for row in range(ROWS - 1):
+            for col in range(COLS - 1):
+                if (matrix[row][col] != matrix[row + 1][col + 1]):
                     return False
         
         return True
