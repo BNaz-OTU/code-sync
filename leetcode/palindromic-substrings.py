bd26bfdf817d03bbfd868bcf50ccfb5362117df1
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-23 19:33:42 -0400

    Update solution for palindromic-substrings

--- leetcode/palindromic-substrings.py
+++ leetcode/palindromic-substrings.py
@@ -2,23 +2,19 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         paliCount = 0
 
+        def checker(left, right):
+            nonlocal paliCount
+            
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                paliCount += 1
+                left -= 1
+                right += 1
+
         for idx in range(len(s)):
             # ODD CASE
-            left = idx
-            right = idx
-
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                paliCount += 1
-                left -= 1
-                right += 1
+            checker(idx, idx)
 
             # EVEN CASE
-            left = idx
-            right = idx + 1
+            checker(idx, idx + 1)
 
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                paliCount += 1
-                left -= 1
-                right += 1
-        
         return paliCount
