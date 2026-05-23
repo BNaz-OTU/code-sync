292e51338c84770c825001d118ddcf403d7a5311
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-23 19:40:41 -0400

    Update solution for number-of-adjacent-elements-with-the-same-color

--- leetcode/number-of-adjacent-elements-with-the-same-color.py
+++ leetcode/number-of-adjacent-elements-with-the-same-color.py
@@ -1,26 +1,25 @@
 class Solution:
     def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
         colorList = [0] * n
+        counter = 0
         final = []
-        count = 0
 
         for position, color in queries:
-            if ((position - 1) >= 0 and (colorList[position] == colorList[position - 1]) and colorList[position] != 0):
-                count -= 1
+            if ((position - 1 >= 0) and (colorList[position] != 0) and (colorList[position] == colorList[position - 1])):
+                counter -= 1
             
-            if ((position + 1) < len(colorList) and (colorList[position] == colorList[position + 1]) and colorList[position] != 0):
-                count -= 1
-                
+            if ((position + 1 < len(colorList)) and (colorList[position] != 0) and (colorList[position] == colorList[position + 1])):
+                counter -= 1
+            
             colorList[position] = color
 
-            if ((position - 1) >= 0 and (colorList[position] == colorList[position - 1]) and colorList[position] != 0):
-                count += 1
+            if ((position - 1 >= 0) and (colorList[position] != 0) and (colorList[position] == colorList[position - 1])):
+                counter += 1
             
-            if ((position + 1) < len(colorList) and (colorList[position] == colorList[position + 1]) and colorList[position] != 0):
-                count += 1
+            if ((position + 1 < len(colorList)) and (colorList[position] != 0) and (colorList[position] == colorList[position + 1])):
+                counter += 1
+            
 
-            final.append(count)
-            # print(colorList)
-            # print(final)
-
+            final.append(counter)
+        
         return final
