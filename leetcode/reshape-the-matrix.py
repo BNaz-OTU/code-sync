09978132532664cc6a8711239ce8323cddfa3d47
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-23 19:46:34 -0400

    Update solution for reshape-the-matrix

--- leetcode/reshape-the-matrix.py
+++ leetcode/reshape-the-matrix.py
@@ -1,13 +1,13 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         ROWS, COLS = len(mat), len(mat[0])
+        final = []
 
         if (ROWS * COLS != r * c):
             return mat
         
-        new_mat = []
         for _ in range(r):
-            new_mat.append([0] * c)
+            final.append([0] * c)
         
         r_count = 0
         c_count = 0
@@ -17,7 +17,7 @@ class Solution:
                     c_count = 0
                     r_count += 1
                 
-                new_mat[r_count][c_count] = mat[row][col]
+                final[r_count][c_count] = mat[row][col]
                 c_count += 1
-        
-        return new_mat
+
+        return final
