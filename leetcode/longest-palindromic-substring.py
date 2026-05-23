574062fe990441b0e256cc9569bcb6f40c386122
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-23 19:00:46 -0400

    Update solution for longest-palindromic-substring

--- leetcode/longest-palindromic-substring.py
+++ leetcode/longest-palindromic-substring.py
@@ -1,29 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        finalStr = ""
+        finalS = ""
         longest = 0
+
+        def checker(left, right):
+            nonlocal longest, finalS
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (longest < (right - left + 1)):
+                    longest = (right - left + 1)
+                    finalS = s[left : right + 1]
+
+                left -= 1
+                right += 1
 
         for idx in range(len(s)):
             # ODD CASE
-            left = idx
-            right = idx
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if ((right - left + 1) > longest):
-                    longest = (right - left + 1)
-                    finalStr = s[left : right + 1]
-                
-                left -= 1
-                right += 1
-            
-            # EVEN CASE
-            left = idx
-            right = idx + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if ((right - left + 1) > longest):
-                    longest = (right - left + 1)
-                    finalStr = s[left : right + 1]
-                
-                left -= 1
-                right += 1
+            checker(idx, idx)
 
-        return finalStr
+            # EVEN CASE
+            checker(idx, idx + 1)
+        
+        return finalS
