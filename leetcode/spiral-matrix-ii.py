b1f8814ae8a2204da463d25f47518dd00e24dfa3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-23 16:38:47 -0400

    Update solution for spiral-matrix-ii

--- leetcode/spiral-matrix-ii.py
+++ leetcode/spiral-matrix-ii.py
@@ -3,38 +3,34 @@ class Solution:
         top, bot = 0, n
         left, right = 0, n
         
-        # Initialize final list to hold all the values
-        final = []
-        for idx in range(n):
-            final.append([0] * n)
+        matrix = []
+        for _ in range(n):
+            matrix.append([0] * n)
         
         counter = 1
         while top < bot and left < right:
             for idx in range(left, right):
-                final[top][idx] = counter
+                matrix[top][idx] = counter
                 counter += 1
             
             top += 1
 
             for idx in range(top, bot):
-                final[idx][right - 1] = counter
+                matrix[idx][right - 1] = counter
                 counter += 1
             
             right -= 1
 
-            if (top >= bot or left >= right):
-                return final
-
             for idx in range(right - 1, left - 1, -1):
-                final[bot - 1][idx] = counter
+                matrix[bot - 1][idx] = counter
                 counter += 1
             
             bot -= 1
 
             for idx in range(bot - 1, top - 1, -1):
-                final[idx][left] = counter 
+                matrix[idx][left] = counter
                 counter += 1
             
             left += 1
-
-        return final
+        
+        return matrix
