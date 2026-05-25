82428f1196a8bc4edf4b5edb71f7882e465987b2
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-25 14:16:11 -0400

    Update solution for number-of-islands

--- leetcode/number-of-islands.py
+++ leetcode/number-of-islands.py
@@ -2,14 +2,14 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         visit = set()
-        count = 0
+        counter = 0
 
         def dfs(row, col):
             if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
-                ((row, col) in visit) or
-                (grid[row][col] == "0")):
-                return
+                (grid[row][col] == "0") or
+                ((row, col) in visit)):
+                return 
             
             visit.add((row, col))
 
@@ -22,6 +22,6 @@ class Solution:
             for col in range(COLS):
                 if (grid[row][col] == "1" and (row, col) not in visit):
                     dfs(row, col)
-                    count += 1
+                    counter += 1
         
-        return count
+        return counter
