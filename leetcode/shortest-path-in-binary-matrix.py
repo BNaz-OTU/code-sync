01f435c1551d4b10a926cb72d9a90ae86cf19de3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-30 13:05:38 -0400

    Update solution for shortest-path-in-binary-matrix

--- leetcode/shortest-path-in-binary-matrix.py
+++ leetcode/shortest-path-in-binary-matrix.py
@@ -1,30 +1,30 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if (grid[0][0] == 1 or grid[-1][-1]):
+        if (grid[0][0] == 1 or grid[-1][-1] == 1):
             return -1
         
-        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [-1, -1], [1, -1]]
+        heap = []
+        heap.append([1, 0, 0])
         visit = set()
-        heap = [(1, 0, 0)]
+        neighbour = [[1, 0], [0, 1], [-1, 0], [0, -1], [-1, -1], [1, 1], [-1, 1], [1, -1]]
 
         while len(heap) > 0:
-            for _ in range(len(heap)):
-                dist, row, col = heappop(heap)
+            dist, row, col = heappop(heap)
 
-                if (row == (len(grid) - 1) and col == (len(grid) - 1)):
-                    return dist
+            if (row == (len(grid) - 1) and col == (len(grid) - 1)):
+                return dist
 
-                for dr, dc in neighbours:
-                    n_row = row + dr
-                    n_col = col + dc
+            for dr, dc in neighbour:
+                n_row = dr + row
+                n_col = dc + col
 
-                    if ((n_row < 0 or n_row >= len(grid)) or 
-                        (n_col < 0 or n_col >= len(grid)) or 
-                        ((n_row, n_col) in visit) or
-                        (grid[n_row][n_col] == 1)):
-                        continue
-                    
-                    visit.add((n_row, n_col))
-                    heappush(heap, (dist + 1, n_row, n_col))
+                if ((n_row < 0 or n_row >= len(grid)) or
+                    (n_col < 0 or n_col >= len(grid)) or
+                    (grid[n_row][n_col] == 1) or
+                    ((n_row, n_col) in visit)):
+                    continue
+                
+                heappush(heap, [dist + 1, n_row, n_col])
+                visit.add((n_row, n_col))
         
         return -1
