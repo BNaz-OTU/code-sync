6f3c960e32c9bdd3fbebb52bf228c781689b9d8e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-31 16:20:32 -0400

    Update solution for rotting-oranges

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -1,16 +1,15 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
         fresh = 0
         rotten = deque()
-        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         visit = set()
+        ROWS, COLS = len(grid), len(grid[0])
+        neighbors = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (grid[row][col] == 1):
                     fresh += 1
-                
                 elif (grid[row][col] == 2):
                     rotten.append((row, col))
                     visit.add((row, col))
@@ -20,13 +19,14 @@ class Solution:
         
         time = 0
         while len(rotten) > 0:
+
             if (fresh == 0):
-                break 
+                return time
 
             for _ in range(len(rotten)):
                 row, col = rotten.popleft()
 
-                for dr, dc in neighbours:
+                for dr, dc in neighbors:
                     n_row = row + dr
                     n_col = col + dc
 
@@ -37,12 +37,11 @@ class Solution:
                         continue
                     
                     visit.add((n_row, n_col))
-                    rotten.append([n_row, n_col])
+                    rotten.append((n_row, n_col))
                     fresh -= 1
-
+            
             time += 1
         
-        if (fresh != 0):
-            return -1
-        
-        return time
+        if (fresh == 0):
+            return time
+        return -1
