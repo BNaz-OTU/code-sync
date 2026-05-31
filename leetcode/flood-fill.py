bff5bb0405d16e53f8d4f84780c7fd6f91fd0480
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-31 15:28:37 -0400

    Update solution for flood-fill

--- leetcode/flood-fill.py
+++ leetcode/flood-fill.py
@@ -1,15 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        target = image[sr][sc]
         ROWS, COLS = len(image), len(image[0])
         visit = set()
+        target = image[sr][sc]
 
         def dfs(row, col):
             if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
-                (image[row][col] != target) or
-                ((row, col) in visit)):
-                return 
+                ((row, col) in visit) or
+                (image[row][col] != target)):
+                return
             
             image[row][col] = color
             visit.add((row, col))
@@ -18,6 +18,7 @@ class Solution:
             dfs(row - 1, col)
             dfs(row, col + 1)
             dfs(row, col - 1)
-        
+
         dfs(sr, sc)
+
         return image
