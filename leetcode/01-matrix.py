fbc333ea61ad03428db2dbbcb1bcf28c2eabba54
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-05-31 15:54:11 -0400

    Update solution for 01-matrix

--- leetcode/01-matrix.py
+++ leetcode/01-matrix.py
@@ -3,34 +3,33 @@ class Solution:
         ROWS, COLS = len(mat), len(mat[0])
         queue = deque()
         visit = set()
-        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        dist = 1
+        neighbors = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (mat[row][col] == 0):
-                    queue.append([row, col])
+                    queue.append((row, col))
                     visit.add((row, col))
         
-        count = 1
         while len(queue) > 0:
-            print(queue)
+
             for _ in range(len(queue)):
                 row, col = queue.popleft()
 
-                for dr, dc in neighbours:
+                for dr, dc in neighbors:
                     n_row = row + dr
                     n_col = col + dc
 
                     if ((n_row < 0 or n_row >= ROWS) or
                         (n_col < 0 or n_col >= COLS) or
                         ((n_row, n_col) in visit)):
-                        # print("h")
                         continue
                     
+                    mat[n_row][n_col] = dist
                     visit.add((n_row, n_col))
-                    queue.append([n_row, n_col])
-                    mat[n_row][n_col] = count
-            
-            count += 1
+                    queue.append((n_row, n_col))
+
+            dist += 1
         
         return mat
