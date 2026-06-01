98a7b0ef935d0ee14f25de2a1109087d2867e821
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-01 12:15:44 -0400

    Update solution for binary-tree-right-side-view

--- leetcode/binary-tree-right-side-view.py
+++ leetcode/binary-tree-right-side-view.py
@@ -4,31 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if (root is None):
+        if root is None:
             return []
-        final = []
+            
         queue = deque()
         queue.append(root)
-
-        while (len(queue) > 0):
-            levels = []
-            len_q = len(queue)
-
-            for _ in range(len_q):
+        final = []
+        
+        while len(queue) > 0:
+            temp = []
+            for _ in range(len(queue)):
                 node = queue.popleft()
+                temp.append(node.val)
 
-                if (node.left):
+                if (node.left is not None):
                     queue.append(node.left)
                 
-                if (node.right):
+                if (node.right is not None):
                     queue.append(node.right)
                 
-                levels.append(node.val)
-            
-            final.append(levels[-1])
-        
+            final.append(temp[-1])
+
         return final
