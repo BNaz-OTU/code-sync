ccb4420e59d500fe6f48f1150ef777950c1edb1f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-03 20:26:58 -0400

    Update solution for word-search

--- leetcode/word-search.py
+++ leetcode/word-search.py
@@ -1,41 +1,42 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS = len(board)
-        COLS = len(board[0])
-        visited = set()
-        flag = False
+        ROWS, COLS = len(board), len(board[0])
+        # visit = set()
 
-        def dfs(row, col, visited, wordIdx):
-            if (len(word) == wordIdx):
-                return True
-            
-            if ((row >= ROWS or row < 0) or
-                (col >= COLS or col < 0) or
-                ((row, col) in visited) or
-                (wordIdx >= len(word)) or
-                (board[row][col] != word[wordIdx])):
+        # [
+        # ["A","B","C","E"],
+        # ["S","F","E","S"],
+        # ["A","D","E","E"]
+        # ]
+        # "ABCE
+        # SEEEFS"
+
+        def dfs(row, col, idx, visit):
+            if ((row < 0 or row >= ROWS) or
+                (col < 0 or col >= COLS) or
+                ((row, col) in visit) or
+                (board[row][col] != word[idx])):
                 return False
             
-            visited.add((row, col))
-            wordIdx += 1
-
-            final = (
-                dfs(row + 1, col, visited, wordIdx) or
-                dfs(row - 1, col, visited, wordIdx) or
-                dfs(row, col + 1, visited, wordIdx) or
-                dfs(row, col - 1, visited, wordIdx)
+            if (idx == len(word) - 1):
+                return True
+            
+            visit.add((row, col))
+            
+            return (
+                dfs(row + 1, col, idx + 1, visit.copy()) or
+                dfs(row - 1, col, idx + 1, visit.copy()) or
+                dfs(row, col + 1, idx + 1, visit.copy()) or
+                dfs(row, col - 1, idx + 1, visit.copy())
             )
+            
 
-            visited.remove((row, col))
 
-            return final
-
-        for idx in range(ROWS):
-            for jdx in range(COLS):
-                if (board[idx][jdx] == word[0]):
-                    flag = dfs(idx, jdx, visited, 0)
-
-                    if (flag == True):
-                        return flag
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (board[row][col] == word[0]):
+                    # visit = set()
+                    if (dfs(row, col, 0, set())):
+                        return True
         
-        return flag
+        return False
