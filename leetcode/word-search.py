c884d2c53cf68a2b1e91d8bdbec9c6f454c08ccd
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-06 16:12:53 -0400

    Update solution for word-search

--- leetcode/word-search.py
+++ leetcode/word-search.py
@@ -1,42 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        # visit = set()
+        visit = set()
 
-        # [
-        # ["A","B","C","E"],
-        # ["S","F","E","S"],
-        # ["A","D","E","E"]
-        # ]
-        # "ABCE
-        # SEEEFS"
+        def dfs(row, col, idx):
+            if (idx == len(word)):
+                return True
 
-        def dfs(row, col, idx, visit):
             if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
-                ((row, col) in visit) or
-                (board[row][col] != word[idx])):
+                (board[row][col] != word[idx]) or
+                ((row, col) in visit)):
                 return False
             
-            if (idx == len(word) - 1):
-                return True
-            
             visit.add((row, col))
-            
-            return (
-                dfs(row + 1, col, idx + 1, visit.copy()) or
-                dfs(row - 1, col, idx + 1, visit.copy()) or
-                dfs(row, col + 1, idx + 1, visit.copy()) or
-                dfs(row, col - 1, idx + 1, visit.copy())
-            )
+
+            val = (dfs(row + 1, col, idx + 1) or 
+                    dfs(row - 1, col, idx + 1) or 
+                    dfs(row, col + 1, idx + 1) or 
+                    dfs(row, col - 1, idx + 1))
+
+            visit.remove((row, col))
+
+            return val
+
+
             
 
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (board[row][col] == word[0]):
-                    # visit = set()
-                    if (dfs(row, col, 0, set())):
+                    if (dfs(row, col, 0)):
                         return True
         
         return False
