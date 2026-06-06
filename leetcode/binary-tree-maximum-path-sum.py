a7f9f983b1ee092eccf5db022aaf0016f17e7621
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-06 17:13:44 -0400

    Update solution for binary-tree-maximum-path-sum

--- leetcode/binary-tree-maximum-path-sum.py
+++ leetcode/binary-tree-maximum-path-sum.py
@@ -9,17 +9,21 @@ class Solution:
         res = [root.val]
 
         def dfs(root):
-            if not root:
-                return 0 
+            if (not root):
+                return 0
             
+            # Get the Left and Right node values
             leftMax = dfs(root.left)
             rightMax = dfs(root.right)
+
+            # If they are negative just set them to '0'
             leftMax = max(leftMax, 0)
             rightMax = max(rightMax, 0)
 
-            res[0] = max(res[0], root.val + leftMax + rightMax) 
+            # Compute max path sum with split
+            res[0] = max(res[0], root.val + leftMax + rightMax)
 
             return root.val + max(leftMax, rightMax)
-        
+            
         dfs(root)
         return res[0]
