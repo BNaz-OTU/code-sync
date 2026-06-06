baca366bd4c60c9435f2ee0bc973c0a172ad71d4
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-06 12:30:01 -0400

    Update solution for subsets-ii

--- leetcode/subsets-ii.py
+++ leetcode/subsets-ii.py
@@ -2,23 +2,20 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         final = []
         sort_nums = sorted(nums)
-        
         def dfs(path, idx):
             if (idx >= len(sort_nums)):
-                if (path not in final):
-                    final.append(path.copy())
+                final.append(path.copy())
                 return
             
             path.append(sort_nums[idx])
             dfs(path, idx + 1)
 
-            path.pop()
             count = idx
-
-            while count < len(nums) and sort_nums[count] == sort_nums[idx]:
+            while count < len(sort_nums) and sort_nums[idx] == sort_nums[count]:
                 count += 1
-
+            
+            path.pop()
             dfs(path, count)
-        
+
         dfs([], 0)
         return final
