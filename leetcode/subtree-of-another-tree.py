2b13f7f1c401ebb13de3da32c4b9f4aafd0e065c
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-07 12:41:27 -0400

    Update solution for subtree-of-another-tree

--- leetcode/subtree-of-another-tree.py
+++ leetcode/subtree-of-another-tree.py
@@ -6,23 +6,22 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def deeperdfs(root, subroot):
-            if (root is None and subroot is None):
+        def deeperdfs(root, subRoot):
+            if (root is None and subRoot is None):
                 return True
             
-            if (root is None or subroot is None or root.val != subroot.val):
+            if (root is None or subRoot is None or root.val != subRoot.val):
                 return False
             
-            return (deeperdfs(root.left, subroot.left) and deeperdfs(root.right, subroot.right))
-            
-        def dfs(root):
+            return (deeperdfs(root.left, subRoot.left) and deeperdfs(root.right, subRoot.right))
+
+        def dfs(root, subRoot):
             if (root is None):
                 return False
             
-            if (root.val == subRoot.val):
-                if (deeperdfs(root, subRoot)):
-                    return True
+            if (root.val == subRoot.val and deeperdfs(root, subRoot)):
+                return True
             
-            return (dfs(root.left) or dfs(root.right))
+            return (dfs(root.left, subRoot) or dfs(root.right, subRoot))
         
-        return (dfs(root))
+        return dfs(root, subRoot)
