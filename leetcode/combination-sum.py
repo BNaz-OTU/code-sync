c4e5d328ce2e2f2cce44ed53bf0337e3095d980e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-07 15:28:49 -0400

    Update solution for combination-sum

--- leetcode/combination-sum.py
+++ leetcode/combination-sum.py
@@ -1,20 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         final = []
-
         def dfs(path, idx):
-            if (sum(path) > target or idx >= len(candidates)):
+            if (idx >= len(candidates) or sum(path) > target):
                 return
             
             if (sum(path) == target):
                 final.append(path.copy())
                 return
-
+            
             path.append(candidates[idx])
             dfs(path, idx)
 
             path.pop()
             dfs(path, idx + 1)
-            
+        
         dfs([], 0)
         return final
