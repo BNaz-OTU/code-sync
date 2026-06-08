991a4fd1f4d2ff3f1ab4475a0a685401514d324b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-08 19:46:41 -0400

    Update solution for find-minimum-in-rotated-sorted-array

--- leetcode/find-minimum-in-rotated-sorted-array.py
+++ leetcode/find-minimum-in-rotated-sorted-array.py
@@ -2,19 +2,26 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
-        minNum = nums[0]
+        final = nums[0]
 
+        idx = 0
+        print(f"Starting - Left: {nums[left]} | Right: {nums[right]} | Current Minimum: {final}")
         while left <= right:
-            if (nums[left] <= nums[right]):
-                minNum = min(minNum, nums[left])
-            
-            middle = right - left
+            idx += 1
+            if (nums[left] < nums[right]):
+                print(f"{idx}. {nums[left]} < {nums[right]} | Minimum Num: {final} & {nums[left]}")
+                final = min(final, nums[left])
+                break
 
-            minNum = min(minNum, nums[middle])
-            if (nums[middle] > nums[right]):
+            middle = (left + right) // 2
+            final = min(final, nums[middle])
+
+            if (nums[middle] >= nums[left]):
+                print(f"{idx}. {nums[middle]} >= {nums[left]} | Minimum Num: {final}  --- Make left bigger since we are at a possible edge")
                 left = middle + 1
-            else:
-                right = middle - 1
             
+            else:
+                print(f"{idx}. {nums[middle]} < {nums[left]} | Minimum Num: {final}  --- Make right smaller since middle number is smaller than left number")
+                right = middle - 1
         
-        return minNum
+        return final
