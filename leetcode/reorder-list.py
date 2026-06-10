3067d54e05b8bf1ec696ef7d80f83789fa69ba59
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-09 22:19:12 -0400

    Update solution for reorder-list

--- leetcode/reorder-list.py
+++ leetcode/reorder-list.py
@@ -8,73 +8,15 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-
-        slow = head
-        fast = head
-
+        # Find middle
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        # print(f'Fast: {fast}')
-        # print(f"Slow: {slow}")
-        # print(f"Head: {head}")
-
-        last_half = slow.next
-        slow.next = None
-        prev = None
-
-        # print(slow)
-        # print(head)
-        # print(last_half)
-
-        while last_half:
-            temp_next = last_half.next
-            last_half.next = prev
-            prev = last_half
-            last_half = temp_next
-        
-        # print(prev)
-        # print(slow)
-        # print(head)
-
-        # print
-
-        first, second = head, prev
-
-        while second:
-            temp_1, temp_2 = first.next, second.next
-            first.next = second
-            second.next = temp_1
-            first, second = temp_1, temp_2
-
-        # [1, 2,` 3]
-        #  | /
-        # [4, 3]`
-        
-        # ---------------------------------------------------------------------------------------------------
-        """
-        # Find the middle
-        slow = head
-        fast = head.next
-
-        while (fast and fast.next):
-            slow = slow.next
-            fast = fast.next.next
-        
-        print(fast)
-        print(slow)
-
         # Reverse second half
         second = slow.next
-        slow.next = None
-        prev = None
-
-        print("--------------")
-        print(f"Second: {second}")
-        print(f"Slow: {slow} | Slow.next: {slow.next}")
-        print(f"Prev: {prev}")
-
+        prev = slow.next = None
         while second:
             tmp = second.next
             second.next = prev
@@ -82,12 +24,9 @@ class Solution:
             second = tmp
         
         # Merge two halfs
-        first = head
-        second = prev
-
+        first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-        """
