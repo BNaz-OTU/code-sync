f066c1a3fa9bfd2b1ac538ccf18d9dda7a4626b0
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-10 18:14:00 -0400

    Update solution for task-scheduler

--- leetcode/task-scheduler.py
+++ leetcode/task-scheduler.py
@@ -1,30 +1,35 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         hashTask = {}
-        heap = []
         queue = deque()
 
         for task in tasks:
-            if (task not in hashTask):
+            if task not in hashTask:
                 hashTask[task] = 0
             
-            hashTask[task] -= 1
-
-        for val in hashTask.values():
-            heap.append(val)
+            hashTask[task] += 1
         
-        heapify(heap)
+        heap = []
+
+        for task, count in hashTask.items():
+            heappush(heap, [-count, task])
         
         time = 0
         while heap or queue:
-            time += 1
-            if (heap):
-                val = heappop(heap)
-
-                if ((val + 1) < 0):
-                    queue.append([time + n, val + 1])
+            # print(f"Heap: {heap}")
+            # print(f"Queue: {queue}")
+            # print(time)
+            # print()
+            if queue and queue[0][0] == time:
+                _, miniContainer = queue.popleft()
+                heappush(heap, [miniContainer[0], miniContainer[1]])
             
-            if (queue and queue[0][0] == time):
-                heappush(heap, queue.popleft()[1])
-                
+            if heap:
+                count, task = heappop(heap)
+                count += 1
+                if (count != 0):  
+                    queue.append([time + n + 1, (count, task)])
+
+            time += 1
+        
         return time
