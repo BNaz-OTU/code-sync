4013843f113e1a712a96a2c01db3f0c2a0a3314f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-09 20:34:43 -0400

    Update solution for merge-two-sorted-lists

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -5,22 +5,33 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if (list1 is None and list2 is None):
+            return None
+        
         dummyNode = ListNode()
         head = dummyNode
 
         while list1 and list2:
-            if (list1.val < list2.val):
+            if (list1.val <= list2.val):
+                temp = list1.next
                 dummyNode.next = list1
-                list1 = list1.next
+                list1.next = list2
+                list1 = temp
+                dummyNode = dummyNode.next
             else:
+                temp = list2.next
                 dummyNode.next = list2
-                list2 = list2.next
-            
-            dummyNode = dummyNode.next
+                list2.next = list1
+                list2 = temp
+                dummyNode = dummyNode.next
         
-        if (list1 is None):
+        if list1 is None and list2 is None:        
+            return head.next
+        
+        if list1 is None and list2 is not None:
             dummyNode.next = list2
-        else:
-            dummyNode.next = list1
+            return head.next
         
-        return head.next
+        if list2 is None and list1 is not None:
+            dummyNode.next = list1
+            return head.next
