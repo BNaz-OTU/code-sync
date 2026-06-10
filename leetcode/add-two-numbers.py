f658de0e1c0118420e78938f7b224ac61df4d31f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-10 11:05:54 -0400

    Update solution for add-two-numbers

--- leetcode/add-two-numbers.py
+++ leetcode/add-two-numbers.py
@@ -5,46 +5,78 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        carry = 0
+        dummyNode = ListNode()
+        head = dummyNode
 
-        # USE FOR REFERENCE: https://www.youtube.com/watch?v=wgFPrzTjm7s
+        while l1 and l2:
+            print(head)
+            new_val = l1.val + l2.val + carry
+            carry = 0
+            if new_val >= 10:
+                carry = new_val // 10
+                new_val = new_val % 10
+            
+            print(carry)
+            
+            node = ListNode(new_val)
+            dummyNode.next = node
+            dummyNode = dummyNode.next
 
-        list1 = ""
-        list2 = ""
+            l1 = l1.next
+            l2 = l2.next
+        
+        while l1 and carry:
+            new_val = l1.val + carry
+            carry = 0
+            if new_val >= 10:
+                carry = new_val // 10
+                new_val = new_val % 10
+            
+            print(carry)
+            
+            node = ListNode(new_val)
+            dummyNode.next = node
+            dummyNode = dummyNode.next
 
-        # 1. Store each value in the nodes in a list
+            l1 = l1.next
+        
+        while l2 and carry:
+            new_val = l2.val + carry
+            carry = 0
+            if new_val >= 10:
+                carry = new_val // 10
+                new_val = new_val % 10
+            
+            print(carry)
+            
+            node = ListNode(new_val)
+            dummyNode.next = node
+            dummyNode = dummyNode.next
+
+            l2 = l2.next
+        
+        if carry:
+            node = ListNode(carry)
+            dummyNode.next = node
+            
         while l1:
-            list1 += str(l1.val)
+            new_val = l1.val
+            
+            node = ListNode(new_val)
+            dummyNode.next = node
+            dummyNode = dummyNode.next
+
             l1 = l1.next
         
         while l2:
-            list2 += str(l2.val)
+            new_val = l2.val
+            
+            node = ListNode(new_val)
+            dummyNode.next = node
+            dummyNode = dummyNode.next
+
             l2 = l2.next
-        
-        # DEBUG
-        print(f"List 1: {list1} | List 2: {list2}")
 
-        # 2. Reverse the Lists and add them together
-        r_list1 = list1[::-1]
-        r_list2 = list2[::-1]
-
-        total = int(r_list1) + int(r_list2)
-
-        # DEBUG
-        print(total)
-
-        # 3. Convert to string, reverse the string and make a new linked list
-        r_total = str(total)[::-1]
-
-        # DEBUG
-        print(r_total)
-        temp = ListNode()
-        head = temp
-
-        print(temp.val, temp.next)
-
-        for val in r_total:
-            c = ListNode(val=int(val))
-            temp.next = c
-            temp = temp.next
-        
         return head.next
