309b4da444ed80f4d3da0bf6544b4e5a2eb4350b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-10 17:27:59 -0400

    Update solution for course-schedule-ii

--- leetcode/course-schedule-ii.py
+++ leetcode/course-schedule-ii.py
@@ -2,8 +2,8 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adjList = {}
         final = []
-        visit = set()
-        cycle = set()
+        visit = set() # optimize, if we seen a course that isn't in the same course stream and know its not in a cycle we can return quickly 
+        cycle = set() # if there happens to be a course cycle this set will declare it and return the loop immediately
 
         def dfs(crs):
             if (crs in cycle):
@@ -11,28 +11,26 @@ class Solution:
             
             if (crs in visit):
                 return True
-
-            cycle.add(crs)
+            
             visit.add(crs)
+            cycle.add(crs)
 
             for new_crs in adjList[crs]:
                 if (not dfs(new_crs)):
                     return False
-            
-            cycle.remove(crs)
-            if crs not in final:
-                final.append(crs)
                 
+            final.append(crs)
+            cycle.remove(crs)
             return True
 
         for crs in range(numCourses):
             adjList[crs] = []
-
+        
         for crs, pre in prerequisites:
             adjList[crs].append(pre)
         
         for crs in range(numCourses):
             if (not dfs(crs)):
                 return []
-            
+        
         return final
