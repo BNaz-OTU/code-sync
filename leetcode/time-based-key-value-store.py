a772ea3d32de97236195a2fb086f96ffd8333fed
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-12 17:59:21 -0400

    Update solution for time-based-key-value-store

--- leetcode/time-based-key-value-store.py
+++ leetcode/time-based-key-value-store.py
@@ -1,45 +1,40 @@
 class TimeMap:
 
     def __init__(self):
-        self.dictKeyToVal = {}
-        self.dictKeyToTime = {}
+        self.hashMap = {}
         
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if (key not in self.dictKeyToTime):
-            self.dictKeyToTime[key] = [timestamp]
-            self.dictKeyToVal[key] = [value]
-        else:
-            self.dictKeyToTime[key].append(timestamp)
-            self.dictKeyToVal[key].append(value)
+        if (key not in self.hashMap):
+            self.hashMap[key] = []
+        
+        self.hashMap[key].append([timestamp, value])
         
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.dictKeyToTime:
+        if key not in self.hashMap:
             return ""
 
-        arr = self.dictKeyToTime[key]
+        arr = self.hashMap[key]
         left = 0
         right = len(arr) - 1
-        prev = None
+        lastVal = ""
 
-        while (left <= right):
+        while left <= right:
             middle = (left + right) // 2
 
-            if (arr[middle] == timestamp):
-                return self.dictKeyToVal[key][middle]
+            if (arr[middle][0] == timestamp):
+                return arr[middle][1]
             
-            elif (arr[middle] > timestamp):
-                right = middle - 1
+            elif (arr[middle][0] < timestamp):
+                lastVal = arr[middle][1]
+                left = middle + 1
             
             else:
-                prev = middle
-                left = middle + 1
+                right = middle - 1
         
-        if prev is  None:
-            return ""
+        return lastVal
         
-        return self.dictKeyToVal[key][prev]
 
 
 # Your TimeMap object will be instantiated and called as such:
