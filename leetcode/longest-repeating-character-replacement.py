5a849b7d20f92b3ff38392f897736a386c222f7a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-12 19:41:58 -0400

    Update solution for longest-repeating-character-replacement

--- leetcode/longest-repeating-character-replacement.py
+++ leetcode/longest-repeating-character-replacement.py
@@ -1,28 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left, right = 0, 0
-        hashChar = {}
+        hashMap = {}
+        left = 0
         maxLen = 0
 
-        while right < len(s):
-            if (s[right] in hashChar):
-                hashChar[s[right]] += 1
+        for right in range(len(s)):
+            if (s[right] not in hashMap):
+                hashMap[s[right]] = 0
             
-            else:
-                hashChar[s[right]] = 1
-            
-            largestChar = max(hashChar.values())
-            # print(hashChar, s[right], largestChar)
-            while (right - left + 1) - largestChar > k:
-                hashChar[s[left]] -= 1
+            hashMap[s[right]] += 1
 
-                if (hashChar[s[left]] == 0):
-                    del hashChar[s[left]]
-                
-                largestChar = max(hashChar.values())
+            while (right - left + 1) - max(hashMap.values()) > k:
+                hashMap[s[left]] -= 1
                 left += 1
-
+            
             maxLen = max(maxLen, right - left + 1)
-            right += 1
 
         return maxLen
