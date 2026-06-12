182a0903ec9ab028e478b41763c048960f8821ba
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-12 12:11:57 -0400

    Update solution for search-a-2d-matrix

--- leetcode/search-a-2d-matrix.py
+++ leetcode/search-a-2d-matrix.py
@@ -1,35 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row_middle = len(matrix) // 2
         top = 0
         bot = len(matrix) - 1
 
-        while (top <= bot):
-            row_middle = (top + bot) // 2
-            print(matrix[row_middle], top, bot)
+        while top <= bot:
+            middle = (top + bot) // 2
 
-            if (matrix[row_middle][0] <= target and target <= matrix[row_middle][-1]):
-                left = 0
-                right = len(matrix[0]) - 1
-
-                while left <= right:
-                    mid = (left + right) // 2
-
-                    if (matrix[row_middle][mid] == target):
-                        return True
-                    
-                    elif (matrix[row_middle][mid] > target):
-                        right = mid - 1
-                    
-                    else:
-                        left = mid + 1
-
-                return False
-            
-            if (matrix[row_middle][0] < target):
-                top = row_middle + 1
+            if (matrix[middle][0] <= target and target <= matrix[middle][-1]):
+                break
+            elif (matrix[middle][0] > target and matrix[middle][-1] > target):
+                bot = middle - 1
             
             else:
-                bot = row_middle - 1
+                top = middle + 1
+        
+        if top > bot:
+            return False
+        
+        row = (top + bot) // 2
+        left, right = 0, len(matrix[row]) - 1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if (matrix[row][mid] == target):
+                return True
+            
+            elif (matrix[row][mid] > target):
+                right = mid - 1
+            
+            else:
+                left = mid + 1
         
         return False
