cb81966f284b6782f961b6f41b4ef7b0cff65f59
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-13 18:34:04 -0400

    Update solution for reorder-list

--- leetcode/reorder-list.py
+++ leetcode/reorder-list.py
@@ -8,22 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Find middle
-        slow, fast = head, head.next
+        copy1 = head
+        copy2 = head
+
+        # 1. Identify the second half of the list
+        fast, slow = head.next, head
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
+            slow = slow.next
         
-        # Reverse second half
+        # 2. Reverse the second half of the list
         second = slow.next
         prev = slow.next = None
         while second:
-            tmp = second.next
+            temp = second.next
             second.next = prev
             prev = second
-            second = tmp
-        
-        # Merge two halfs
+            second = temp
+
+        # 3. Merge the 2 lists together
         first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
