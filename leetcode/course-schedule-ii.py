22ca78057378aff37af6b6374d9db7f3d41b035e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-14 16:24:25 -0400

    Update solution for course-schedule-ii

--- leetcode/course-schedule-ii.py
+++ leetcode/course-schedule-ii.py
@@ -2,8 +2,8 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adjList = {}
         final = []
-        visit = set()
         cycle = set()
+        visit = set()
 
         def dfs(crs):
             if (crs in cycle):
@@ -21,16 +21,14 @@ class Solution:
             
             cycle.remove(crs)
             final.append(crs)
-
+            
             return True
-
 
         for crs in range(numCourses):
             adjList[crs] = []
-
+        
         for crs, pre in prerequisites:
             adjList[crs].append(pre)
-        
 
         for crs in range(numCourses):
             if (not dfs(crs)):
