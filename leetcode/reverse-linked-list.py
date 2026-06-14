9699490464c889994ed3c0151827b1418b879d91
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-14 15:29:53 -0400

    Update solution for reverse-linked-list

--- leetcode/reverse-linked-list.py
+++ leetcode/reverse-linked-list.py
@@ -6,8 +6,8 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur = head
+        
         prev = None
-
         while cur:
             temp = cur.next
             cur.next = prev
