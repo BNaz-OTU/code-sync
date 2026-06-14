883bb2d62656ac1a3e7ebfc62802754a614ededf
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-14 16:00:24 -0400

    Update solution for add-two-numbers

--- leetcode/add-two-numbers.py
+++ leetcode/add-two-numbers.py
@@ -10,8 +10,7 @@ class Solution:
         carry = 0
 
         while l1 or l2 or carry:
-            val1, val2 = 0, 0
-
+            val1, val2, = 0, 0
             if (l1 is not None):
                 val1 = l1.val
                 l1 = l1.next
@@ -19,7 +18,7 @@ class Solution:
             if (l2 is not None):
                 val2 = l2.val
                 l2 = l2.next
-
+            
             sumVal = val1 + val2 + carry
             finalVal = sumVal % 10
             carry = sumVal // 10
