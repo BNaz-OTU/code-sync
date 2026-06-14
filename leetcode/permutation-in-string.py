308be5a1119849a7b4c9d4bad7cb576cc11f98e5
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-14 14:44:31 -0400

    Update solution for permutation-in-string

--- leetcode/permutation-in-string.py
+++ leetcode/permutation-in-string.py
@@ -1,36 +1,48 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if (len(s1) > len(s2)):
             return False
-        
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+
+        for idx in range(len(s1)):
+            s1Let = ord(s1[idx]) - ord('a')
+            s2Let = ord(s2[idx]) - ord('a')
+
+            s1Count[s1Let] += 1
+            s2Count[s2Let] += 1
         
         matches = 0
-        for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        for idx in range(26):
+            if (s1Count[idx] == s2Count[idx]):
+                matches += 1
         
-        l = 0
-        for r in range(len(s1), len(s2)):
+        # print(matches)
+        left = 0
+        for idx in range(len(s1), len(s2)):
             if (matches == 26):
                 return True
-            
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if (s1Count[index] == s2Count[index]):
+
+            # print(matches)
+
+            s2Let = ord(s2[idx]) - ord('a')
+            s2Count[s2Let] += 1
+
+            if (s2Count[s2Let] == s1Count[s2Let]):
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif (s2Count[s2Let] == s1Count[s2Let] + 1):
                 matches -= 1
             
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
+            s2LetPrev = ord(s2[left]) - ord('a')
+            s2Count[s2LetPrev] -= 1
+
+            if (s2Count[s2LetPrev] == s1Count[s2LetPrev]):
                 matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
+            elif (s2Count[s2LetPrev] == s1Count[s2LetPrev] - 1):
                 matches -= 1
             
-            l += 1
-        
+            left += 1
+
+
         return matches == 26
