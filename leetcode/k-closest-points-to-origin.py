a6ccc5afb1b885b6abb4a4a5f08b3269cc3c059e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-15 08:22:44 -0400

    Update solution for k-closest-points-to-origin

--- leetcode/k-closest-points-to-origin.py
+++ leetcode/k-closest-points-to-origin.py
@@ -1,16 +1,19 @@
+# import math
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
         final = []
 
         for x, y in points:
-            calc = math.sqrt(((x - 0) ** 2) + ((y - 0) ** 2))
-            heap.append([calc, x, y])
-        
-        heapify(heap)
+            x_val = abs(x) ** 2
+            y_val = abs(y) ** 2
 
-        while len(final) != k:
+            final_val = (x_val + y_val) ** 0.5
+            heappush(heap, [final_val, x, y])
+            
+        while k > 0:
             _, x, y = heappop(heap)
             final.append([x, y])
+            k -= 1
         
         return final
