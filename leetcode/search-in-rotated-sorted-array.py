908a42a9da102d9f73fd8a8b8cd41e3484c421ad
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-15 16:47:18 -0400

    Update solution for search-in-rotated-sorted-array

--- leetcode/search-in-rotated-sorted-array.py
+++ leetcode/search-in-rotated-sorted-array.py
@@ -5,20 +5,22 @@ class Solution:
 
         while left <= right:
             middle = (left + right) // 2
+            print(f"Left: {nums[left]} | Middle: {nums[middle]} | Right: {nums[right]}")
 
             if (nums[middle] == target):
                 return middle
-            
+
+
             if (nums[left] <= nums[middle]):
-                if (target > nums[middle] or target < nums[left]):
-                    left = middle + 1
-                else:
+                if (nums[left] <= target < nums[middle]):
                     right = middle - 1
+                else:
+                    left = middle + 1
             
             else:
-                if (target < nums[middle] or target > nums[right]):
-                    right = middle - 1
-                else:
+                if (nums[middle] < target <= nums[right]):
                     left = middle + 1
+                else:
+                    right = middle -1
         
         return -1
