a42b0b3a128678d898c274c99decaa03427f030f
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-20 11:54:52 -0400

    Update solution for middle-of-the-linked-list

--- leetcode/middle-of-the-linked-list.py
+++ leetcode/middle-of-the-linked-list.py
@@ -8,7 +8,7 @@ class Solution:
         slow = head
         fast = head
 
-        while (fast and fast.next):
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
