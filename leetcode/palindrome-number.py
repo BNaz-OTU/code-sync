e21551f2e0e9ee0c22844eb23f7c5ff04871acf1
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-19 21:28:01 -0400

    Update solution for palindrome-number

--- leetcode/palindrome-number.py
+++ leetcode/palindrome-number.py
@@ -1,28 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         str_x = str(x)
-        middle = len(str_x) // 2
 
-        if (len(str_x) % 2 == 1):
-            left, right = middle, middle
+        left, right = 0, len(str_x) - 1
 
-            while str_x[left] == str_x[right]:
-                left -= 1
-                right += 1
+        while left < right and str_x[left] == str_x[right]:
+            left += 1
+            right -= 1
 
-                if (left < 0):
-                    return True
-            
-            return False
-        
+        if (left >= right):
+            return True
         else:
-            left, right = middle - 1, middle
-
-            while str_x[left] == str_x[right]:
-                left -= 1
-                right += 1
-
-                if (left < 0):
-                    return True
-            
             return False
