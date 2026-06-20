0ea097e455fac5b76e191ed6ef85431d2766eccb
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-19 21:00:45 -0400

    Update solution for merge-sorted-array

--- leetcode/merge-sorted-array.py
+++ leetcode/merge-sorted-array.py
@@ -3,23 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # m -> nums1 | n -> nums2
         combine = m + n - 1
 
-        while m > 0 and n > 0:
-            val1 = nums1[m - 1]
-            val2 = nums2[n - 1]
-
-            if (val1 > val2):
-                nums1[combine] = val1
-                m -= 1
-                combine -= 1
-            
-            else:
-                nums1[combine] = val2
+        while n > 0 and m > 0:
+            if nums1[m - 1] <= nums2[n - 1]:
+                nums1[combine] = nums2[n - 1]
                 n -= 1
-                combine -= 1
+            else:
+                nums1[combine] = nums1[m - 1]
+                m -= 1
+            
+            combine -= 1
         
+        print(f"n: {n}")
+        print(f"m: {m}")
+
         while n > 0:
             nums1[combine] = nums2[n - 1]
             combine -= 1
             n -= 1
+        
+        # print(nums1)
