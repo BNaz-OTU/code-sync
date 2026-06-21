eedc5e76e421b80152f7eab4675977629f2ebab3
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-21 15:44:19 -0400

    Update solution for climbing-stairs

--- leetcode/climbing-stairs.py
+++ leetcode/climbing-stairs.py
@@ -4,7 +4,7 @@ class Solution:
 
         for _ in range(n):
             temp = dp[0]
-            dp[0] = dp[0] + dp[1]
+            dp[0] = dp[1] + dp[0]
             dp[1] = temp
         
         return dp[0]
