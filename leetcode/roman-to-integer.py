21c4577e8a8b4548d8d2be1581faf67e4d150860
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-21 13:37:32 -0400

    Update solution for roman-to-integer

--- leetcode/roman-to-integer.py
+++ leetcode/roman-to-integer.py
@@ -1,26 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sumVal = 0
         roman = {
             "I" : 1,
             "V" : 5,
             "X" : 10,
             "L" : 50,
-            "C" : 100,
+            "C" : 100, 
             "D" : 500,
             "M" : 1000
         }
 
-        for idx in range(len(s) - 1):
-            cur = roman[s[idx]]
-            nxt = roman[s[idx + 1]]
+        total = 0
 
-            if (cur < nxt):
-                sumVal -= cur
+        for idx in range(len(s) - 1):
+            if (roman[s[idx]] < roman[s[idx + 1]]):
+                total -= roman[s[idx]]
             
             else:
-                sumVal += cur
-            
-        sumVal += roman[s[-1]]
+                total += roman[s[idx]]
         
-        return sumVal
+        total += roman[s[-1]]
+        
+        return total
