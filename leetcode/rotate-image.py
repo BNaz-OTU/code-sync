3d8241dfde35d4e222696b24e373c0ff21ad6810
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-21 21:57:40 -0400

    Update solution for rotate-image

--- leetcode/rotate-image.py
+++ leetcode/rotate-image.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
         top, bot = 0, len(matrix) - 1
         left, right = 0, len(matrix) - 1
 
-        while top <= bot and left <= right:
-
+        while top < bot and left < right:
             for idx in range(right - left):
                 tempTopLeft = matrix[top][left + idx]
 
@@ -19,7 +17,7 @@ class Solution:
                 matrix[bot][right - idx] = matrix[top + idx][right]
 
                 matrix[top + idx][right] = tempTopLeft
-            
+
             top += 1
             bot -= 1
             left += 1
