a462904ccfe6eb486598f43be4305c3ac2271c3e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-22 19:33:51 -0400

    Update solution for roman-to-integer

--- leetcode/roman-to-integer.py
+++ leetcode/roman-to-integer.py
@@ -5,13 +5,12 @@ class Solution:
             "V" : 5,
             "X" : 10,
             "L" : 50,
-            "C" : 100, 
+            "C" : 100,
             "D" : 500,
             "M" : 1000
         }
 
         total = 0
-
         for idx in range(len(s) - 1):
             if (roman[s[idx]] < roman[s[idx + 1]]):
                 total -= roman[s[idx]]
@@ -20,5 +19,4 @@ class Solution:
                 total += roman[s[idx]]
         
         total += roman[s[-1]]
-        
         return total
