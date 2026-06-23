52f31a355ccd6074fc7b07c8870a2087e98ba398
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-22 21:14:31 -0400

    Update solution for move-zeroes

--- leetcode/move-zeroes.py
+++ leetcode/move-zeroes.py
@@ -4,10 +4,9 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         left = 0
-
         for right in range(len(nums)):
             while left < right and nums[left] != 0:
                 left += 1
             
-            if (nums[left] == 0 and nums[right] != 0):
+            if (nums[right] != 0 and nums[left] == 0):
                 nums[left], nums[right] = nums[right], nums[left]
