acb2caffa0d5bdbe502e4c88a59b54dd717a8785
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-24 19:59:00 -0400

    Update solution for number-of-islands

--- leetcode/number-of-islands.py
+++ leetcode/number-of-islands.py
@@ -7,21 +7,21 @@ class Solution:
         def dfs(row, col):
             if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
-                ((row, col) in visit) or
-                (grid[row][col] == "0")):
+                (grid[row][col] == "0") or
+                ((row, col) in visit)):
                 return
             
             visit.add((row, col))
 
             dfs(row + 1, col)
             dfs(row - 1, col)
-            dfs(row, col + 1)
             dfs(row, col - 1)
+            dfs(row, col + 1)
 
         for row in range(ROWS):
             for col in range(COLS):
                 if (grid[row][col] == "1" and (row, col) not in visit):
                     count += 1
                     dfs(row, col)
-
+        
         return count
