53c2b8bcf36ff6b2bdcc2ea4a640ac52d7f4e1cf
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-23 22:59:29 -0400

    Update solution for lru-cache

--- leetcode/lru-cache.py
+++ leetcode/lru-cache.py
@@ -1,33 +1,33 @@
-class Node:
+class Node: 
     def __init__(self, key=0, val=0, prev=None, nxt=None):
         self.key = key
         self.val = val
         self.prev = prev
         self.nxt = nxt
 
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.hashMap = {}
         self.cap = capacity
+        self.hashMap = {}
 
-        self.left = Node() # Least Used
-        self.right = Node() # Most Used
+        self.left = Node()
+        self.right = Node()
 
-        # Connect the nodes
-        self.right.prev = self.left
         self.left.nxt = self.right
+        self.right.prev = self.left
         
+
     def get(self, key: int) -> int:
-        if key in self.hashMap:
+        if (key in self.hashMap):
             val = self.hashMap[key].val
             self.remove(key)
             self.insert(key, val)
-
-            return self.hashMap[key].val
+            return val
         
         return -1
-    
+
     def remove(self, key):
         node = self.hashMap[key]
 
@@ -40,16 +40,17 @@ class LRUCache:
         node = Node(key, val)
 
         prev = self.right.prev
-        nxt = self.right
-
+        
+        # Connect Node to DLL
+        node.nxt = self.right
         node.prev = prev
-        node.nxt = nxt
 
+        # Connect the DLL to Node
         self.right.prev = node
         prev.nxt = node
 
         self.hashMap[key] = node
-
+        
 
     def put(self, key: int, value: int) -> None:
         if (key in self.hashMap):
@@ -62,7 +63,6 @@ class LRUCache:
             self.remove(least.key)
 
             del self.hashMap[least.key]
-
         
 
 
