568041d818e6cf3aa73f957f3c2349af4418da63
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-06-26 08:03:10 -0400

    Update solution for top-k-frequent-elements

--- leetcode/top-k-frequent-elements.py
+++ leetcode/top-k-frequent-elements.py
@@ -1,21 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        final = []
         hashMap = {}
         heap = []
+        final = []
 
         for num in nums:
             if (num not in hashMap):
                 hashMap[num] = 0
-            
+
             hashMap[num] += 1
         
-        for value, freq in hashMap.items():
-            heappush(heap, [-freq, value])
+        for key, freq in hashMap.items():
+            heappush(heap, [-freq, key])
         
         while k > 0:
-            _, val = heappop(heap)
-            final.append(val)
+            _, key = heappop(heap)
+            final.append(key)
+
             k -= 1
         
         return final
