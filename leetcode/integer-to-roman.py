9bfa159297e351f6ebc1a7e7b8154186f0898f05
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-10 16:41:35 -0400

    Update solution for integer-to-roman

--- leetcode/integer-to-roman.py
+++ leetcode/integer-to-roman.py
@@ -1,28 +1,29 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         roman = [
-            ["M", 1000],
-            ["CM", 900],
-            ["D", 500],
-            ["CD", 400],
-            ["C", 100],
-            ["XC", 90],
-            ["L", 50],
-            ["XL", 40],
-            ["X", 10],
-            ["IX", 9],
-            ["V", 5],
-            ["IV", 4], 
-            ["I", 1]
+            [1000, "M"],
+            [900, "CM"],
+            [500, "D"],
+            [400, "CD"],
+            [100, "C"],
+            [90, "XC"],
+            [50, "L"],
+            [40, "XL"],
+            [10, "X"],
+            [9, "IX"],
+            [5, "V"],
+            [4, "IV"],
+            [1, "I"]
         ]
 
         final = ""
 
-        for sym, val in roman:
-            count = num // val
-
-            final += (count * sym)
-
-            num = num % val
+        for nRom, cRom in roman:
+            if (num // nRom == 0):
+                continue
+            else:
+                val = num // nRom
+                final += val * cRom
+                num = num % nRom
         
         return final
