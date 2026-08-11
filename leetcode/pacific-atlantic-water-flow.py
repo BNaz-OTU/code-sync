948f34bd081dff2a833c6a97337dba2806d8893e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-11 13:50:56 -0400

    Update solution for pacific-atlantic-water-flow

--- leetcode/pacific-atlantic-water-flow.py
+++ leetcode/pacific-atlantic-water-flow.py
@@ -1,22 +1,23 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        atl = set()
+        pac = set()
         final = []
 
         def dfs(row, col, ocean, prevHeight):
-            if ((row < 0 or row >= ROWS) or 
+            if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
                 ((row, col) in ocean) or
                 (prevHeight > heights[row][col])):
                 return
             
             ocean.add((row, col))
-            
-            dfs(row + 1, col, ocean, heights[row][col])
+
             dfs(row - 1, col, ocean, heights[row][col])
-            dfs(row, col + 1, ocean, heights[row][col])
+            dfs(row + 1, col, ocean, heights[row][col])
             dfs(row, col - 1, ocean, heights[row][col])
+            dfs(row, col + 1, ocean, heights[row][col])
 
         for row in range(ROWS):
             dfs(row, 0, pac, -1)
@@ -26,8 +27,8 @@ class Solution:
             dfs(0, col, pac, -1)
             dfs(ROWS - 1, col, atl, -1)
         
-        for coords in atl:
-            if (coords in pac):
+        for coords in pac:
+            if (coords in atl):
                 final.append(coords)
         
         return final
