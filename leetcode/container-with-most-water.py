81c7fe7ded8d946a5d296045dada76ab19fe2342
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-16 16:09:08 -0400

    Update solution for container-with-most-water

--- leetcode/container-with-most-water.py
+++ leetcode/container-with-most-water.py
@@ -1,25 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # SOLVED BY MYSELF
-        # SOLN VID FOR REFERENCE: https://www.youtube.com/watch?v=UuiTKBwPgAo
         left = 0
         right = len(height) - 1
-        maxWater = 0
+        maxArea = 0
 
         while left < right:
-            # I'm forced to choose the smaller height, otherwise choosing the
-            # smaller bigger height leak out some water/go against the rule of
-            # "no slanting the container" 
-            minHeight = min(height[left], height[right])
-            width = right - left
+            heightVal = min(height[left], height[right])
+            widthVal = right - left
 
-            # Update everytime there is a new area that can take more water
-            maxWater = max((minHeight * width), maxWater)
+            maxArea = max(maxArea, heightVal * widthVal)
 
-            # Update the smaller height to hopefully get a bigger height value
             if (height[left] < height[right]):
                 left += 1
             else:
                 right -= 1
         
-        return maxWater
+        return maxArea
