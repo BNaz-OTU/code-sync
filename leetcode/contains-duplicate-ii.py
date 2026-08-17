050943d8c7c72dd1f73941abc31ad3e9fc36f22b
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-17 16:12:54 -0400

    Add solution for contains-duplicate-ii

---
+++ leetcode/contains-duplicate-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        visit = set()
+        left = 0
+
+        for right in range(len(nums)):
+            if (abs(left - right) > k):
+                visit.remove(nums[left])
+                left += 1
+            
+            if (nums[right] in visit):
+                return True
+            
+            visit.add(nums[right])
+        
+        return False
