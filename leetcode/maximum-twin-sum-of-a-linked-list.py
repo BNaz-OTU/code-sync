691f90677f9a3d0db1072fc10353a9ac2a0786d5
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-19 18:03:28 -0400

    Update solution for maximum-twin-sum-of-a-linked-list

--- leetcode/maximum-twin-sum-of-a-linked-list.py
+++ leetcode/maximum-twin-sum-of-a-linked-list.py
@@ -5,21 +5,22 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        # t: 0, 0, 0, 0, 0, 0
-        # n: 1, 2, 3, 4, 5, 6
-        # i: 0, 1, 2, 3, 4, 5
-        list_nums = []
+        slow = head
+        fast = head
+        prev = None
 
-        while head:
-            list_nums.append(head.val)
-            head = head.next
+        while fast and fast.next:
+            fast = fast.next.next
+
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
         
         maxNum = 0
-        left, right = 0, len(list_nums) - 1
-
-        while left < right:
-            maxNum = max(maxNum, list_nums[left] + list_nums[right])
-            left += 1
-            right -= 1
+        while slow:
+            maxNum = max(maxNum, slow.val + prev.val)
+            slow = slow.next
+            prev = prev.next
         
         return maxNum
