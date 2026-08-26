e94969f35b2c64202b416f819e9789e20052d0a9
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-26 16:42:47 -0400

    Update solution for letter-combinations-of-a-phone-number

--- leetcode/letter-combinations-of-a-phone-number.py
+++ leetcode/letter-combinations-of-a-phone-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # USED SOLN: https://www.youtube.com/watch?v=0snEunUacZY
-
-        letToNum = {
+        num_to_let = {
             "2" : ["a", "b", "c"],
             "3" : ["d", "e", "f"],
             "4" : ["g", "h", "i"],
@@ -13,17 +11,16 @@ class Solution:
             "9" : ["w", "x", "y", "z"]
         }
 
-        final = []
+        final = []        
 
-        def backtrack(idx, curStr):
-            if (len(curStr) == len(digits)):
-                final.append(curStr)
-                return
-            
-            for char in letToNum[digits[idx]]:
-                backtrack(idx + 1, curStr + char)
+        def dfs_let(idx, comb):
+            if (len(comb) == len(digits)):
+                final.append(comb)
+                return 
+            for cur in num_to_let[digits[idx]]:
+                dfs_let(idx + 1, comb + cur)
         
-        if (len(digits) > 0):
-            backtrack(0, "")
-
+        if digits:
+            dfs_let(0, "")
+            
         return final
