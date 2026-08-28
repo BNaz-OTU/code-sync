e89c13bdb5cbe0fe99333a4f3e39a2dde05d70aa
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-28 12:39:16 -0400

    Update solution for palindrome-partitioning

--- leetcode/palindrome-partitioning.py
+++ leetcode/palindrome-partitioning.py
@@ -1,27 +1,27 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        # USED SOLN: https://www.youtube.com/watch?v=3jvWodd7ht0
-        res = []
+        final = []
         part = []
 
-        def dfs(idx):
-            if (idx >= len(s)):
-                res.append(part.copy())
+        def dfs(i):
+            if (i >= len(s)):
+                final.append(part.copy())
                 return
             
-            for jdx in range(idx, len(s)):
-                if (self.isPali(s, idx, jdx)):
-                    part.append(s[idx:jdx + 1])
-                    dfs(jdx + 1)
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i : j+1])
+                    dfs(j + 1)
                     part.pop()
         
         dfs(0)
-        return res
+        return final
 
-    def isPali(self, s, left, right):
-        while left < right:
-            if (s[left] != s[right]):
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
                 return False
-            left += 1
-            right -= 1
+            
+            l, r = l + 1, r - 1
+        
         return True
