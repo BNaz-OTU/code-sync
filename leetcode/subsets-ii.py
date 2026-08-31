f37ff6621c6a8a3ba917d6dace7cf84b4057bc0e
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-08-31 15:00:22 -0400

    Update solution for subsets-ii

--- leetcode/subsets-ii.py
+++ leetcode/subsets-ii.py
@@ -1,21 +1,20 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         final = []
-        current = []
         nums.sort()
 
-        def subsetFinder(idx, current):
-            if (idx == len(nums)):
-                final.append(current.copy())
-                return
+        def dfs(subset, idx):
+            if (idx >= len(nums) or len(subset) == len(nums)):
+                final.append(subset.copy())
+                return 
             
-            current.append(nums[idx])
-            subsetFinder(idx + 1, current)
-            current.pop()
-
+            subset.append(nums[idx])
+            dfs(subset, idx + 1)
+            subset.pop()
             while idx + 1 < len(nums) and nums[idx] == nums[idx + 1]:
                 idx += 1
-            subsetFinder(idx + 1, current)
-        
-        subsetFinder(0, current)
+
+            dfs(subset, idx + 1)
+
+        dfs([], 0)
         return final
