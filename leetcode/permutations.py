05d9ab428f71d55d9bb7abff109e4ab9485a8e12
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-09-01 13:57:20 -0400

    Update solution for permutations

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -1,15 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
         perms = [[]]
 
         for num in nums:
-            res = []
+            temp = []
             for p in perms:
                 for idx in range(len(p) + 1):
                     p_copy = p.copy()
                     p_copy.insert(idx, num)
-                    res.append(p_copy)
-            perms = res
+                    temp.append(p_copy)
+            
+            perms = temp
         
         return perms
