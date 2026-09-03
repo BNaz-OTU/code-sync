14217360ff0b14e7e22b70f12f800d47339f7c15
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-09-03 11:52:26 -0400

    Update solution for word-search

--- leetcode/word-search.py
+++ leetcode/word-search.py
@@ -3,31 +3,29 @@ class Solution:
         ROWS, COLS = len(board), len(board[0])
         visit = set()
 
-        def dfs(row, col, wordIdx):
+        def dfs(wordIdx, row, col):
             if (wordIdx == len(word)):
                 return True
-
+            
             if ((row < 0 or row >= ROWS) or
                 (col < 0 or col >= COLS) or
-                (board[row][col] != word[wordIdx]) or
+                (board[row][col] != word[wordIdx]) or 
                 ((row, col) in visit)):
                 return False
             
             visit.add((row, col))
 
-            verdict = (dfs(row + 1, col, wordIdx + 1) or
-                        dfs(row - 1, col, wordIdx + 1) or
-                        dfs(row, col + 1, wordIdx + 1) or
-                        dfs(row, col - 1, wordIdx + 1)
-                    )
-            
+            verdict = (dfs(wordIdx + 1, row + 1, col) or
+            dfs(wordIdx + 1, row - 1, col) or
+            dfs(wordIdx + 1, row, col + 1) or
+            dfs(wordIdx + 1, row, col - 1))
+
             visit.remove((row, col))
-            
             return verdict
 
         for row in range(ROWS):
             for col in range(COLS):
-                if (board[row][col] == word[0] and dfs(row, col, 0)):
+                if (board[row][col] == word[0] and dfs(0, row, col)):
                     return True
         
         return False
