1d8d20417b086187f0d9488b37f922b1aabc588a
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-09-03 11:58:36 -0400

    Update solution for generate-parentheses

--- leetcode/generate-parentheses.py
+++ leetcode/generate-parentheses.py
@@ -1,18 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         final = []
-
-        def dfs(text, left, right):
-            # print(text)
+        def dfs(left, right, brackets):
             if (left == n and right == n):
-                final.append(text)
+                final.append(brackets)
                 return
             
             if (left < n):
-                dfs(text + "(", left + 1, right)
+                dfs(left + 1, right, brackets + "(")
             
             if (right < left):
-                dfs(text + ")", left, right + 1)
+                dfs(left, right + 1, brackets + ")")
 
-        dfs("", 0, 0)
+        dfs(0, 0, "")
         return final
