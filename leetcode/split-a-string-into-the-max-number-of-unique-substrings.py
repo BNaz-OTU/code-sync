355eb0d72e8b12cc25767cd66253223539eba504
Bilal Nazar <156726707+BNaz-OTU@users.noreply.github.com>
2026-09-18 11:09:56 -0400

    Add solution for split-a-string-into-the-max-number-of-unique-substrings

---
+++ leetcode/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        maxCount = 0
+        subset = set()
+        
+        def dfs(idx):
+            nonlocal maxCount
+            if (idx >= len(s)):
+                maxCount = max(maxCount, len(subset))
+                return
+
+            for jdx in range(idx, len(s)):
+                val = s[idx : jdx + 1]
+                
+                if (val in subset):
+                    continue
+
+                subset.add(val)
+                dfs(jdx + 1)
+                subset.remove(val)
+        
+        dfs(0)
+        return maxCount
